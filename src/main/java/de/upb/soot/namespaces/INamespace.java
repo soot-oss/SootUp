975b898ca7b855d1addcc66c6c981cbfa7681ccb
Andreas Dann <adann@mail.upb.de>
2018-08-10 12:56:09 +0200

    fix compilation issues in namespace

--- src/main/java/de/upb/soot/namespaces/INamespace.java
+++ src/main/java/de/upb/soot/namespaces/INamespace.java
@@ -15,21 +15,21 @@ import java.util.Optional;
  * @author Manuel Benz created on 22.05.18
  */
 public interface INamespace {
-  /**
+  /** FIXME: a namespace should only return class sources
    * Searches the namespace and sub-namespaces for all contained classes.
    * 
    * @return A collection of not-yet-resolved {@link SootClass}es
-   */
+   *//*
   Collection<SootClass> getClasses(SignatureFactory factory);
 
-  /**
+  *//**
    * Searches the namespace and all sub-namespaces for a {@link SootClass} matching the given {@link ClassSignature}.
    *
    * @param classSignature
    *          The {@link ClassSignature} denoting the searched {@link SootClass}
    * @return An optional containing the found class or empty if the class does not reside in this namespace
-   */
-  Optional<SootClass> getClass(ClassSignature classSignature);
+   *//*
+  Optional<SootClass> getClass(ClassSignature classSignature);*/
 
   Optional<ClassSource> getClassSource(ClassSignature signature);
 }
