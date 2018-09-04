7b88bd06dc68e990e8ed68bc63273fcb46f0d57c
Manuel Benz <manuelf.benz@gmail.com>
2018-09-04 12:50:22 +0200

    Adds some comments

--- src/main/java/de/upb/soot/views/Scene.java
+++ src/main/java/de/upb/soot/views/Scene.java
@@ -1,9 +1,5 @@
 package de.upb.soot.views;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.pattern.Patterns;
-import akka.util.Timeout;
 import de.upb.soot.buildactor.ClassBuilderActor;
 import de.upb.soot.buildactor.ModuleBuilderActor;
 import de.upb.soot.buildactor.ReifyMessage;
@@ -12,51 +8,54 @@ import de.upb.soot.namespaces.INamespace;
 import de.upb.soot.namespaces.JavaClassPathNamespace;
 import de.upb.soot.namespaces.classprovider.ClassSource;
 import de.upb.soot.signatures.ClassSignature;
+
+import java.util.Optional;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
-import java.util.Optional;
-
 public class Scene {
-    private ActorSystem system;
+  private ActorSystem system;
 
-    public Optional<SootClass> getClass(ClassSignature signature) {
-        Optional<SootClass> result = Optional.empty();
-        // TODO: cache
+  public Optional<SootClass> getClass(ClassSignature signature) {
+    Optional<SootClass> result = Optional.empty();
+    // TODO: cache
 
-        // TODO: decide for phantom
+    // TODO: decide for phantom
 
-        Optional<ClassSource> source = pollNamespaces(signature).getClassSource(signature);
+    Optional<ClassSource> source = pollNamespaces(signature).getClassSource(signature);
+    // MB: consider using source.flatMap(#methodRef) here. methodRef can than point to the actual logic for class resolution
+    if (source.isPresent()) {
 
-        if (source.isPresent()) {
-
-            ActorRef cb = createActor(source.get());
-            Timeout timeout = new Timeout(Duration.create(5, "seconds"));
-            Future<Object> cbFuture = Patterns.ask(cb, new ReifyMessage(), timeout);
-            try {
-                result = Optional.of((SootClass) Await.result(cbFuture, timeout.duration()));
-            } catch (Exception e) {
-                // TODO: Do something meaningful here
-            }
-        }
-
-        return result;
+      ActorRef cb = createActor(source.get());
+      Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+      Future<Object> cbFuture = Patterns.ask(cb, new ReifyMessage(), timeout);
+      try {
+        result = Optional.of((SootClass) Await.result(cbFuture, timeout.duration()));
+      } catch (Exception e) {
+        // TODO: Do something meaningful here
+      }
     }
 
-    private ActorRef createActor(ClassSource source) {
-        if (source.getClassSignature().isModuleInfo())
-            return system.actorOf(ModuleBuilderActor.props(source));
-        return system.actorOf(ClassBuilderActor.props(source));
-    }
+    return result;
+  }
 
-    private INamespace pollNamespaces(ClassSignature signature) {
-        // TODO: Traverse through namespaces
+  private ActorRef createActor(ClassSource source) {
+    if (source.getClassSignature().isModuleInfo())
+      return system.actorOf(ModuleBuilderActor.props(source));
+    return system.actorOf(ClassBuilderActor.props(source));
+  }
 
-        return new JavaClassPathNamespace(null, "");
-    }
-
-
-
+  private INamespace pollNamespaces(ClassSignature signature) {
+    // TODO: Traverse through namespaces
+    // MB this does not make sense in our current impl. you would rather poll the namespaces one after another and if one
+    // returns a non-empty optional, you would pass that to the caller
+    return new JavaClassPathNamespace(null, "");
+  }
 
 }
