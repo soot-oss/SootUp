77c4b576fec749395b4545565a7b2c13c2b9649b
Rajat Khanna <rajatkhanna311@gmail.com>
2018-09-20 15:24:18 +0200

    Formatted code

--- src/main/java/de/upb/soot/namespaces/FileType.java
+++ src/main/java/de/upb/soot/namespaces/FileType.java
@@ -1,16 +1,15 @@
 package de.upb.soot.namespaces;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.filefilter.MagicNumberFileFilter;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.EnumSet;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.MagicNumberFileFilter;
 
 /**
  * An enumeration of common file types used for class loading/writing and other purposes.
@@ -19,85 +18,90 @@ import java.util.zip.ZipFile;
  */
 public enum FileType {
 
-    JAR("jar", new byte[]{(byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04}), ZIP("zip", new byte[]{(byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04}),
-    APK("apk", new byte[]{(byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04}), CLASS("class", new byte[]{(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE}),
-    JAVA("java", new byte[]{(byte) 0x70, (byte) 0x61, (byte) 0x63, (byte) 0x6B}), JIMPLE("jimple", new byte[]{(byte) 0x70, (byte) 0x75, (byte) 0x62, (byte) 0x6C});
+  JAR("jar", new byte[] { (byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04 }),
+  ZIP("zip", new byte[] { (byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04 }),
+  APK("apk", new byte[] { (byte) 0x50, (byte) 0x4B, (byte) 0x03, (byte) 0x04 }),
+  CLASS("class", new byte[] { (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE }),
+  JAVA("java", new byte[] { (byte) 0x70, (byte) 0x61, (byte) 0x63, (byte) 0x6B }),
+  JIMPLE("jimple", new byte[] { (byte) 0x70, (byte) 0x75, (byte) 0x62, (byte) 0x6C });
 
-    public static final EnumSet<FileType> ARCHIVE_TYPES = EnumSet.of(JAR, ZIP, APK);
+  public static final EnumSet<FileType> ARCHIVE_TYPES = EnumSet.of(JAR, ZIP, APK);
 
-    public static boolean isFileType(Path path, FileType fileType) {
-        // converts path to a file object
-        String pth = path.toString().toLowerCase();
-        File file = path.toFile();
-        MagicNumberFileFilter fileFilter = new MagicNumberFileFilter(fileType.magic_bytes);
+  public static boolean isFileType(Path path, FileType fileType) {
+    // converts path to a file object
+    String pth = path.toString().toLowerCase();
+    File file = path.toFile();
+    MagicNumberFileFilter fileFilter = new MagicNumberFileFilter(fileType.magic_bytes);
 
-        // If magic number matches
-        if (fileFilter.accept(file)) {
-            if (isArchive(path)) {
-                if (hasClassesDex(file)) {
-                    return fileType == APK;
-                } else if(fileType == APK && !hasClassesDex(file)) {
-                    return false;
-                } else
-                return fileType.extension.equals(FilenameUtils.getExtension(pth));
-            }
-            return fileType.extension.equals(FilenameUtils.getExtension(pth));
+    // If magic number matches
+    if (fileFilter.accept(file)) {
+      if (isArchive(path)) {
+        if (hasClassesDex(file)) {
+          return fileType == APK;
+        } else if (fileType == APK && !hasClassesDex(file)) {
+          return false;
+        } else {
+          return fileType.extension.equals(FilenameUtils.getExtension(pth));
         }
-
-        // Checking extension in case magic number doesn't match
-        return fileType.extension.equals(FilenameUtils.getExtension(pth));
+      }
+      return fileType.extension.equals(FilenameUtils.getExtension(pth));
     }
 
-    // Checks if the file is an archive
-    public static boolean isArchive(Path path) {
-        // Get the extension of file
-        String str_path = path.toString().toLowerCase();
-        String fileExt = FilenameUtils.getExtension(str_path);
-        for (FileType f : ARCHIVE_TYPES) {
-            if(fileExt.equals(f.toString().toLowerCase()))
-                return true;
-            else
-                continue;
-        }
-        return false;
-    }
+    // Checking extension in case magic number doesn't match
+    return fileType.extension.equals(FilenameUtils.getExtension(pth));
+  }
 
-    // Checks if the archive contains classes.dex files
-    private static boolean hasClassesDex(File file) {
-        ZipFile zf = null;
+  // Checks if the file is an archive
+  public static boolean isArchive(Path path) {
+    // Get the extension of file
+    String str_path = path.toString().toLowerCase();
+    String fileExt = FilenameUtils.getExtension(str_path);
+    for (FileType f : ARCHIVE_TYPES) {
+      if (fileExt.equals(f.toString().toLowerCase())) {
+        return true;
+      } else {
+        continue;
+      }
+    }
+    return false;
+  }
+
+  // Checks if the archive contains classes.dex files
+  private static boolean hasClassesDex(File file) {
+    ZipFile zf = null;
+    try {
+      zf = new ZipFile(file);
+      Enumeration<?> en = zf.entries();
+      while (en.hasMoreElements()) {
+        ZipEntry z = (ZipEntry) en.nextElement();
+        String name = z.getName();
+        if (name.equals("classes.dex")) {
+          return true;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (zf != null) {
         try {
-            zf = new ZipFile(file);
-            Enumeration<?> en = zf.entries();
-            while (en.hasMoreElements()) {
-                ZipEntry z = (ZipEntry) en.nextElement();
-                String name = z.getName();
-                if (name.equals("classes.dex")) {
-                    return true;
-                }
-            }
+          zf.close();
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (zf != null) {
-                try {
-                    zf.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+          e.printStackTrace();
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    private String extension;
-    private byte magic_bytes[];
+  private String extension;
+  private byte magic_bytes[];
 
-    FileType(String fileExtension, byte magic_bytes[]) {
-        this.magic_bytes = magic_bytes;
-        this.extension = fileExtension;
-    }
+  FileType(String fileExtension, byte magic_bytes[]) {
+    this.magic_bytes = magic_bytes;
+    this.extension = fileExtension;
+  }
 
-    public String getExtension() {
-        return extension;
-    }
+  public String getExtension() {
+    return extension;
+  }
 }
--- src/test/java/de/upb/soot/namespaces/FileTypeTest.java
+++ src/test/java/de/upb/soot/namespaces/FileTypeTest.java
@@ -1,76 +1,77 @@
 package de.upb.soot.namespaces;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class FileTypeTest {
 
-    @Test
-    public void testFileTypeJar() throws URISyntaxException {
-        URL url = getClass().getResource("demo.jar");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.JAR));
-    }
+  @Test
+  public void testFileTypeJar() throws URISyntaxException {
+    URL url = getClass().getResource("demo.jar");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.JAR));
+  }
 
-    @Test
-    public void testFileTypeJava() throws URISyntaxException {
-        URL url = getClass().getResource("PrimitiveLocals.java");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.JAVA));
-    }
+  @Test
+  public void testFileTypeJava() throws URISyntaxException {
+    URL url = getClass().getResource("PrimitiveLocals.java");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.JAVA));
+  }
 
-    @Test
-    public void testFileTypeClass() throws URISyntaxException {
-        URL url = getClass().getResource("PrimitiveLocals.class");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.CLASS));
-    }
+  @Test
+  public void testFileTypeClass() throws URISyntaxException {
+    URL url = getClass().getResource("PrimitiveLocals.class");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.CLASS));
+  }
 
-    @Test
-    public void testFileTypeApk() throws URISyntaxException {
-        URL url = getClass().getResource("demo.apk");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.APK));
-    }
+  @Test
+  public void testFileTypeApk() throws URISyntaxException {
+    URL url = getClass().getResource("demo.apk");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.APK));
+  }
 
-    @Test
-    public void testFileTypeApkAsJar() throws URISyntaxException {
-        URL url = getClass().getResource("demo.apk");
-        Path path = Paths.get(url.toURI());
-        assertFalse(FileType.isFileType(path, FileType.JAR));
-    }
+  @Test
+  public void testFileTypeApkAsJar() throws URISyntaxException {
+    URL url = getClass().getResource("demo.apk");
+    Path path = Paths.get(url.toURI());
+    assertFalse(FileType.isFileType(path, FileType.JAR));
+  }
 
-    @Test
-    public void testFileTypeApkWithoutDex() throws URISyntaxException {
-        URL url = getClass().getResource("NoClassesDex.apk");
-        Path path = Paths.get(url.toURI());
-        assertFalse(FileType.isFileType(path, FileType.APK));
-    }
+  @Test
+  public void testFileTypeApkWithoutDex() throws URISyntaxException {
+    URL url = getClass().getResource("NoClassesDex.apk");
+    Path path = Paths.get(url.toURI());
+    assertFalse(FileType.isFileType(path, FileType.APK));
+  }
 
-    @Test
-    public void testFileTypeZip() throws URISyntaxException {
-        URL url = getClass().getResource("demo.zip");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.ZIP));
-    }
+  @Test
+  public void testFileTypeZip() throws URISyntaxException {
+    URL url = getClass().getResource("demo.zip");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.ZIP));
+  }
 
-    @Test
-    public void testFileTypeJimple() throws URISyntaxException {
-        URL url = getClass().getResource("PrimitiveLocals.jimple");
-        Path path = Paths.get(url.toURI());
-        assertTrue(FileType.isFileType(path, FileType.JIMPLE));
-    }
+  @Test
+  public void testFileTypeJimple() throws URISyntaxException {
+    URL url = getClass().getResource("PrimitiveLocals.jimple");
+    Path path = Paths.get(url.toURI());
+    assertTrue(FileType.isFileType(path, FileType.JIMPLE));
+  }
 
-    @Test
-    public void testFileTypeJimpleAsJava() throws URISyntaxException {
-        URL url = getClass().getResource("PrimitiveLocals.jimple");
-        Path path = Paths.get(url.toURI());
-        assertFalse(FileType.isFileType(path, FileType.JAVA));
-    }
+  @Test
+  public void testFileTypeJimpleAsJava() throws URISyntaxException {
+    URL url = getClass().getResource("PrimitiveLocals.jimple");
+    Path path = Paths.get(url.toURI());
+    assertFalse(FileType.isFileType(path, FileType.JAVA));
+  }
 }
