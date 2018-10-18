cc088ca46a1730e02e0c3c905a3da45e53b860d1
Markus Schmidt <markus262@web.de>
2018-10-18 15:55:49 +0200

    extended for Fields

--- src/main/java/de/upb/soot/jimple/basic/LocalGenerator.java
+++ src/main/java/de/upb/soot/jimple/basic/LocalGenerator.java
@@ -15,6 +15,9 @@ import de.upb.soot.jimple.common.type.Type;
 import de.upb.soot.jimple.common.type.UnknownType;
 import de.upb.soot.jimple.common.type.VoidType;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*-
  * #%L
  * Soot - a J*va Optimization Framework
@@ -25,20 +28,17 @@ import de.upb.soot.jimple.common.type.VoidType;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Generates locals for Body.
@@ -68,94 +68,93 @@ public class LocalGenerator {
   }
 
   /**
-   * generates a new soot local given the type
+   * generates a new @Local given the type for field.
+   */
+  public Local generateField(Type type) {
+    return generate(type, true);
+  }
+
+  /**
+   * generates a new @Local given the type for local.
    */
   public Local generateLocal(Type type) {
+    return generate(type, false);
+  }
+
+  private Local generate(Type type, boolean isField) {
 
     // store local names for enhanced performance
     initLocalNames();
 
     String name = "v";
+
     if (type instanceof IntType) {
-      while (true) {
-        name = nextIntName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextIntName();
+      } while (bodyContainsLocal(name));
     } else if (type instanceof ByteType) {
-      while (true) {
-        name = nextByteName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextByteName();
+      } while (bodyContainsLocal(name));
+
     } else if (type instanceof ShortType) {
-      while (true) {
-        name = nextShortName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextShortName();
+      } while (bodyContainsLocal(name));
+
     } else if (type instanceof BooleanType) {
-      while (true) {
-        name = nextBooleanName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextBooleanName();
+      } while (bodyContainsLocal(name));
+
     } else if (type instanceof VoidType) {
-      while (true) {
-        name = nextVoidName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextVoidName();
+      } while (bodyContainsLocal(name));
+
     } else if (type instanceof CharType) {
-      while (true) {
-        name = nextCharName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextCharName();
+      } while (bodyContainsLocal(name));
+
+      // TODO: anomalie detected - needed?
       type = CharType.getInstance();
+
     } else if (type instanceof DoubleType) {
-      while (true) {
-        name = nextDoubleName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextDoubleName();
+      } while (bodyContainsLocal(name));
+
     } else if (type instanceof FloatType) {
-      while (true) {
-        name = nextFloatName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextFloatName();
+      } while (bodyContainsLocal(name));
     } else if (type instanceof LongType) {
-      while (true) {
-        name = nextLongName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextLongName();
+      } while (bodyContainsLocal(name));
     } else if (type instanceof RefLikeType) {
-      while (true) {
-        name = nextRefLikeTypeName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+      do {
+        name = isField ? "" : "$";
+        name += nextRefLikeTypeName();
+      } while (bodyContainsLocal(name));
     } else if (type instanceof UnknownType) {
-      while (true) {
-        name = nextUnknownTypeName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
+
+      do {
+        name = isField ? "" : "$";
+        name += nextUnknownTypeName();
+      } while (bodyContainsLocal(name));
+
     } else {
-      localNames = null;
       throw new RuntimeException("Unhandled Type of Local variable to Generate - Not Implemented");
     }
 
@@ -177,57 +176,57 @@ public class LocalGenerator {
 
   private String nextIntName() {
     tempInt++;
-    return "$i" + tempInt;
+    return "i" + tempInt;
   }
 
   private String nextCharName() {
     tempChar++;
-    return "$c" + tempChar;
+    return "c" + tempChar;
   }
 
   private String nextVoidName() {
     tempVoid++;
-    return "$v" + tempVoid;
+    return "v" + tempVoid;
   }
 
   private String nextByteName() {
     tempByte++;
-    return "$b" + tempByte;
+    return "b" + tempByte;
   }
 
   private String nextShortName() {
     tempShort++;
-    return "$s" + tempShort;
+    return "s" + tempShort;
   }
 
   private String nextBooleanName() {
     tempBoolean++;
-    return "$z" + tempBoolean;
+    return "z" + tempBoolean;
   }
 
   private String nextDoubleName() {
     tempDouble++;
-    return "$d" + tempDouble;
+    return "d" + tempDouble;
   }
 
   private String nextFloatName() {
     tempFloat++;
-    return "$f" + tempFloat;
+    return "f" + tempFloat;
   }
 
   private String nextLongName() {
     tempLong++;
-    return "$l" + tempLong;
+    return "l" + tempLong;
   }
 
   private String nextRefLikeTypeName() {
     tempRefLikeType++;
-    return "$r" + tempRefLikeType;
+    return "r" + tempRefLikeType;
   }
 
   private String nextUnknownTypeName() {
     tempUnknownType++;
-    return "$u" + tempUnknownType;
+    return "u" + tempUnknownType;
   }
 
   // this should be used for generated locals only
