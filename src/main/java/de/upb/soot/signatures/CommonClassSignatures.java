228dcfadb41c44cff20416a09ff29e02cec40b08
Christian Brüggemann <mail@cbruegg.com>
2019-02-12 13:04:18 +0100

    Remove unneeded static initializer

--- src/main/java/de/upb/soot/signatures/CommonClassSignatures.java
+++ src/main/java/de/upb/soot/signatures/CommonClassSignatures.java
@@ -7,10 +7,7 @@ package de.upb.soot.signatures;
  * 
  */
 public final class CommonClassSignatures {
-  private static final SignatureFactory factory;
-  static {
-    factory = new DefaultSignatureFactory();
-  }
+  private static final SignatureFactory factory = new DefaultSignatureFactory();
 
   public static final JavaClassSignature JavaLangObject = factory.getClassSignature("Object", "java.lang");
 }
