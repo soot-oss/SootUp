07448827ded70a8a860da932d5ad7fbaa70e9a30
Markus Schmidt <markus262@web.de>
2019-02-20 00:59:05 +0100

    simplified

--- src/main/java/de/upb/soot/util/EscapedWriter.java
+++ src/main/java/de/upb/soot/util/EscapedWriter.java
@@ -10,12 +10,12 @@ package de.upb.soot.util;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
@@ -31,7 +31,6 @@ import java.io.Writer;
  * representation. Used for Soot output.
  */
 
-// TODO: check code copied from old soot
 public class EscapedWriter extends FilterWriter {
   /** Convenience field containing the system's line separator. */
   public final String lineSeparator = System.getProperty("line.separator");
@@ -43,8 +42,6 @@ public class EscapedWriter extends FilterWriter {
     super(fos);
   }
 
-  private final StringBuilder mini = new StringBuilder();
-
   /** Print a single character (unsupported). */
   public void print(int ch) throws IOException {
     write(ch);
@@ -52,31 +49,34 @@ public class EscapedWriter extends FilterWriter {
   }
 
   /** Write a segment of the given String. */
-  @Override
   public void write(String s, int off, int len) throws IOException {
-    for (int i = off; i < off + len; i++) {
+    // PTODO: possible Performance+: less calls/loopbranch/synchronizedcalls/ -> sequence of Strings to super.write(String, start, len).
+    final int end = off + len;
+    for (int i = off; i < end; i++) {
       write(s.charAt(i));
     }
   }
 
+  private final StringBuilder mini = new StringBuilder(8);
+
+  private boolean isClean( int ch ){
+      return ch >= 32 && ch <= 126 || ch == cr || ch == lf;
+  }
+
   /** Write a single character. */
-  @Override
   public void write(int ch) throws IOException {
-    if (ch >= 32 && ch <= 126 || ch == cr || ch == lf) {
+    if ( isClean(ch) ) {
       super.write(ch);
       return;
     }
 
     mini.setLength(0);
     mini.append(Integer.toHexString(ch));
+    final int len = mini.length();
 
-    while (mini.length() < 4) {
-      mini.insert(0, "0");
-    }
-
-    mini.insert(0, "\\u");
-    for (int i = 0; i < mini.length(); i++) {
-      super.write(mini.charAt(i));
-    }
+    // prepend \\u + [0]{0,4} to hex string so it writes "\\u" + "minimum 4 alphanumeric chars and max. 8"
+    int cutPos = ( len < 4 )? 2+4-len : 2;
+    super.write("\\u0000", 0, cutPos);
+    super.write(mini.toString(), 0, len);
   }
 }
