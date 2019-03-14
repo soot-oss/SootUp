8e08803fbbd7eb66201f058b780378aa2d591b24
Linghui Luo <linghui@mail.upb.de>
2019-03-14 18:54:20 +0100

    add outputJimple method to Utils

--- src/test/java/de/upb/soot/frontends/java/Utils.java
+++ src/test/java/de/upb/soot/frontends/java/Utils.java
@@ -3,26 +3,48 @@ package de.upb.soot.frontends.java;
 import de.upb.soot.core.SootClass;
 import de.upb.soot.core.SootMethod;
 import de.upb.soot.util.printer.Printer;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 /** @author Linghui Luo */
 public class Utils {
 
-  public static void print(SootClass cl, boolean print) {
-    PrintWriter writer = new PrintWriter(System.out);
-    Printer printer = new Printer();
-    printer.printTo(cl, writer);
+  public static void outputJimple(SootClass cl, boolean print) {
     if (print) {
+      File outputDir = new File("jimpleOutput");
+      if (!outputDir.exists()) {
+        outputDir.mkdir();
+      }
+      File file = new File(outputDir + File.separator + cl.getName() + ".jimple");
+      PrintWriter writer;
+      try {
+        writer = new PrintWriter(file);
+        Printer printer = new Printer();
+        printer.printTo(cl, writer);
+        writer.flush();
+        writer.close();
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void print(SootClass cl, boolean print) {
+    if (print) {
+      PrintWriter writer = new PrintWriter(System.out);
+      Printer printer = new Printer();
+      printer.printTo(cl, writer);
       writer.flush();
       writer.close();
     }
   }
 
-  public static void print(SootMethod method, boolean flush) {
-    PrintWriter writer = new PrintWriter(System.out);
-    Printer printer = new Printer();
-    printer.printTo(method.getActiveBody(), writer);
-    if (flush) {
+  public static void print(SootMethod method, boolean print) {
+    if (print) {
+      PrintWriter writer = new PrintWriter(System.out);
+      Printer printer = new Printer();
+      printer.printTo(method.getActiveBody(), writer);
       writer.flush();
       writer.close();
     }
