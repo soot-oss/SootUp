6ed84cb5a1d9f878ceb7905af642b872039e00e1
Jan Martin Persch <jmpersch@hni.uni-paderborn.de>
2019-03-21 18:05:31 +0100

    Added Unit tests for `JavaView` class

---
+++ src/test/java/de/upb/soot/views/JavaViewTest.java
@@ -0,0 +1,122 @@
+package de.upb.soot.views;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import categories.Java8Test;
+import de.upb.soot.Project;
+import de.upb.soot.core.AbstractClass;
+import de.upb.soot.frontends.ClassSource;
+import de.upb.soot.namespaces.JavaClassPathNamespace;
+import de.upb.soot.signatures.DefaultSignatureFactory;
+import de.upb.soot.signatures.ISignature;
+import de.upb.soot.signatures.JavaClassSignature;
+import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Defines tests for the {@link JavaView} class.
+ *
+ * @author Jan Martin Persch
+ */
+@Category(Java8Test.class)
+public class JavaViewTest {
+
+  private List<JavaClassSignature> signatures;
+  private JavaView view;
+
+  @Before
+  public void initialize() {
+    String jarFile = "target/test-classes/de/upb/soot/namespaces/Soot-4.0-SNAPSHOT.jar";
+
+    assertTrue(new File(jarFile).exists());
+
+    JavaClassPathNamespace namespace = new JavaClassPathNamespace(jarFile);
+
+    this.signatures =
+        Collections.unmodifiableList(
+            namespace.getClassSources(DefaultSignatureFactory.getInstance()).stream()
+                .map(ClassSource::getClassSignature)
+                .sorted(Comparator.comparing(JavaClassSignature::toString))
+                .collect(Collectors.toList()));
+
+    Project project = new Project(namespace);
+
+    this.view = new JavaView(project);
+  }
+
+  @Test
+  public void testIsResolvedPropertyBeforeAll() {
+    assertFalse(this.view.isEntirelyResolved());
+  }
+
+  @Test
+  public void testIsResolvedPropertyAfterOne() {
+    assertTrue(this.view.getClass(this.signatures.get(0)).isPresent());
+    assertFalse(this.view.isEntirelyResolved());
+  }
+
+  @Test
+  public void testIsResolvedPropertyAfterAll() {
+    this.view.resolveAll();
+    assertTrue(this.view.isEntirelyResolved());
+  }
+
+  @Test
+  public void testResolveIteratively() {
+    this.signatures.forEach(
+        it -> {
+          AbstractClass clazz = this.view.getClass(it).orElse(null);
+          assertNotNull("Class for signature \"" + it + "\" not found.", clazz);
+          assertEquals(it, clazz.getSignature());
+        });
+  }
+
+  private void resolveUndefinedClass() {
+    JavaClassSignature signature =
+        DefaultSignatureFactory.getInstance().getClassSignature("com.example.NonExistingClass");
+
+    if (this.signatures.contains(signature)) {
+      Assert.fail("FATAL ERROR: Non-existing class exists in signature list!");
+    }
+
+    assertFalse(this.view.getClass(signature).isPresent());
+  }
+
+  @Test
+  public void testResolveUndefinedClassBeforeAllResolved() {
+    this.resolveUndefinedClass();
+  }
+
+  @Test
+  public void testResolveUndefinedClassAfterAllResolved() {
+    this.view.resolveAll();
+    this.resolveUndefinedClass();
+  }
+
+  @Test
+  public void testResolveAll() {
+    this.view.resolveAll();
+
+    Collection<AbstractClass> classes = this.view.getClasses();
+
+    assertEquals(classes.size(), this.signatures.size());
+
+    assertEquals(
+        classes.stream()
+            .map(AbstractClass::getSignature)
+            .sorted(Comparator.comparing(ISignature::toString))
+            .collect(Collectors.toList()),
+        this.signatures);
+  }
+}
