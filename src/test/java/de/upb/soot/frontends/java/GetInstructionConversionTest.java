5f6db3ba0868f5a8b6df597ef380746f35239a50
Christian Brüggemann <mail@cbruegg.com>
2019-04-08 12:25:00 +0200

    Implement GetInstructionConversionTest.test

--- src/test/java/de/upb/soot/frontends/java/GetInstructionConversionTest.java
+++ src/test/java/de/upb/soot/frontends/java/GetInstructionConversionTest.java
@@ -1,12 +1,29 @@
 package de.upb.soot.frontends.java;
 
+import static de.upb.soot.frontends.java.Utils.assertEquiv;
+import static de.upb.soot.frontends.java.Utils.assertInstanceOfSatisfying;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import categories.Java8Test;
+import de.upb.soot.core.Body;
 import de.upb.soot.core.SootMethod;
+import de.upb.soot.jimple.Jimple;
+import de.upb.soot.jimple.basic.Local;
+import de.upb.soot.jimple.common.ref.JInstanceFieldRef;
+import de.upb.soot.jimple.common.stmt.IStmt;
+import de.upb.soot.jimple.common.stmt.JAssignStmt;
+import de.upb.soot.jimple.common.stmt.JIdentityStmt;
+import de.upb.soot.jimple.common.stmt.JReturnStmt;
+import de.upb.soot.jimple.common.type.IntType;
+import de.upb.soot.jimple.common.type.RefType;
 import de.upb.soot.signatures.DefaultSignatureFactory;
+import de.upb.soot.signatures.FieldSignature;
 import de.upb.soot.signatures.JavaClassSignature;
+import de.upb.soot.signatures.PrimitiveTypeSignature;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,7 +53,53 @@ public class GetInstructionConversionTest {
                 "getA_X", declareClassSig, "int", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<IStmt> stmts = body.getStmts();
+    assertEquals(3, stmts.size());
+
+    assertInstanceOfSatisfying(
+        stmts.get(0),
+        JIdentityStmt.class,
+        stmt -> {
+          assertEquiv(
+              new Local("r0", RefType.getInstance("alreadywalaunittests.InnerClassAA")),
+              stmt.getLeftOp());
+          assertEquiv(
+              Jimple.newThisRef(RefType.getInstance("alreadywalaunittests.InnerClassAA")),
+              stmt.getRightOp());
+        });
+
+    assertInstanceOfSatisfying(
+        stmts.get(1),
+        JAssignStmt.class,
+        stmt -> {
+          assertEquiv(new Local("$i0", IntType.getInstance()), stmt.getLeftOp());
+          assertInstanceOfSatisfying(
+              stmt.getRightOp(),
+              JInstanceFieldRef.class,
+              fieldRef -> {
+                assertEquiv(
+                    new Local("r0", RefType.getInstance("alreadywalaunittests.InnerClassAA")),
+                    fieldRef.getBase());
+
+                FieldSignature fieldSig = fieldRef.getFieldSignature();
+                assertNotNull(fieldSig);
+                assertEquals("a_x", fieldSig.getName());
+                assertEquals(
+                    PrimitiveTypeSignature.INT_TYPE_SIGNATURE, fieldSig.getTypeSignature());
+                assertEquals(
+                    new DefaultSignatureFactory()
+                        .getClassSignature("alreadywalaunittests.InnerClassAA"),
+                    fieldSig.getDeclClassSignature());
+              });
+        });
+
+    assertInstanceOfSatisfying(
+        stmts.get(2),
+        JReturnStmt.class,
+        stmt -> assertEquiv(new Local("$i0", IntType.getInstance()), stmt.getOp()));
   }
 }
