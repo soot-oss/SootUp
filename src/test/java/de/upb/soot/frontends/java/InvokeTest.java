86e87f7c712c3518dd2fcd9ad4874b420b3065a5
Christian Brüggemann <mail@cbruegg.com>
2019-04-16 20:12:15 +0200

    Add InvokeTest

--- src/test/java/de/upb/soot/frontends/java/InvokeTest.java
+++ src/test/java/de/upb/soot/frontends/java/InvokeTest.java
@@ -1,14 +1,22 @@
 package de.upb.soot.frontends.java;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import categories.Java8Test;
+import de.upb.soot.core.Body;
 import de.upb.soot.core.SootMethod;
+import de.upb.soot.jimple.common.stmt.IStmt;
 import de.upb.soot.signatures.DefaultSignatureFactory;
 import de.upb.soot.signatures.JavaClassSignature;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -36,8 +44,25 @@ public class InvokeTest {
                 "specialInvokeInstanceInit", declareClassSig, "void", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeSpecial",
+                "$r1 = new java.util.ArrayList",
+                "specialinvoke $r1.<java.util.ArrayList: void <init>()>()",
+                "$z0 = virtualinvoke $r1.<java.util.ArrayList: boolean add(java.lang.Object)>(\"item1\")",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -49,8 +74,23 @@ public class InvokeTest {
                 "specialInvokePrivateMethod", declareClassSig, "void", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeSpecial",
+                "$i0 = specialinvoke r0.<InvokeSpecial: int privateMethod()>()",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -65,8 +105,23 @@ public class InvokeTest {
                 Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeSpecial",
+                "$r1 = specialinvoke r0.<java.lang.Object: java.lang.String toString()>()",
+                "return $r1")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -78,8 +133,25 @@ public class InvokeTest {
                 "<clinit>", declareClassSig, "void", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "$r0 = new java.lang.String",
+                "specialinvoke $r0.<java.lang.String: void <init>()>()",
+                "<InvokeStatic: java.lang.String string> = $r0",
+                "<InvokeStatic: java.lang.String x> = \"abc\"",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -94,8 +166,24 @@ public class InvokeTest {
                 Arrays.asList("int", "java.lang.String", "boolean")));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "$i0 := @parameter0: int",
+                "$r0 := @parameter1: java.lang.String",
+                "$z0 := @parameter2: boolean",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -107,8 +195,24 @@ public class InvokeTest {
                 "repro1", declareClassSig, "void", Arrays.asList("java.lang.Object")));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "$r0 := @parameter0: java.lang.Object",
+                "$r1 = $r0",
+                "$r2 = virtualinvoke $r1.<java.lang.Object: java.lang.String toString()>()",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -120,8 +224,35 @@ public class InvokeTest {
                 "repro2", declareClassSig, "void", Arrays.asList("java.lang.Object")));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "$r0 := @parameter0: java.lang.Object",
+                "$r1 = \"\"",
+                "$r2 = virtualinvoke $r1.<java.lang.Object: java.lang.String toString()>()",
+                "$r3 = \"A\"",
+                "$r4 = \"B\"",
+                "$z0 = $r3 == $r4",
+                "$z0 == 0 goto $z1 = 5 < 3",
+                "return",
+                "$z1 = 5 < 3",
+                "$z1 == 0 goto $z2 = 5.0 < 3.0",
+                "return",
+                "$z2 = 5.0 < 3.0",
+                "if $z2 == 0 goto return",
+                "return",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -133,8 +264,26 @@ public class InvokeTest {
                 "equals", declareClassSig, "boolean", Collections.singletonList("InvokeVirtual")));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeVirtual",
+                "$r1 := @parameter0: InvokeVirtual",
+                "$r2 = r0.<InvokeVirtual: java.lang.String x>",
+                "$r3 = r0.<InvokeVirtual: java.lang.String x>",
+                "$z0 = virtualinvoke $r2.<java.lang.String: boolean equals(java.lang.Object)>($r3)",
+                "return $z0")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -146,8 +295,24 @@ public class InvokeTest {
                 "interfaceMethod", declareClassSig, "void", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeVirtual",
+                "$r1 = <java.lang.System: java.io.PrintStream out>",
+                "virtualinvoke $r1.<java.io.PrintStream: void println(java.lang.String)>(\"abc\")",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 
   @Test
@@ -159,7 +324,22 @@ public class InvokeTest {
                 "doStuf", declareClassSig, "void", Collections.emptyList()));
     assertTrue(m.isPresent());
     SootMethod method = m.get();
-    // TODO. replace the next line with assertions.
-    Utils.print(method, false);
+
+    Body body = method.getActiveBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(IStmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InvokeVirtual",
+                "virtualinvoke r0.<InvokeVirtual: void interfaceMethod()>()",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
   }
 }
