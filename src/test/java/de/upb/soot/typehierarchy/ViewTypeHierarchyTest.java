91ca30793439f786986641865b2fda6a48ad7299
Christian Brüggemann <mail@cbruegg.com>
2019-07-15 12:51:22 +0200

    Implement all tests for TypeHierarchy

--- src/test/java/de/upb/soot/typehierarchy/ViewTypeHierarchyTest.java
+++ src/test/java/de/upb/soot/typehierarchy/ViewTypeHierarchyTest.java
@@ -1,5 +1,6 @@
 package de.upb.soot.typehierarchy;
 
+import static de.upb.soot.util.ImmutableUtils.immutableList;
 import static de.upb.soot.util.ImmutableUtils.immutableSet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,19 +14,21 @@ import de.upb.soot.views.View;
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(Java8Test.class)
 public class ViewTypeHierarchyTest {
 
-  private View view;
+  private static View view;
+  private static ViewTypeHierarchy typeHierarchy;
 
-  @Before
-  public void setup() {
+  @BeforeClass
+  public static void setup() {
     String jarFile = "target/test-classes/de/upb/soot/namespaces/Soot-4.0-SNAPSHOT.jar";
     assertTrue(new File(jarFile).exists());
     String currentClassPath =
@@ -41,11 +44,11 @@ public class ViewTypeHierarchyTest {
         new Project<>(
             new JavaClassPathAnalysisInputLocation(jarFile + File.pathSeparator + rtJarClassPath));
     view = p.createOnDemandView();
+    typeHierarchy = new ViewTypeHierarchy(view);
   }
 
   @Test
   public void implementersOf() {
-    TypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
     IdentifierFactory factory = view.getIdentifierFactory();
     JavaClassType iNamespace = factory.getClassType("INamespace", "de.upb.soot.namespaces");
     Set<JavaClassType> implementers = typeHierarchy.implementersOf(iNamespace);
@@ -61,14 +64,52 @@ public class ViewTypeHierarchyTest {
   }
 
   @Test
-  public void subclassesOf() {}
+  public void subclassesOf() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    JavaClassType abstractNamespace =
+        factory.getClassType("AbstractNamespace", "de.upb.soot.namespaces");
+    Set<JavaClassType> subclasses = typeHierarchy.subclassesOf(abstractNamespace);
+    assertEquals(
+        immutableSet(
+            factory.getClassType("de.upb.soot.namespaces.PathBasedNamespace$ArchiveBasedNamespace"),
+            factory.getClassType("de.upb.soot.namespaces.PathBasedNamespace"),
+            factory.getClassType("de.upb.soot.namespaces.JavaClassPathNamespace"),
+            factory.getClassType(
+                "de.upb.soot.namespaces.PathBasedNamespace$DirectoryBasedNamespace")),
+        subclasses);
+  }
 
   @Test
-  public void implementedInterfacesOf() {}
+  public void implementedInterfacesOf() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    JavaClassType javaClassPathNamespace =
+        factory.getClassType("JavaClassPathNamespace", "de.upb.soot.namespaces");
+    Set<JavaClassType> implementedInterfaces =
+        typeHierarchy.implementedInterfacesOf(javaClassPathNamespace);
+    assertEquals(
+        immutableSet(factory.getClassType("de.upb.soot.namespaces.INamespace")),
+        implementedInterfaces);
+  }
 
   @Test
-  public void superClassOf() {}
+  public void superClassOf() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    JavaClassType javaClassPathNamespace =
+        factory.getClassType("JavaClassPathNamespace", "de.upb.soot.namespaces");
+    JavaClassType superClass = typeHierarchy.superClassOf(javaClassPathNamespace);
+    assertEquals(factory.getClassType("de.upb.soot.namespaces.AbstractNamespace"), superClass);
+  }
 
   @Test
-  public void superClassesOf() {}
+  public void superClassesOf() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    JavaClassType javaClassPathNamespace =
+        factory.getClassType("JavaClassPathNamespace", "de.upb.soot.namespaces");
+    List<JavaClassType> superClasses = typeHierarchy.superClassesOf(javaClassPathNamespace);
+    assertEquals(
+        immutableList(
+            factory.getClassType("de.upb.soot.namespaces.AbstractNamespace"),
+            factory.getClassType("java.lang.Object")),
+        superClasses);
+  }
 }
