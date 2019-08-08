112124a49ba4b99be6551567be927c6a9d26996c
Markus Schmidt <markus262@web.de>
2019-08-08 12:20:51 +0200

    get invoked class/method via view

--- src/main/java/de/upb/soot/callgraph/ClassHierarchyAlgorithm.java
+++ src/main/java/de/upb/soot/callgraph/ClassHierarchyAlgorithm.java
@@ -7,6 +7,7 @@ import de.upb.soot.jimple.common.expr.AbstractInvokeExpr;
 import de.upb.soot.jimple.common.stmt.JAssignStmt;
 import de.upb.soot.jimple.common.stmt.JInvokeStmt;
 import de.upb.soot.jimple.common.stmt.Stmt;
+import de.upb.soot.views.View;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ public class ClassHierarchyAlgorithm implements CallGraphAlgorithm {
 
   Hierarchy hierarchy;
   CallGraph callGraph;
+  View v;
 
   @Override
   public CallGraph build(List<SootMethod> entryPoints, Hierarchy hierarchy) {
@@ -49,7 +51,10 @@ public class ClassHierarchyAlgorithm implements CallGraphAlgorithm {
   }
 
   private void handleInvokeExpression(SootMethod method, AbstractInvokeExpr invokeExpr) {
-    SootMethod calledMethod = invokeExpr.getMethod(); // TODO: how to? getMethodSignature() via View
+
+    SootClass calledMethodClass = (SootClass) v.getClass(invokeExpr.getMethodSignature().getDeclClassType()).get();
+    SootMethod calledMethod = calledMethodClass.getMethod(invokeExpr.getMethodSignature()).get();
+
     if (calledMethod.isStatic()) {
       if (!callGraph.hasNode(calledMethod)) {
         callGraph.addNode(calledMethod);
@@ -60,11 +65,10 @@ public class ClassHierarchyAlgorithm implements CallGraphAlgorithm {
         analyzeMethod(method);
       }
     } else {
-      SootClass calledMethodClass = calledMethod.getDeclaringClass(); // TODO: how to? via View
       Stream<SootClass> subclasses =
           calledMethodClass.isInterface()
-              ? hierarchy.getImplementersOf(calledMethodClass).stream()
-              : hierarchy.getSubclassesOf(calledMethodClass).stream();
+              ? hierarchy.implementersOf(calledMethodClass).stream()
+              : hierarchy.subclassesOf(calledMethodClass).stream();
 
       List<SootMethod> targets =
           new ArrayList<>(
