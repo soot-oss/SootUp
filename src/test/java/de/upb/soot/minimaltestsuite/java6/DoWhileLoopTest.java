6828d8674221625c8ebbd7b7c7846a671b20c51f
hasitha <herajapakse@gmail.com>
2019-08-12 09:01:05 +0200

    DoWhileLoopTest

--- src/test/java/de/upb/soot/minimaltestsuite/java6/DoWhileLoopTest.java
+++ src/test/java/de/upb/soot/minimaltestsuite/java6/DoWhileLoopTest.java
@@ -1,5 +1,7 @@
 package de.upb.soot.minimaltestsuite.java6;
 
+import static org.junit.Assert.*;
+
 import categories.Java8Test;
 import de.upb.soot.core.Body;
 import de.upb.soot.core.SootMethod;
@@ -7,65 +9,62 @@ import de.upb.soot.frontends.java.Utils;
 import de.upb.soot.frontends.java.WalaClassLoaderTestUtils;
 import de.upb.soot.jimple.common.stmt.Stmt;
 import de.upb.soot.minimaltestsuite.LoadClassesWithWala;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 @Category(Java8Test.class)
 public class DoWhileLoopTest {
 
-    private String srcDir = "src/test/resources/minimaltestsuite/java6/";
-    private String className = "DoWhileLoop";
-    private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
+  private String srcDir = "src/test/resources/minimaltestsuite/java6/";
+  private String className = "DoWhileLoop";
+  private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
 
-    @Before
-    public void loadClasses() {
-        loadClassesWithWala.classLoader(srcDir, className);
-    }
+  @Before
+  public void loadClasses() {
+    loadClassesWithWala.classLoader(srcDir, className);
+  }
 
-    @Test
-    public void shortVariableTest() {
-        Optional<SootMethod> m =
-                WalaClassLoaderTestUtils.getSootMethod(
-                        loadClassesWithWala.loader,
-                        loadClassesWithWala.identifierFactory.getMethodSignature(
-                                "doWhileLoop",
-                                loadClassesWithWala.declareClassSig,
-                                "int",
-                                Collections.singletonList("int")));
-        assertTrue(m.isPresent());
-        SootMethod method = m.get();
-        Utils.print(method, false);
-        Body body = method.getBody();
-        assertNotNull(body);
+  @Test
+  public void shortVariableTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "doWhileLoop",
+                loadClassesWithWala.declareClassSig,
+                "int",
+                Collections.singletonList("int")));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
 
-        List<String> actualStmts =
-                body.getStmts().stream()
-                        .map(Stmt::toString)
-                        .collect(Collectors.toCollection(ArrayList::new));
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
 
-        List<String> expectedStmts =
-                Stream.of(
-                        "r0 := @this: DoWhileLoop",
-                        "$i0 := @parameter0: int",
-                        "$i1 = 0",
-                        "$i2 = $i1",
-                        "$i3 = $i1 + 1",
-                        "$i1 = $i3",
-                        "$z0 = $i0 > $i1",
-                        "if $z0 != 0 goto $i2 = $i1",
-                        "return $i1")
-                        .collect(Collectors.toCollection(ArrayList::new));
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: DoWhileLoop",
+                "$i0 := @parameter0: int",
+                "$i1 = 0",
+                "$i2 = $i1",
+                "$i3 = $i1 + 1",
+                "$i1 = $i3",
+                "$z0 = $i0 > $i1",
+                "if $z0 != 0 goto $i2 = $i1",
+                "return $i1")
+            .collect(Collectors.toCollection(ArrayList::new));
 
-        assertEquals(expectedStmts, actualStmts);
-    }
+    assertEquals(expectedStmts, actualStmts);
+  }
 }
