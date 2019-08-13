bc4d6a7b414b9d7048e41e1ed954ae3b5b4df4ea
Christian Brüggemann <mail@cbruegg.com>
2019-08-13 17:32:03 +0200

    Add internal use warning

--- src/main/java/de/upb/soot/typehierarchy/$ViewTypeHierarchyAccessor.java
+++ src/main/java/de/upb/soot/typehierarchy/$ViewTypeHierarchyAccessor.java
@@ -3,14 +3,20 @@ package de.upb.soot.typehierarchy;
 import de.upb.soot.views.View;
 
 /**
- * To avoid leaking the {@link ViewTypeHierarchy} constructor as an implementation detail to the
+ * <b>For internal use only.</b>
+ *
+ * <p>To avoid leaking the {@link ViewTypeHierarchy} constructor as an implementation detail to the
  * public API, we only allow its instantiation through this class. It is deliberately cryptically
  * named. Soot users are not supposed to instantiate this class, as it is managed by the View.
  */
 public final class $ViewTypeHierarchyAccessor {
   private $ViewTypeHierarchyAccessor() {}
 
-  /** Calls the constructor of {@link ViewTypeHierarchy}. */
+  /**
+   * <b>For internal use only.</b>
+   *
+   * <p>Calls the constructor of {@link ViewTypeHierarchy}.
+   */
   public static ViewTypeHierarchy createViewTypeHierarchy(View view) {
     return new ViewTypeHierarchy(view);
   }
