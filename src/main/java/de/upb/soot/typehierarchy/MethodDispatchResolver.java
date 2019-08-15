2236b97fc418873ac97284af6d85ff605fdaeb14
Christian Brüggemann <mail@cbruegg.com>
2019-08-15 13:04:12 +0200

    Add method dispatch feature and tests

---
+++ src/main/java/de/upb/soot/typehierarchy/MethodDispatchResolver.java
@@ -0,0 +1,98 @@
+package de.upb.soot.typehierarchy;
+
+import de.upb.soot.core.AbstractClass;
+import de.upb.soot.core.Method;
+import de.upb.soot.core.SootMethod;
+import de.upb.soot.frontends.AbstractClassSource;
+import de.upb.soot.frontends.ResolveException;
+import de.upb.soot.jimple.common.expr.JSpecialInvokeExpr;
+import de.upb.soot.signatures.MethodSignature;
+import de.upb.soot.types.JavaClassType;
+import de.upb.soot.views.View;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+
+public final class MethodDispatchResolver {
+  private MethodDispatchResolver() {}
+
+  /**
+   * Searches the view for classes that implement or override the method <code>m</code> and returns
+   * the set of method signatures that a method call could resolve to.
+   */
+  @Nonnull
+  public static Set<MethodSignature> resolveAbstractDispatch(View view, MethodSignature m) {
+    TypeHierarchy hierarchy = view.typeHierarchy();
+
+    return hierarchy.subtypesOf(m.getDeclClassType()).stream()
+        .map(
+            subtype ->
+                view.getClass(subtype)
+                    .orElseThrow(
+                        () ->
+                            new ResolveException(
+                                "Could not resolve " + subtype + ", but found it in hierarchy.")))
+        .map(subtype -> subtype.getMethod(m.getSubSignature()))
+        .filter(Optional::isPresent)
+        .map(method -> method.get().getSignature())
+        .collect(Collectors.toSet());
+  }
+
+  /**
+   * Searches for the signature of the method that is the concrete implementation of <code>m</code>.
+   * This is done by checking each superclass and the class itself for whether it contains the
+   * concrete implementation.
+   */
+  @Nonnull
+  public static MethodSignature resolveConcreteDispatch(View view, MethodSignature m) {
+    TypeHierarchy hierarchy = view.typeHierarchy();
+    JavaClassType superClassType = m.getDeclClassType();
+    do {
+      JavaClassType finalSuperClassType = superClassType;
+      AbstractClass<? extends AbstractClassSource> superClass =
+          view.getClass(superClassType)
+              .orElseThrow(
+                  () ->
+                      new ResolveException(
+                          "Did not find class " + finalSuperClassType + " in View"));
+
+      Method concreteMethod = superClass.getMethod(m.getSubSignature()).orElse(null);
+      if (concreteMethod instanceof SootMethod && !((SootMethod) concreteMethod).isAbstract()) {
+        return concreteMethod.getSignature();
+      }
+
+      superClassType = hierarchy.superClassOf(superClassType);
+    } while (superClassType != null);
+
+    throw new ResolveException("Could not find concrete method for " + m);
+  }
+
+  /**
+   * Resolves the actual method called by the <code>specialInvokeExpr</code> that is contained by
+   * <code>container</code>.
+   */
+  @Nonnull
+  public static MethodSignature resolveSpecialDispatch(
+      View view, JSpecialInvokeExpr specialInvokeExpr, MethodSignature container) {
+    MethodSignature specialMethodSig = specialInvokeExpr.getMethodSignature();
+    if (specialMethodSig.getSubSignature().getName().equals("<init>")) {
+      return specialMethodSig;
+    }
+
+    Method specialMethod =
+        view.getClass(specialMethodSig.getDeclClassType())
+            .flatMap(cl -> cl.getMethod(specialMethodSig))
+            .orElse(null);
+    if (specialMethod instanceof SootMethod && ((SootMethod) specialMethod).isPrivate()) {
+      return specialMethodSig;
+    }
+
+    if (view.typeHierarchy()
+        .isSubtype(container.getDeclClassType(), specialMethodSig.getDeclClassType())) {
+      return resolveConcreteDispatch(view, specialMethodSig);
+    }
+
+    return specialMethodSig;
+  }
+}
---
+++ src/test/java/de/upb/soot/typehierarchy/MethodDispatchResolverTest.java
@@ -0,0 +1,124 @@
+package de.upb.soot.typehierarchy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import de.upb.soot.IdentifierFactory;
+import de.upb.soot.Project;
+import de.upb.soot.frontends.ResolveException;
+import de.upb.soot.inputlocation.JavaClassPathAnalysisInputLocation;
+import de.upb.soot.jimple.basic.Local;
+import de.upb.soot.jimple.common.constant.StringConstant;
+import de.upb.soot.jimple.common.expr.JSpecialInvokeExpr;
+import de.upb.soot.signatures.MethodSignature;
+import de.upb.soot.util.ImmutableUtils;
+import de.upb.soot.views.View;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MethodDispatchResolverTest {
+
+  private View view;
+
+  @Before
+  public void setUp() {
+    String jarFile = "target/test-classes/de/upb/soot/namespaces/Soot-4.0-SNAPSHOT.jar";
+    assertTrue(new File(jarFile).exists());
+    String currentClassPath =
+        System.getProperty("java.class.path")
+            + File.pathSeparator
+            + ManagementFactory.getRuntimeMXBean().getBootClassPath();
+    String rtJarClassPath =
+        Arrays.stream(currentClassPath.split(File.pathSeparator))
+            .filter(pathEntry -> pathEntry.endsWith(File.separator + "rt.jar"))
+            .distinct()
+            .collect(Collectors.joining(File.pathSeparator));
+    JavaClassPathAnalysisInputLocation analysisInputLocation =
+        new JavaClassPathAnalysisInputLocation(jarFile + File.pathSeparator + rtJarClassPath);
+    Project<JavaClassPathAnalysisInputLocation> p = new Project<>(analysisInputLocation);
+    view = p.createOnDemandView();
+  }
+
+  @Test
+  public void resolveAbstractDispatch() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    MethodSignature collectionSize =
+        factory.parseMethodSignature("java.util.Collection#size(): int");
+    MethodSignature abstractListSize =
+        factory.parseMethodSignature("java.util.AbstractList#size(): int");
+    MethodSignature setSize = factory.parseMethodSignature("java.util.HashSet#size(): int");
+    MethodSignature listSize = factory.parseMethodSignature("java.util.ArrayList#size(): int");
+
+    Set<MethodSignature> candidates =
+        MethodDispatchResolver.resolveAbstractDispatch(view, collectionSize);
+    assertTrue(collectionSize + " can resolve to " + setSize, candidates.contains(setSize));
+    assertTrue(collectionSize + " can resolve to " + listSize, candidates.contains(listSize));
+
+    assertTrue(
+        abstractListSize + " can resolve to " + listSize,
+        MethodDispatchResolver.resolveAbstractDispatch(view, abstractListSize).contains(listSize));
+  }
+
+  @Test(expected = ResolveException.class)
+  public void invalidResolveConcreteDispatch() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    MethodDispatchResolver.resolveConcreteDispatch(
+        view, factory.parseMethodSignature("java.util.Collection#size(): int"));
+  }
+
+  @Test
+  public void resolveConcreteDispatch() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    MethodSignature strToStringSig =
+        factory.parseMethodSignature("java.lang.String#toString(): java.lang.String");
+    assertEquals(
+        "String.toString() should resolve to itself",
+        strToStringSig,
+        MethodDispatchResolver.resolveConcreteDispatch(view, strToStringSig));
+    assertEquals(
+        "AbstractNamespace.toString() should resolve to Object.toString()",
+        factory.parseMethodSignature("java.lang.Object#toString(): java.lang.String"),
+        MethodDispatchResolver.resolveConcreteDispatch(
+            view,
+            factory.parseMethodSignature(
+                "de.upb.soot.namespaces.AbstractNamespace#toString(): java.lang.String")));
+  }
+
+  @Test
+  public void resolveSpecialDispatch() {
+    IdentifierFactory factory = view.getIdentifierFactory();
+    MethodSignature strInit =
+        factory.parseMethodSignature("java.lang.String#<init>(java.lang.String): void");
+    MethodSignature strToStringSig =
+        factory.parseMethodSignature("java.lang.String#toString(): java.lang.String");
+
+    JSpecialInvokeExpr strInitInvoke =
+        new JSpecialInvokeExpr(
+            new Local("str", factory.getClassType("java.lang.String")),
+            strInit,
+            ImmutableUtils.immutableList(StringConstant.getInstance("abc")));
+
+    assertEquals(
+        "String init should resolve to itself",
+        strInit,
+        MethodDispatchResolver.resolveSpecialDispatch(view, strInitInvoke, strToStringSig));
+
+    MethodSignature privateExplode =
+        factory.parseMethodSignature(
+            "de.upb.soot.namespaces.JavaClassPathNamespace#explode(java.lang.String): java.util.stream.Stream");
+    JSpecialInvokeExpr privateExplodeInvoke =
+        new JSpecialInvokeExpr(
+            new Local("jcp", factory.getClassType("de.upb.soot.namespaces.JavaClassPathNamespace")),
+            privateExplode,
+            ImmutableUtils.immutableList(StringConstant.getInstance("abc")));
+    assertEquals(
+        privateExplode + " is private and should resolve to itself",
+        privateExplode,
+        MethodDispatchResolver.resolveSpecialDispatch(view, privateExplodeInvoke, strToStringSig));
+  }
+}
