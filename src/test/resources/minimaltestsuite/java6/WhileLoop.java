6a6931b0a5920a2f50681a3bd1c6984303897cfc
hasitha <herajapakse@gmail.com>
2019-08-18 19:53:42 +0200

    WhileLoopTest

--- src/test/resources/minimaltestsuite/java6/WhileLoop.java
+++ src/test/resources/minimaltestsuite/java6/WhileLoop.java
@@ -1,10 +1,10 @@
-package de.upb.soot.java6;
-
 public class WhileLoop {
 
-    public boolean whileLoop(){
-        while(true){
-            return true;
+    public void whileLoop(){
+        int num = 10;
+        int i = 0;
+        while(num>i){
+            num--;
         }
     }
 }
