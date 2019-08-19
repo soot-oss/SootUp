0de94586234b0c0c6f08e2ad3c6e952ce971b0af
hasitha <herajapakse@gmail.com>
2019-08-19 21:13:41 +0200

    ContinueInWhileLoopTest

---
+++ src/test/java/de/upb/soot/minimaltestsuite/java6/ContinueInWhileLoopTest.java
@@ -0,0 +1,75 @@
+package de.upb.soot.minimaltestsuite.java6;
+
+import static org.junit.Assert.*;
+
+import categories.Java8Test;
+import de.upb.soot.core.Body;
+import de.upb.soot.core.SootMethod;
+import de.upb.soot.frontends.java.Utils;
+import de.upb.soot.frontends.java.WalaClassLoaderTestUtils;
+import de.upb.soot.jimple.common.stmt.Stmt;
+import de.upb.soot.minimaltestsuite.LoadClassesWithWala;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(Java8Test.class)
+public class ContinueInWhileLoopTest {
+  private String srcDir = "src/test/resources/minimaltestsuite/java6/";
+  private String className = "ContinueInWhileLoop";
+  private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
+
+  @Before
+  public void loadClasses() {
+    loadClassesWithWala.classLoader(srcDir, className);
+  }
+
+  @Test
+  public void continueInWhileLoopTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "continueInWhileLoop",
+                loadClassesWithWala.declareClassSig,
+                "void",
+                Collections.emptyList()));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: ContinueInWhileLoop",
+                "$i0 = 0",
+                "$z0 = $i0 < 10",
+                "if $z0 == 0 goto return",
+                "$z1 = $i0 == 5",
+                "if $z1 == 0 goto $i3 = $i0",
+                "$i1 = $i0",
+                "$i2 = $i0 + 1",
+                "$i0 = $i2",
+                "goto [?= (branch)]",
+                "$i3 = $i0",
+                "$i4 = $i0 + 1",
+                "$i0 = $i4",
+                "goto [?= $z0 = $i0 < 10]",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+}
--- src/test/resources/minimaltestsuite/java6/ContinueInWhileLoop.java
+++ src/test/resources/minimaltestsuite/java6/ContinueInWhileLoop.java
@@ -1,17 +1,13 @@
-package de.upb.soot.java6;
-
 public class ContinueInWhileLoop {
 
-    public int continueInWhileLoop(int num1, int num2){
-        int total = 0;
-        while (num1 > num2){
-            if(num1==0) {
+    public void continueInWhileLoop(){
+        int num = 0;
+        while (num < 10) {
+            if (num == 5) {
+                num++;
                 continue;
             }
-            total += num1;
-            num1--;
-            System.out.println("Current total is = " + total);
+            num++;
         }
-        return total;
     }
 }
