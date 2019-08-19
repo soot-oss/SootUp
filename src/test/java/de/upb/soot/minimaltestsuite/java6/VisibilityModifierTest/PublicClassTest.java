8604f1e3eeaed3b3088ebb86b1b9f2d705c02642
hasitha <herajapakse@gmail.com>
2019-08-19 09:43:31 +0200

    VisibilityModifierTest

---
+++ src/test/java/de/upb/soot/minimaltestsuite/java6/VisibilityModifierTest/PublicClassTest.java
@@ -0,0 +1,168 @@
+package de.upb.soot.minimaltestsuite.java6.VisibilityModifierTest;
+
+import static org.junit.Assert.*;
+
+import categories.Java8Test;
+import de.upb.soot.core.Body;
+import de.upb.soot.core.SootMethod;
+import de.upb.soot.frontends.java.Utils;
+import de.upb.soot.frontends.java.WalaClassLoaderTestUtils;
+import de.upb.soot.jimple.common.stmt.Stmt;
+import de.upb.soot.minimaltestsuite.LoadClassesWithWala;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(Java8Test.class)
+public class PublicClassTest {
+  private String srcDir = "src/test/resources/minimaltestsuite/java6/";
+  private String className = "PublicClass";
+  private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
+
+  @Before
+  public void loadClasses() {
+    loadClassesWithWala.classLoader(srcDir, className);
+  }
+
+  @Test
+  public void publicMethodTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "publicMethod",
+                loadClassesWithWala.declareClassSig,
+                "void",
+                Collections.emptyList()));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: PublicClass",
+                "$i0 = 10",
+                "$i1 = 20",
+                "$i2 = 30",
+                "$i3 = 40",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+
+  @Test
+  public void privateMethodTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "privateMethod",
+                loadClassesWithWala.declareClassSig,
+                "void",
+                Collections.emptyList()));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: PublicClass",
+                "$i0 = 10",
+                "$i1 = 20",
+                "$i2 = 30",
+                "$i3 = 40",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+
+  @Test
+  public void protectedMethodTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "protectedMethod",
+                loadClassesWithWala.declareClassSig,
+                "void",
+                Collections.emptyList()));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: PublicClass",
+                "$i0 = 10",
+                "$i1 = 20",
+                "$i2 = 30",
+                "$i3 = 40",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+
+  @Test
+  public void noModifierMethodTest() {
+    Optional<SootMethod> m =
+        WalaClassLoaderTestUtils.getSootMethod(
+            loadClassesWithWala.loader,
+            loadClassesWithWala.identifierFactory.getMethodSignature(
+                "noModifierMethod",
+                loadClassesWithWala.declareClassSig,
+                "void",
+                Collections.emptyList()));
+    assertTrue(m.isPresent());
+    SootMethod method = m.get();
+    Utils.print(method, false);
+    Body body = method.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts =
+        body.getStmts().stream()
+            .map(Stmt::toString)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: PublicClass",
+                "$i0 = 10",
+                "$i1 = 20",
+                "$i2 = 30",
+                "$i3 = 40",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+}
