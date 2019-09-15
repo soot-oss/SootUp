55a880174d0181b1336e84e19850acf15dc62ad7
hasitha <herajapakse@gmail.com>
2019-09-15 14:57:08 +0200

    LabelStatementTest

---
+++ src/test/resources/minimaltestsuite/java6/LabelStatement.java
@@ -0,0 +1,13 @@
+public class LabelStatement{
+    public void labelStatement(){
+        int num = 20;
+        int i = 1;
+        start:
+            while (i<num){
+                if ( i % 10 == 0 )
+                    break start;
+                else
+                    i++;
+        }
+    }
+}
