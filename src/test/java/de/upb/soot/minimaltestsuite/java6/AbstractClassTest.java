ddc05c23d27615a212f7a7f5bbe724403d446744
Markus Schmidt <markus262@web.de>
2019-09-20 14:38:04 +0200

    refactored test structure

--- src/test/java/de/upb/soot/minimaltestsuite/java6/AbstractClassTest.java
+++ src/test/java/de/upb/soot/minimaltestsuite/java6/AbstractClassTest.java
@@ -1,65 +1,27 @@
 package de.upb.soot.minimaltestsuite.java6;
 
-import static org.junit.Assert.*;
-
-import categories.Java8Test;
-import de.upb.soot.core.Body;
-import de.upb.soot.core.SootMethod;
-import de.upb.soot.frontends.java.Utils;
-import de.upb.soot.frontends.java.WalaClassLoaderTestUtils;
-import de.upb.soot.jimple.common.stmt.Stmt;
-import de.upb.soot.minimaltestsuite.LoadClassesWithWala;
-import java.util.ArrayList;
+import de.upb.soot.minimaltestsuite.MinimalTestSuiteBase;
+import de.upb.soot.signatures.MethodSignature;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
-@Category(Java8Test.class)
-public class AbstractClassTest {
-  private String srcDir = "src/test/resources/minimaltestsuite/java6/AbstractClass/";
-  private String className = "AbstractClass";
-  private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
+public class AbstractClassTest extends MinimalTestSuiteBase {
 
-  @Before
-  public void loadClasses() {
-    loadClassesWithWala.classLoader(srcDir, className);
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "abstractClass", getDeclaredClassSignature(), "void", Collections.emptyList());
   }
 
-  @Test
-  public void abstractClassTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "abstractClass",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
-        Stream.of(
-                "r0 := @this: AbstractClass",
-                "r0 = new AbstractClass",
-                "specialinvoke $u0.<AbstractClass: void <init>()>()",
-                "virtualinvoke $u0.<A: void a()>()",
-                "return")
-            .collect(Collectors.toList());
-
-    assertEquals(expectedStmts, actualStmts);
+  @Override
+  public List<String> getJimpleLines() {
+    return Stream.of(
+            "r0 := @this: AbstractClass",
+            "r0 = new AbstractClass",
+            "specialinvoke $u0.<AbstractClass: void <init>()>()",
+            "virtualinvoke $u0.<A: void a()>()",
+            "return")
+        .collect(Collectors.toList());
   }
 }
