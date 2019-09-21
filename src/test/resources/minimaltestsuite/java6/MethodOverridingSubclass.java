c04b7b9cdba97e18c78142fcc7db6cdf39e6e2e5
kaustubhkelkar <kskelkar@mail.uni-paderborn.de>
2019-09-22 00:58:02 +0200

    Method Overriding

---
+++ src/test/resources/minimaltestsuite/java6/MethodOverridingSubclass.java
@@ -0,0 +1,6 @@
+class MethodOverridingSubclass extends MethodOverriding{
+
+    public double calculateArea(){
+        System.out.println("Inside MethodOverridingSubclass-calculateArea()");
+    }
+}
