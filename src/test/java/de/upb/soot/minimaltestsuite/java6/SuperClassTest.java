3e6eded70991cb1af65042c049a392caea476eeb
kaustubhkelkar <kskelkar@mail.uni-paderborn.de>
2019-09-26 17:59:12 +0200

    Maven formatting

--- src/test/java/de/upb/soot/minimaltestsuite/java6/SuperClassTest.java
+++ src/test/java/de/upb/soot/minimaltestsuite/java6/SuperClassTest.java
@@ -53,7 +53,12 @@ public class SuperClassTest {
 
     List<String> expectedStmts =
         Stream.of(
-                "r0 := @this: SuperClass", "r0.<SuperClass: int a> = 10", "r0.<SuperClass: int b> = 20", "r0.<SuperClass: int c> = 30", "r0.<SuperClass: int d> = 40", "return")
+                "r0 := @this: SuperClass",
+                "r0.<SuperClass: int a> = 10",
+                "r0.<SuperClass: int b> = 20",
+                "r0.<SuperClass: int c> = 30",
+                "r0.<SuperClass: int d> = 40",
+                "return")
             .collect(Collectors.toCollection(ArrayList::new));
 
     assertEquals(expectedStmts, actualStmts);
