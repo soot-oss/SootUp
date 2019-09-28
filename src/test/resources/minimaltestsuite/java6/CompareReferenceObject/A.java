f7130319255e885340f16a846099b96aecdefb58
hasitha <herajapakse@gmail.com>
2019-09-28 05:43:51 +0200

    CompareReferenceObjectTest

---
+++ src/test/resources/minimaltestsuite/java6/CompareReferenceObject/A.java
@@ -0,0 +1,6 @@
+ public class A{
+    int num = 0;
+    public A(){
+        this.num = 1;
+    }
+ }
---
+++ src/test/resources/minimaltestsuite/java6/CompareReferenceObject/CompareReferenceObject.java
@@ -0,0 +1,9 @@
+public class CompareReferenceObject{
+    public void compareReferenceObject(){
+        A obj1 = new A();
+        A obj2 = new A();
+        if(obj1 == obj2){
+            int val = 1;
+        }
+    }
+}
