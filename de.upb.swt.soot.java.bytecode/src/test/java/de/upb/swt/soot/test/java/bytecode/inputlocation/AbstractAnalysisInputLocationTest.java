19a45b4cfcc841e981eb2d54465ce54cb285410b
Linghui Luo <linghui@mail.upb.de>
2019-10-01 14:29:33 +0200

    change jarFile location

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/inputlocation/AbstractAnalysisInputLocationTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/inputlocation/AbstractAnalysisInputLocationTest.java
@@ -39,8 +39,7 @@ import org.mockito.internal.matchers.LessOrEqual;
 /** @author Manuel Benz created on 07.06.18 */
 public abstract class AbstractAnalysisInputLocationTest {
 
-  public static final String jarFile =
-      "target/test-classes/de/upb/soot/namespaces/Soot-4.0-SNAPSHOT.jar";
+  public static final String jarFile = "../shared-test-resources/Soot-4.0-SNAPSHOT.jar";
 
   protected static final int CLASSES_IN_JAR = 25;
   private IdentifierFactory identifierFactory;
