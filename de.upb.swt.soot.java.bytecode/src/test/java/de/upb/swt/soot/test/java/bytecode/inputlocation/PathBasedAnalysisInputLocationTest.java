19b3e09cedae873eb5b8506c7428ef0612448817
Markus Schmidt <markus262@web.de>
2019-10-01 23:21:05 +0200

    added a test to read from rt.jar

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/inputlocation/PathBasedAnalysisInputLocationTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/inputlocation/PathBasedAnalysisInputLocationTest.java
@@ -22,12 +22,22 @@ package de.upb.swt.soot.test.java.bytecode.inputlocation;
  * #L%
  */
 import categories.Java8Test;
+import de.upb.swt.soot.core.Project;
+import de.upb.swt.soot.core.frontend.AbstractClassSource;
 import de.upb.swt.soot.core.types.JavaClassType;
+import de.upb.swt.soot.core.views.JavaView;
+import de.upb.swt.soot.core.views.View;
+import de.upb.swt.soot.java.bytecode.frontend.AsmJavaClassProvider;
+import de.upb.swt.soot.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
 import de.upb.swt.soot.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertTrue;
 
 /** @author Manuel Benz created on 06.06.18 */
 @Category(Java8Test.class)
@@ -38,10 +48,39 @@ public class PathBasedAnalysisInputLocationTest extends AbstractAnalysisInputLoc
   @Test
   public void testJar() {
     PathBasedAnalysisInputLocation pathBasedNamespace =
-        PathBasedAnalysisInputLocation.createForClassContainer(jar);
+            PathBasedAnalysisInputLocation.createForClassContainer(jar);
     System.err.println(jar.toFile().getAbsolutePath());
     final JavaClassType sig =
-        getIdentifierFactory().getClassType("PathBasedNamespace", "de.upb.soot.namespaces");
+            getIdentifierFactory().getClassType("PathBasedNamespace", "de.upb.soot.namespaces");
     testClassReceival(pathBasedNamespace, sig, CLASSES_IN_JAR);
   }
+
+
+  void runtimeContains( View view, String classname, String packageName ){
+    final JavaClassType sig =  getIdentifierFactory().getClassType(classname,packageName );
+    assertTrue(sig + " is not found in rt.jar" , view.getClass( sig ).isPresent());
+  }
+
+  @Test
+  public void testRuntimeJar() {
+    PathBasedAnalysisInputLocation pathBasedNamespace =
+            PathBasedAnalysisInputLocation.createForClassContainer(Paths.get(System.getProperty("java.home")+"/lib/rt.jar"));
+
+    final Collection<? extends AbstractClassSource> classSources =
+            pathBasedNamespace.getClassSources(getIdentifierFactory());
+
+    View v = new JavaView( new Project(pathBasedNamespace) );
+
+    // test some standard jre classes
+    runtimeContains(v,"Object","java.lang");
+    runtimeContains(v,"List","java.util");
+    runtimeContains(v,"Map","java.util");
+    runtimeContains(v,"ArrayList","java.util");
+    runtimeContains(v,"HashMap","java.util");
+    runtimeContains(v,"Collection","java.util");
+    runtimeContains(v,"Comparator","java.util");
+
+  }
+
+
 }
