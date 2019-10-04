260d20f123817eb1f7abe215b99954f6b01cdb27
kaustubhkelkar <loneranger31415@gmail.com>
2019-10-04 17:03:49 +0200

    Code change for following test cases (Done) 10. Declaring an int using a decimal/hex/octalliteral

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareIntTest.java
@@ -0,0 +1,35 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
+
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/** @author Kaustubh Kelkar */
+public class DeclareIntTest extends MinimalTestSuiteBase {
+    public MethodSignature getMethodSignature() {
+      return identifierFactory.getMethodSignature(
+              "declareIntMethod", getDeclaredClassSignature(), "void", Collections.emptyList());
+    }
+  @Override
+  public List<String> getJimpleLines() {
+    return Stream.of(
+                "r0 := @this: DeclareInt",
+                "$r1 = <java.lang.System: java.io.PrintStream out>",
+                "$i0 = r0.<DeclareInt: int dec>",
+                "virtualinvoke $r1.<java.io.PrintStream: void println(int)>($i0)",
+                "$r2 = <java.lang.System: java.io.PrintStream out>",
+                "$i1 = r0.<DeclareInt: int hex>",
+                "virtualinvoke $r2.<java.io.PrintStream: void println(int)>($i1)",
+                "$r3 = <java.lang.System: java.io.PrintStream out>",
+                "$i2 = r0.<DeclareInt: int oct>",
+                "virtualinvoke $r3.<java.io.PrintStream: void println(int)>($i2)",
+                "return")
+            .collect(Collectors.toCollection(ArrayList::new));
+
+  }
+}
