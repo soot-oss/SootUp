937111bc9e3b003cd9f4089e40737de17da4e8e2
hasitha <herajapakse@gmail.com>
2019-10-05 07:19:20 +0200

    refactor BreakInWhileLoopTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BreakInWhileLoopTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BreakInWhileLoopTest.java
@@ -13,6 +13,7 @@ import org.junit.experimental.categories.Category;
 @Category(Java8Test.class)
 public class BreakInWhileLoopTest extends MinimalTestSuiteBase {
 
+  @Override
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
         "breakInWhileLoop", getDeclaredClassSignature(), "void", Collections.emptyList());
