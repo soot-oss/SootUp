e753edd6a6300c6d356662f45f2472aa5bc18440
hasitha <herajapakse@gmail.com>
2019-10-05 07:16:32 +0200

    refactor EscapeSequencesInStringTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/EscapeSequencesInStringTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/EscapeSequencesInStringTest.java
@@ -1,252 +1,138 @@
 /** @author: Hasitha Rajapakse */
 package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import categories.Java8Test;
-import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
-import de.upb.swt.soot.core.model.Body;
-import de.upb.swt.soot.core.model.SootMethod;
-import de.upb.swt.soot.test.java.sourcecode.frontend.Utils;
-import de.upb.swt.soot.test.java.sourcecode.frontend.WalaClassLoaderTestUtils;
-import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.LoadClassesWithWala;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(Java8Test.class)
-public class EscapeSequencesInStringTest {
-  private String srcDir = "src/test/resources/minimaltestsuite/java6/";
-  private String className = "EscapeSequencesInString";
-  private LoadClassesWithWala loadClassesWithWala = new LoadClassesWithWala();
-
-  @Before
-  public void loadClasses() {
-    loadClassesWithWala.classLoader(srcDir, className);
-  }
+public class EscapeSequencesInStringTest extends MinimalTestSuiteBase {
+  private String methodName;
+  private String methodSignature;
+  private List<String> jimpleLines;
 
   @Test
-  public void escapeBackslashBTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeBackslashB",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
+  public void defaultTest() {
+    HashMap<String, HashMap<String, Object>> methodList = setValues();
+    Set<String> methodListKeys = methodList.keySet();
 
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
+    for (String methodListKey : methodListKeys) {
+      methodName = methodListKey;
+      HashMap<String, Object> mv = methodList.get(methodListKey);
+      methodSignature = (String) mv.get("methodSignature");
+      jimpleLines = (List<String>) mv.get("jimpleLines");
 
-    List<String> expectedStmts =
+      test(getJimpleLines(), getMethodSignature());
+    }
+  }
+
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        methodName, getDeclaredClassSignature(), methodSignature, Collections.emptyList());
+  }
+
+  @Override
+  public List<String> getJimpleLines() {
+    return jimpleLines;
+  }
+
+  private HashMap<String, HashMap<String, Object>> setValues() {
+    HashMap<String, HashMap<String, Object>> methodList = new HashMap<>();
+    HashMap<String, Object> methodValues = new HashMap<>();
+
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes backslash b \\u0008\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslashB", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
-
-  @Test
-  public void escapeBackslashTTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeBackslashT",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes backslash t \\t\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslashT", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
+        Stream.of(
+                "r0 := @this: EscapeSequencesInString",
+                "$r1 = \"This escapes backslash n \\n\"",
+                "return")
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslashN", methodValues);
 
-  @Test
-  public void escapeBackslashFTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeBackslashF",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes backslash f \\f\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslashF", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
-
-  @Test
-  public void escapeBackslashRTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeBackslashR",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes backslash r \\r\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslashR", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
-
-  @Test
-  public void escapeDoubleQuotesTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeDoubleQuotes",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes double quotes \\\"\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeDoubleQuotes", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
-
-  @Test
-  public void escapeSingleQuoteTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeSingleQuote",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes single quote \\\'\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toCollection(ArrayList::new)));
+    methodList.put("escapeSingleQuote", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
-  }
-
-  @Test
-  public void escapeBackslashTest() {
-    Optional<SootMethod> m =
-        WalaClassLoaderTestUtils.getSootMethod(
-            loadClassesWithWala.loader,
-            loadClassesWithWala.identifierFactory.getMethodSignature(
-                "escapeBackslash",
-                loadClassesWithWala.declareClassSig,
-                "void",
-                Collections.emptyList()));
-    assertTrue(m.isPresent());
-    SootMethod method = m.get();
-    Utils.print(method, false);
-    Body body = method.getBody();
-    assertNotNull(body);
-
-    List<String> actualStmts =
-        body.getStmts().stream()
-            .map(Stmt::toString)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-    List<String> expectedStmts =
+    methodValues = new HashMap<>();
+    methodValues.put("methodSignature", "void");
+    methodValues.put(
+        "jimpleLines",
         Stream.of(
                 "r0 := @this: EscapeSequencesInString",
                 "$r1 = \"This escapes backslash \\\\\"",
                 "return")
-            .collect(Collectors.toCollection(ArrayList::new));
+            .collect(Collectors.toList()));
+    methodList.put("escapeBackslash", methodValues);
 
-    assertEquals(expectedStmts, actualStmts);
+    return methodList;
   }
 }
