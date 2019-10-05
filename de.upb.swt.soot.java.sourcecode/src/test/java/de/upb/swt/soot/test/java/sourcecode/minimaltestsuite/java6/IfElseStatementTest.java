232be4d52267d778d1282c31e86493fe5554c4bb
hasitha <herajapakse@gmail.com>
2019-10-05 07:12:57 +0200

    IfElseStatementTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/IfElseStatementTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/IfElseStatementTest.java
@@ -13,6 +13,7 @@ import org.junit.experimental.categories.Category;
 @Category(Java8Test.class)
 public class IfElseStatementTest extends MinimalTestSuiteBase {
 
+  @Override
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
         "ifElseStatement", getDeclaredClassSignature(), "void", Collections.emptyList());
