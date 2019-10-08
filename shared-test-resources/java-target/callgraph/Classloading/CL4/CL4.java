6805584f5a339c50f949817a6f5b7f301e1f3f60
Thomas Johannesmeyer <thomas@geeky.gent>
2019-10-08 11:33:00 +0200

    Add description #148

--- shared-test-resources/java-target/callgraph/Classloading/CL4/CL4.java
+++ shared-test-resources/java-target/callgraph/Classloading/CL4/CL4.java
@@ -1,5 +1,10 @@
 // https://bitbucket.org/delors/jcg/src/master/jcg_testcases/src/main/resources/Classloading.md
 
+// This test case defines a custom classloader, that loads the class
+// lib.IntComparator from a byte array. The array bytes contains the bytes of
+// the ByteClassLoader class. In order to extract the bytes, we used
+// java.nio.file.Files.readAllBytes().
+
 import lib.annotations.callgraph.IndirectCall;
 import java.util.Comparator;
 
