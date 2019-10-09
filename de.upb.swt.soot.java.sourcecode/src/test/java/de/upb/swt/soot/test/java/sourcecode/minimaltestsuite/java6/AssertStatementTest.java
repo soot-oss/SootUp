d36b7bc86aaa9e2cfabd9ede342f5104961c2fbe
hasitha <herajapakse@gmail.com>
2019-10-09 19:28:32 +0200

    AssertStatementTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/AssertStatementTest.java
@@ -0,0 +1,35 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
+
+import categories.Java8Test;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.experimental.categories.Category;
+
+@Category(Java8Test.class)
+public class AssertStatementTest extends MinimalTestSuiteBase {
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "assertStatement", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Override
+  public List<String> getJimpleLines() {
+    return Stream.of(
+            "r0 := @this: AssertStatement",
+            "$z0 = \"\" != null",
+            "$z1 = <AssertStatement: boolean $assertionsDisabled>",
+            "if $z1 == 1 goto nop",
+            "if $z0 == 1 goto nop",
+            "$r1 = new java.lang.AssertionError",
+            "specialinvoke $r1.<java.lang.AssertionError: void <init>()>()",
+            "throw $r1",
+            "nop",
+            "return")
+        .collect(Collectors.toList());
+  }
+}
