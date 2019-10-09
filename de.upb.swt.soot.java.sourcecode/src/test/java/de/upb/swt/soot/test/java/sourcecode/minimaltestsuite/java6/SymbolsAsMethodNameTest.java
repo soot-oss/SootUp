ead9d3ccbdf62022d47443d575216c4f4305fd15
hasitha <herajapakse@gmail.com>
2019-10-09 19:37:24 +0200

    SymbolsAsMethodNameTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/SymbolsAsMethodNameTest.java
@@ -0,0 +1,26 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
+
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SymbolsAsMethodNameTest extends MinimalTestSuiteBase {
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "αρετη", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Override
+  public List<String> getJimpleLines() {
+    return Stream.of(
+            "r0 := @this: SymbolsAsMethodName",
+            "$r1 = <java.lang.System: java.io.PrintStream out>",
+            "virtualinvoke $r1.<java.io.PrintStream: void println(java.lang.String)>(\"this is \\u03b1\\u03c1\\u03b5\\u03c4\\u03b7 method\")",
+            "return")
+        .collect(Collectors.toList());
+  }
+}
