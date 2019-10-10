63bb080e70b5f69be05427f7c5b7344c6cd52f81
kaustubhkelkar <loneranger31415@gmail.com>
2019-10-10 08:37:35 +0200

    Fixing as per code review -WIP

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/StaticMethodTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/StaticMethodTest.java
@@ -2,18 +2,28 @@
 
 package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.model.SootField;
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /** @author Kaustubh Kelkar */
 public class StaticMethodTest extends MinimalTestSuiteBase {
+
+  SootClass sootClass = new SootClass();
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
         "staticMethod", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  public boolean isMethodStatic(){
+    if (sootClass.getFields().contains("static")) return true;
+    else return false;
   }
 
   @Override
