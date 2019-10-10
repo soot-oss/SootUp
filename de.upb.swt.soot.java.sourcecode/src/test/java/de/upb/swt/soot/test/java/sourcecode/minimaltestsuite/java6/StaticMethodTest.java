01575e693a47ae061565860d3e2395d41a99345b
kaustubhkelkar <loneranger31415@gmail.com>
2019-10-10 13:06:36 +0200

    Changes for static method check

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/StaticMethodTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/StaticMethodTest.java
@@ -2,32 +2,34 @@
 
 package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 
-import de.upb.swt.soot.core.model.SootClass;
-import de.upb.swt.soot.core.model.SootField;
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import org.junit.Test;
+
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static org.junit.Assert.assertTrue;
 
 /** @author Kaustubh Kelkar */
 public class StaticMethodTest extends MinimalTestSuiteBase {
 
-  SootClass sootClass = new SootClass();
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
         "staticMethod", getDeclaredClassSignature(), "void", Collections.emptyList());
   }
 
-  public boolean isMethodStatic(){
-    if (sootClass.getFields().contains("static")) return true;
-    else return false;
+  @Test
+  @Override
+  public void defaultTest() {
+    super.defaultTest();
+    assertTrue(method.isStatic());
   }
 
   @Override
-  public List<String> getJimpleLines() {
+  public List<String> expectedBodyStmts() {
     return Stream.of(
             "$r0 = <java.lang.System: java.io.PrintStream out>",
             "virtualinvoke $r0.<java.io.PrintStream: void println(java.lang.String)>(\"static method\")",
