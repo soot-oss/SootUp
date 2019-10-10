79594028915160a9334ef936c07d26932638672e
kaustubhkelkar <loneranger31415@gmail.com>
2019-10-10 13:23:01 +0200

    Removing commented code

--- de.upb.swt.soot.java.sourcecode/src/test/resources/minimaltestsuite/java6/TernaryOperator.java
+++ de.upb.swt.soot.java.sourcecode/src/test/resources/minimaltestsuite/java6/TernaryOperator.java
@@ -1,7 +1,6 @@
 public class TernaryOperator {
     
-    //boolean status = true;
-    int num= 2; 
+    int num= 2;
     
     boolean ternaryOperatorMethod(){
         return num < 0 ? false : true; 
