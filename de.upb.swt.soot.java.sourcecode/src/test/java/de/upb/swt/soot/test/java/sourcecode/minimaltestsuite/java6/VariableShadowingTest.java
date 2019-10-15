8441c8b38653475cba9b9826d0f04916b9a49e6a
hasitha <herajapakse@gmail.com>
2019-10-15 22:58:54 +0200

    VariableShadowingTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/VariableShadowingTest.java
@@ -0,0 +1,29 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
+
+import categories.Java8Test;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import org.junit.experimental.categories.Category;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Category(Java8Test.class)
+public class VariableShadowingTest extends MinimalTestSuiteBase {
+    @Override
+    public MethodSignature getMethodSignature() {
+        return identifierFactory.getMethodSignature(
+                "variableShadowing", getDeclaredClassSignature(), "void", Collections.emptyList());
+    }
+
+    @Override
+    public List<String> expectedBodyStmts() {
+        return Stream.of(
+                "r0 := @this: VariableShadowing",
+                "$i0 = 10",
+                "return")
+                .collect(Collectors.toList());
+    }
+}
---
+++ de.upb.swt.soot.java.sourcecode/src/test/resources/minimaltestsuite/java6/VariableShadowing.java
@@ -0,0 +1,10 @@
+
+/** @author: Hasitha Rajapakse */
+
+public class VariableShadowing{
+    int num = 5;
+
+    public void variableShadowing(){
+        int num = 10;
+    }
+}
