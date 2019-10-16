05a7d64f4c7e53b1983003f09907131df9842071
hasitha <herajapakse@gmail.com>
2019-10-16 02:48:23 +0200

    BitwiseOperationsIntTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BitwiseOperationsIntTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BitwiseOperationsIntTest.java
@@ -4,10 +4,9 @@ package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 import categories.Java8Test;
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import java.util.Collections;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-
-import java.util.Collections;
 
 @Category(Java8Test.class)
 public class BitwiseOperationsIntTest extends MinimalTestSuiteBase {
@@ -16,31 +15,50 @@ public class BitwiseOperationsIntTest extends MinimalTestSuiteBase {
   public void defaultTest() {
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = 20","$i2 = $i0 & $i1","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt",
+            "$i0 = 70",
+            "$i1 = 20",
+            "$i2 = $i0 & $i1",
+            "return"),
         getMethodSignature("bitwiseOpAnd"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = 20","$i2 = $i0 | $i1","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt",
+            "$i0 = 70",
+            "$i1 = 20",
+            "$i2 = $i0 | $i1",
+            "return"),
         getMethodSignature("bitwiseOpOr"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = 20","$i2 = $i0 ^ $i1","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt",
+            "$i0 = 70",
+            "$i1 = 20",
+            "$i2 = $i0 ^ $i1",
+            "return"),
         getMethodSignature("bitwiseOpXor"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = neg $i0","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt", "$i0 = 70", "$i1 = neg $i0", "return"),
         getMethodSignature("bitwiseOpComplement"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = $i0 >> 5","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt", "$i0 = 70", "$i1 = $i0 >> 5", "return"),
         getMethodSignature("bitwiseOpSignedRightShift"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = $i0 << 5","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt", "$i0 = 70", "$i1 = $i0 << 5", "return"),
         getMethodSignature("bitwiseOpLeftShift"));
 
     loadMethod(
-        expectedBodyStmts("r0 := @this: BitwiseOperationsInt","$i0 = 70","$i1 = $i0 >>> 5","return"),
+        expectedBodyStmts(
+            "r0 := @this: BitwiseOperationsInt", "$i0 = 70", "$i1 = $i0 >>> 5", "return"),
         getMethodSignature("bitwiseOpUnsignedRightShift"));
   }
 
