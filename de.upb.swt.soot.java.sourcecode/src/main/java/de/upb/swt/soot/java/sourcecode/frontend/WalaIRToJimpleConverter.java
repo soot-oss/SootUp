7971ab9a72004e82f992d4c9ed6610e00e0f816a
hasitha <herajapakse@gmail.com>
2019-10-20 17:42:10 +0200

    change to use OverridingClassSource and OverridingMethodSource

--- de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
+++ de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
@@ -164,7 +164,10 @@ public class WalaIRToJimpleConverter {
         modifiers);
   }
 
-  /** Create a {@link de.upb.swt.soot.core.frontend.OverridingClassSource} object for the given walaClass. */
+  /**
+   * Create a {@link de.upb.swt.soot.core.frontend.OverridingClassSource} object for the given
+   * walaClass.
+   */
   public OverridingClassSource createClassSource(
       AstClass walaClass,
       JavaClassType superClass,
@@ -257,7 +260,11 @@ public class WalaIRToJimpleConverter {
 
     Body body = createBody(methodSig, modifiers, walaMethod);
     return new de.upb.swt.soot.java.sourcecode.frontend.WalaSootMethod(
-        new OverridingMethodSource(methodSig, body), methodSig, modifiers, thrownExceptions, debugInfo);
+        new OverridingMethodSource(methodSig, body),
+        methodSig,
+        modifiers,
+        thrownExceptions,
+        debugInfo);
   }
 
   public Type convertType(TypeReference type) {
