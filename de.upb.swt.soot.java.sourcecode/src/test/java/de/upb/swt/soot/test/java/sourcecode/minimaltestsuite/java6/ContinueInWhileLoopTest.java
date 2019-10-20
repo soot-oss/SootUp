a288b9c704408c6ba3cb894c57a1965df95fc77d
hasitha <herajapakse@gmail.com>
2019-10-20 18:57:42 +0200

    refactor ContinueInWhileLoopTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/ContinueInWhileLoopTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/ContinueInWhileLoopTest.java
@@ -24,18 +24,22 @@ public class ContinueInWhileLoopTest extends MinimalTestSuiteBase {
     return Stream.of(
             "r0 := @this: ContinueInWhileLoop",
             "$i0 = 0",
+            "label1:",
             "$z0 = $i0 < 10",
-            "if $z0 == 0 goto return",
+            "if $z0 == 0 goto label4",
             "$z1 = $i0 == 5",
-            "if $z1 == 0 goto $i3 = $i0",
+            "if $z1 == 0 goto label2",
             "$i1 = $i0",
             "$i2 = $i0 + 1",
             "$i0 = $i2",
-            "goto [?= (branch)]",
+            "goto label3",
+            "label2:",
             "$i3 = $i0",
             "$i4 = $i0 + 1",
             "$i0 = $i4",
-            "goto [?= $z0 = $i0 < 10]",
+            "label3:",
+            "goto label1",
+            "label4:",
             "return")
         .collect(Collectors.toList());
   }
