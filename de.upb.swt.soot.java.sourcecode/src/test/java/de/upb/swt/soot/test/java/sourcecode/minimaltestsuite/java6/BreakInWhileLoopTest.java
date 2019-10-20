c5d6bc569e2c7e11757575af4d9e6ecdb775b6b0
hasitha <herajapakse@gmail.com>
2019-10-20 18:52:34 +0200

    refactor BreakInWhileLoopTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BreakInWhileLoopTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/BreakInWhileLoopTest.java
@@ -25,15 +25,18 @@ public class BreakInWhileLoopTest extends MinimalTestSuiteBase {
             "r0 := @this: BreakInWhileLoop",
             "$i0 = 10",
             "$i1 = 5",
+            "label1:",
             "$z0 = $i0 > 0",
-            "if $z0 == 0 goto return",
+            "if $z0 == 0 goto label3",
             "$i2 = $i0",
             "$i3 = $i0 - 1",
             "$i0 = $i3",
             "$z1 = $i0 == $i1",
-            "if $z1 == 0 goto (branch)",
-            "goto [?= return]",
-            "goto [?= $z0 = $i0 > 0]",
+            "if $z1 == 0 goto label2",
+            "goto label3",
+            "label2:",
+            "goto label1",
+            "label3:",
             "return")
         .collect(Collectors.toList());
   }
