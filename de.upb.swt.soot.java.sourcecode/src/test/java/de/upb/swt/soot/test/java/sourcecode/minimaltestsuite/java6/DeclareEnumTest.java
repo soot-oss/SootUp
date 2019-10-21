789909cb33c5f7bd4138d0a5703ee0f8268fd1a8
hasitha <herajapakse@gmail.com>
2019-10-21 10:18:08 +0200

    refactor DeclareEnumTest

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumTest.java
@@ -25,16 +25,18 @@ public class DeclareEnumTest extends MinimalTestSuiteBase {
             "r0 := @this: DeclareEnum",
             "$r1 = staticinvoke <DeclareEnum$Type: DeclareEnum$Type[] values()>()",
             "$i0 = 0",
+            "label1:",
             "$i1 = lengthof $r1",
             "$z0 = $i0 < $i1",
-            "if $z0 == 0 goto return",
+            "if $z0 == 0 goto label2",
             "$r2 = $r1[$i0]",
             "$r3 = <java.lang.System: java.io.PrintStream out>",
             "virtualinvoke $r3.<java.io.PrintStream: void println(java.lang.Object)>($r2)",
             "$i2 = $i0",
             "$i3 = $i0 + 1",
             "$i0 = $i3",
-            "goto [?= $i1 = lengthof $r1]",
+            "goto label1",
+            "label2:",
             "return")
         .collect(Collectors.toList());
   }
