817665399f739fc6717e975c5acbac433ee843a5
hasitha <herajapakse@gmail.com>
2019-10-24 06:48:58 +0200

    GenericTypeParamOnJavaTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/resources/minimaltestsuite/java6/GenericTypeParamOnClass.java
@@ -0,0 +1,24 @@
+
+/** @author: Hasitha Rajapakse **/
+
+
+public class GenericTypeParamOnJava{
+
+    public class A<T> {
+        private T t;
+
+        private void set(T t) {
+            this.t = t;
+        }
+
+        public T get() {
+            return t;
+        }
+    }
+
+    public void genericTypeParamOnJava() {
+        A<Integer> a = new A<Integer>();
+        a.set(5);
+        int x = a.get();
+    }
+}
