2e39137ecd6b6cbf5f09cf988bdf78dbb08da878
kaustubhkelkar <loneranger31415@gmail.com>
2019-10-31 12:05:52 +0100

    test case- Annotation interface (ignore)

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/AnnotationLibraryTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/AnnotationLibraryTest.java
@@ -6,22 +6,19 @@ import de.upb.swt.soot.core.model.Modifier;
 import de.upb.swt.soot.core.model.SootClass;
 import de.upb.swt.soot.core.types.JavaClassType;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AnnotationLibraryTest extends MinimalTestSuiteBase {
 
-  @Override
-  protected JavaClassType getDeclaredClassSignature() {
-    return identifierFactory.getClassType(getClassName());
-  }
-
   @Test
   public void defaultTest() {
+  }
 
+  @Ignore
+  public void testAnnotation(){
     System.out.println(getDeclaredClassSignature());
     SootClass sootClass = loadClass(getDeclaredClassSignature());
-    // view.getClass().getAnnotationsByType(getDeclaredClassSignature());
-    // SootClass sootClass = loadClass(getDeclaredClassSignature());
     assertTrue(Modifier.isAnnotation(sootClass.getModifiers()));
   }
 }
