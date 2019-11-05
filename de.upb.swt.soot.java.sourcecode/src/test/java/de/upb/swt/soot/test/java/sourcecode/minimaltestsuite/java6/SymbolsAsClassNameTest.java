3f4dc2fe66fda1bf37e4644d5dec0d50a8da1064
Markus Schmidt <markus262@web.de>
2019-11-05 14:28:34 +0100

    style

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/SymbolsAsClassNameTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/SymbolsAsClassNameTest.java
@@ -4,22 +4,16 @@ package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 import de.upb.swt.soot.core.model.SootClass;
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertTrue;
-
 public class SymbolsAsClassNameTest extends MinimalTestSuiteBase {
   @Override
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
-        "αρετηAsClassName",
-        getDeclaredClassSignature(),
-        "void",
-        Collections.emptyList());
+        "αρετηAsClassName", getDeclaredClassSignature(), "void", Collections.emptyList());
   }
 
   @Override
@@ -27,8 +21,8 @@ public class SymbolsAsClassNameTest extends MinimalTestSuiteBase {
     super.defaultTest();
     SootClass sootClass = loadClass(getDeclaredClassSignature());
     System.out.println("dummy");
-    //System.out.println(sootClass.getClassSource().getClassType().getClassName());
-    //assertTrue(sootClass.getClass().getName().equals("\u03b1\u03c1\u03b5\u03c4\u03b7"));
+    // System.out.println(sootClass.getClassSource().getClassType().getClassName());
+    // assertTrue(sootClass.getClass().getName().equals("\u03b1\u03c1\u03b5\u03c4\u03b7"));
   }
 
   @Override
--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java9/AnonymousDiamondOperatorTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java9/AnonymousDiamondOperatorTest.java
@@ -2,18 +2,17 @@ package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java9;
 
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
-
 import java.util.List;
 
 public class AnonymousDiamondOperatorTest extends MinimalTestSuiteBase {
 
-    @Override
-    public MethodSignature getMethodSignature() {
-        return null;
-    }
+  @Override
+  public MethodSignature getMethodSignature() {
+    return null;
+  }
 
-    @Override
-    public List<String> expectedBodyStmts() {
-        return null;
-    }
+  @Override
+  public List<String> expectedBodyStmts() {
+    return null;
+  }
 }
