1ddfd183f653a129994a6d89be7125a955c3f33d
Linghui Luo <linghui@outlook.de>
2019-11-06 12:55:49 +0100

    try to fix the failed tests in ModuleBuilderActorTest

--- de.upb.swt.soot.java.core/src/main/java/de/upb/swt/soot/java/core/views/JavaModuleView.java
+++ de.upb.swt.soot.java.core/src/main/java/de/upb/swt/soot/java/core/views/JavaModuleView.java
@@ -1,17 +1,13 @@
 package de.upb.swt.soot.java.core.views;
 
 import de.upb.swt.soot.core.Project;
-import de.upb.swt.soot.core.frontend.AbstractClassSource;
 import de.upb.swt.soot.core.inputlocation.AnalysisInputLocation;
 import de.upb.swt.soot.core.inputlocation.ClassLoadingOptions;
-import de.upb.swt.soot.core.model.AbstractClass;
 import de.upb.swt.soot.core.model.SootClass;
-import de.upb.swt.soot.core.types.ClassType;
 import de.upb.swt.soot.core.types.Type;
 import de.upb.swt.soot.java.core.JavaModuleInfo;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
 import javax.annotation.Nonnull;
 import org.apache.commons.lang3.tuple.Pair;
@@ -31,16 +27,7 @@ public class JavaModuleView extends JavaView {
   /** Creates a new instance of the {@link JavaView} class. */
   public JavaModuleView(@Nonnull Project project) {
     this(project, analysisInputLocation -> null);
-    this.classLoadingOptionsSpecifier = classLoadingOptionsSpecifier;
   }
 
   @Nonnull private final Map<Pair<JavaModuleInfo, Type>, SootClass> map = new HashMap<>();
-
-  @Override
-  @Nonnull
-  public synchronized Optional<AbstractClass<? extends AbstractClassSource>> getClass(
-      @Nonnull ClassType type) {
-    // FIXME: get the first class you find, in the annouymous module...
-    return null;
-  }
 }
