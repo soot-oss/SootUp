acb2d9fce385ceb051766ab5ee27d9aeed509456
kaustubhkelkar <loneranger31415@gmail.com>
2019-11-07 01:27:06 +0100

    test case :Private interface methods  (Java 9)

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java9/PrivateMethodInterfaceImplTest.java
@@ -0,0 +1,42 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java9;
+
+import static org.junit.Assert.assertTrue;
+
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/** @author Kaustubh Kelkar */
+public class PrivateMethodInterfaceImplTest extends MinimalTestSuiteBase {
+
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "methodInterfaceImpl", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Override
+  public void defaultTest() {
+    super.defaultTest();
+    SootClass sootClass = loadClass(getDeclaredClassSignature());
+    assertTrue(
+        sootClass.getInterfaces().stream()
+            .anyMatch(
+                javaClassType ->
+                    javaClassType.getClassName().equalsIgnoreCase("PrivateMethodInterface")));
+  }
+
+  @Override
+  public List<String> expectedBodyStmts() {
+    return Stream.of(
+            "r0 := @this: PrivateMethodInterfaceImpl",
+            "interfaceinvoke r0.<PrivateMethodInterface: void methodInterface(int,int)>(4, 2)",
+            "return")
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
+}
