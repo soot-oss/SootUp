df35e936438e5000c73668304b8703015e3f7b8a
Thomas Johannesmeyer <thomas@geeky.gent>
2019-11-12 23:31:08 +0100

    Replace jdoc links with html

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/inputlocation/ClassLoadingOptions.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/inputlocation/ClassLoadingOptions.java
@@ -4,14 +4,18 @@ import de.upb.swt.soot.core.transform.BodyInterceptor;
 import java.util.List;
 import javax.annotation.Nonnull;
 
+// Regarding the html-links in the Javadoc:
+// We cannot use javadoc links directly if we would like to avoid javadoc errors:
+// The referenced module is unknown from within here. Therefore we use html links
+// which are relative to the documentation root
+
 /**
  * Options that are passed through to the respective frontend. These define how the frontend should
  * behave while loading classes.
  *
  * <p>Besides being able to create your own by implementing this interface, each frontend has
- * built-in sets of options such as {@link
- * de.upb.swt.soot.java.bytecode.inputlocation.BytecodeClassLoadingOptions} and {@link
- * de.upb.swt.soot.java.sourcecode.inputlocation.SourcecodeClassLoadingOptions}.
+ * built-in sets of options such as <a href="../../../../../../de/upb/swt/soot/java/bytecode/inputlocation/BytecodeClassLoadingOptions.html">java.bytecode.inputlocation.BytecodeClassLoadingOptions</a>
+ * and <a href="../../../../../../de/upb/swt/soot/java/sourcecode/inputlocation/SourcecodeClassLoadingOptions.html">java.sourcecode.inputlocation.SourcecodeClassLoadingOptions</a>
  *
  * @author Christian Brüggemann
  */
