60e02787fb35afe2f79a3f547661d6490412ac2e
Thomas Johannesmeyer <thomas@geeky.gent>
2019-11-14 14:32:34 +0100

    Improve readability of cross-module references

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/inputlocation/ClassLoadingOptions.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/inputlocation/ClassLoadingOptions.java
@@ -4,20 +4,13 @@ import de.upb.swt.soot.core.transform.BodyInterceptor;
 import java.util.List;
 import javax.annotation.Nonnull;
 
-// Regarding the html-links in the Javadoc:
-// We cannot use javadoc links directly if we would like to avoid javadoc errors:
-// The referenced module is unknown from within here. Therefore we use html links
-// which are relative to the documentation root
-
 /**
  * Options that are passed through to the respective frontend. These define how the frontend should
  * behave while loading classes.
  *
  * <p>Besides being able to create your own by implementing this interface, each frontend has
- * built-in sets of options such as <a
- * href="../../../../../../de/upb/swt/soot/java/bytecode/inputlocation/BytecodeClassLoadingOptions.html">java.bytecode.inputlocation.BytecodeClassLoadingOptions</a>
- * and <a
- * href="../../../../../../de/upb/swt/soot/java/sourcecode/inputlocation/SourcecodeClassLoadingOptions.html">java.sourcecode.inputlocation.SourcecodeClassLoadingOptions</a>
+ * built-in sets of options such as {@code java.bytecode.inputlocation.BytecodeClassLoadingOptions}
+ * and {@code java.sourcecode.inputlocation.SourcecodeClassLoadingOptions}
  *
  * @author Christian Brüggemann
  */
