93b9de14d6b014a944550ab0c53b7b880b761b0a
hasitha <herajapakse@gmail.com>
2019-11-20 16:46:07 +0100

    MultiInterfaceImplementationTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/testcase/MultiInterfaceImplementationTest.java
@@ -0,0 +1,29 @@
+package de.upb.swt.soot.test.java.sourcecode.typehierarchy.testcase;
+
+import categories.Java8Test;
+import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
+import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
+import de.upb.swt.soot.core.types.ClassType;
+import de.upb.swt.soot.test.java.sourcecode.typehierarchy.JavaTypeHierarchyBase;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/** @author: Hasitha Rajapakse **/
+
+
+@Category(Java8Test.class)
+public class MultiInterfaceImplementationTest extends JavaTypeHierarchyBase {
+    @Test
+    public void method(){
+        ViewTypeHierarchy typeHierarchy = (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
+        Set<ClassType> interfaceSet = new HashSet<>();
+        interfaceSet.add(getClassType("InterfaceA"));
+        interfaceSet.add(getClassType("InterfaceB"));
+        assertEquals(typeHierarchy.implementedInterfacesOf(getClassType(customTestWatcher.getClassName())),interfaceSet);
+    }
+}
