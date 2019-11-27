5ab1769e1cb445983717d9f5d18165428ac8628d
kaustubhkelkar <loneranger31415@gmail.com>
2019-11-27 04:16:30 +0100

    test case : declare Eum with constructor

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumWithConstructorTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumWithConstructorTest.java
@@ -1,7 +1,7 @@
-
 package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
 
 import categories.Java8Test;
+import de.upb.swt.soot.core.model.SootClass;
 import de.upb.swt.soot.core.signatures.MethodSignature;
 import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalTestSuiteBase;
 import java.util.Collections;
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.experimental.categories.Category;
 
-
 /** @author Kaustubh Kelkar */
 @Category(Java8Test.class)
 public class DeclareEnumWithConstructorTest extends MinimalTestSuiteBase {
@@ -18,27 +17,23 @@ public class DeclareEnumWithConstructorTest extends MinimalTestSuiteBase {
   @Override
   public MethodSignature getMethodSignature() {
     return identifierFactory.getMethodSignature(
-        "declareEnum", getDeclaredClassSignature(), "void", Collections.emptyList());
+        "<init>", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Override
+  public void defaultTest() {
+    super.defaultTest();
+    SootClass clazz = loadClass(getDeclaredClassSignature());
+    /**
+     * TODO check whether object holds ENUM value assertTrue(clazz.getFields().stream().anyMatch());
+     */
   }
 
   @Override
   public List<String> expectedBodyStmts() {
     return Stream.of(
             "r0 := @this: DeclareEnumWithConstructor",
-            "$r1 = staticinvoke <DeclareEnum$Type: DeclareEnum$Type[] values()>()",
-            "$i0 = 0",
-            "label1:",
-            "$i1 = lengthof $r1",
-            "$z0 = $i0 < $i1",
-            "if $z0 == 0 goto label2",
-            "$r2 = $r1[$i0]",
-            "$r3 = <java.lang.System: java.io.PrintStream out>",
-            "virtualinvoke $r3.<java.io.PrintStream: void println(java.lang.Object)>($r2)",
-            "$i2 = $i0",
-            "$i3 = $i0 + 1",
-            "$i0 = $i3",
-            "goto label1",
-            "label2:",
+            "specialinvoke r0.<java.lang.Object: void <init>()>()",
             "return")
         .collect(Collectors.toList());
   }
