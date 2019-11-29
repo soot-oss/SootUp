0c0894c929459b9efc3a3447119e102bdf1abdad
hasitha <herajapakse@gmail.com>
2019-11-29 10:53:45 +0100

    mvn fmt

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/JavaTypeHierarchyBase.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/JavaTypeHierarchyBase.java
@@ -7,72 +7,69 @@ import de.upb.swt.soot.java.core.language.JavaLanguage;
 import de.upb.swt.soot.java.core.types.JavaClassType;
 import de.upb.swt.soot.java.core.views.JavaView;
 import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import java.util.Collections;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
-import java.util.Collections;
-
-/** @author: Hasitha Rajapakse **/
-
-
+/** @author: Hasitha Rajapakse * */
 public class JavaTypeHierarchyBase {
 
-    static final String baseDir = "src/test/resources/javatypehierarchy/";
-    protected JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
+  static final String baseDir = "src/test/resources/javatypehierarchy/";
+  protected JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
 
+  @ClassRule public static CustomTestWatcher customTestWatcher = new CustomTestWatcher();
 
-    @ClassRule public static CustomTestWatcher customTestWatcher = new CustomTestWatcher();
+  public static class CustomTestWatcher extends TestWatcher {
+    private String className = JavaTypeHierarchyBase.class.getSimpleName();
+    private AnalysisInputLocation srcCode;
+    private JavaView view;
+    private JavaProject project;
 
-    public static class CustomTestWatcher extends TestWatcher {
-        private String className = JavaTypeHierarchyBase.class.getSimpleName();
-        private AnalysisInputLocation srcCode;
-        private JavaView view;
-        private JavaProject project;
-
-        @Override
-        protected void starting(Description description) {
-            String prevClassName = getClassName();
-            setClassName(extractClassName(description.getClassName()));
-            if (!prevClassName.equals(getClassName())) {
-                srcCode = new JavaSourcePathAnalysisInputLocation(Collections.singleton(baseDir+"/"+getClassName()));
-                project = JavaProject.builder(new JavaLanguage(8)).addClassPath(this.srcCode).build();
-                setView(project.createFullView());
-            }
-        }
-
-        public String getClassName() {
-            return className;
-        }
-
-        private void setClassName(String className) {
-            this.className = className;
-        }
-
-        private void setView(JavaView view) {
-            this.view = view;
-        }
-
-        public JavaView getView() {
-            return view;
-        }
+    @Override
+    protected void starting(Description description) {
+      String prevClassName = getClassName();
+      setClassName(extractClassName(description.getClassName()));
+      if (!prevClassName.equals(getClassName())) {
+        srcCode =
+            new JavaSourcePathAnalysisInputLocation(
+                Collections.singleton(baseDir + "/" + getClassName()));
+        project = JavaProject.builder(new JavaLanguage(8)).addClassPath(this.srcCode).build();
+        setView(project.createFullView());
+      }
     }
 
-    public JavaClassType getClassType(String className){
-        return identifierFactory.getClassType(className);
+    public String getClassName() {
+      return className;
     }
 
-    public static String extractClassName(String classPath) {
-        String classPathArray = classPath.substring(classPath.lastIndexOf(".")+1);
-        String testDirectoryName = "";
-        if (!classPathArray.isEmpty()) {
-            testDirectoryName = classPathArray.substring(0,classPathArray.length()-4);
-        }
-        return testDirectoryName;
+    private void setClassName(String className) {
+      this.className = className;
     }
 
-    @Test
-    public void method(){
+    private void setView(JavaView view) {
+      this.view = view;
     }
+
+    public JavaView getView() {
+      return view;
+    }
+  }
+
+  public JavaClassType getClassType(String className) {
+    return identifierFactory.getClassType(className);
+  }
+
+  public static String extractClassName(String classPath) {
+    String classPathArray = classPath.substring(classPath.lastIndexOf(".") + 1);
+    String testDirectoryName = "";
+    if (!classPathArray.isEmpty()) {
+      testDirectoryName = classPathArray.substring(0, classPathArray.length() - 4);
+    }
+    return testDirectoryName;
+  }
+
+  @Test
+  public void method() {}
 }
