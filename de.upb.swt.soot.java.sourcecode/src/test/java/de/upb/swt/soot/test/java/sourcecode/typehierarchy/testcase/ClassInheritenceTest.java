6d3b354a41d7dfaf71d50da2e5d18a2243197bdf
hasitha <herajapakse@gmail.com>
2019-11-29 11:05:14 +0100

    fix to check inheritence only with ViewTypeHierarchy

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/testcase/ClassInheritenceTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/testcase/ClassInheritenceTest.java
@@ -3,23 +3,22 @@ package de.upb.swt.soot.test.java.sourcecode.typehierarchy.testcase;
 import categories.Java8Test;
 import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
 import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
-import de.upb.swt.soot.core.model.SootClass;
 import de.upb.swt.soot.test.java.sourcecode.typehierarchy.JavaTypeHierarchyBase;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-/** @author: Hasitha Rajapakse **/
+/** @author: Hasitha Rajapakse * */
 
 @Category(Java8Test.class)
 public class ClassInheritenceTest extends JavaTypeHierarchyBase {
-    @Test
-    public void method(){
-        SootClass sootClass = (SootClass) customTestWatcher.getView().getClass(customTestWatcher.getView().getIdentifierFactory().getClassType(customTestWatcher.getClassName())).get();
-        assertTrue(sootClass.hasSuperclass());
-        ViewTypeHierarchy typeHierarchy = (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
-        assertEquals(typeHierarchy.superClassOf(getClassType(customTestWatcher.getClassName())),getClassType("SuperClass"));
-    }
+  @Test
+  public void method() {
+    ViewTypeHierarchy typeHierarchy =
+        (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
+    assertEquals(
+        typeHierarchy.superClassOf(getClassType(customTestWatcher.getClassName())),
+        getClassType("SuperClass"));
+  }
 }
