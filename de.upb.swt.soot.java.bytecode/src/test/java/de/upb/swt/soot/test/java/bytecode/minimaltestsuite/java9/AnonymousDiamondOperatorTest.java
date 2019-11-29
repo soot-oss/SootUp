2538a656938f014430cb439b45ff271367020f5d
Kaustubh Kelkar <kskelkar@mail.uni-paderborn.de>
2019-11-29 02:31:32 +0100

    test cases except class files (Java 9 and Java 10)

---
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java9/AnonymousDiamondOperatorTest.java
@@ -0,0 +1,57 @@
+package de.upb.swt.soot.test.java.bytecode.minimaltestsuite.java9;
+
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.bytecode.minimaltestsuite.MinimalBytecodeTestSuiteBase;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/** @author Kaustubh Kelkar */
+public class AnonymousDiamondOperatorTest extends MinimalBytecodeTestSuiteBase {
+
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "innerClassDiamond", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Test
+  public void defaultTest() {}
+
+  @Ignore
+  public void ignoreTest() {
+    /** TODO Check for anonymous declarations once Java 9 is supported in WALA */
+  }
+
+  @Override
+  public List<String> expectedBodyStmts() {
+    return Stream.of(
+            "r0 := @this: AnonymousDiamondOperator",
+            "$r1 = new java.io.BufferedReader",
+            "$r2 = new java.io.FileReader",
+            "specialinvoke $r2.<java.io.FileReader: void <init>(java.lang.String)>(\"file.txt\")",
+            "specialinvoke $r1.<java.io.BufferedReader: void <init>(java.io.Reader)>($r2)",
+            "$r3 = \"\"",
+            "label1:",
+            "$r4 = virtualinvoke $r1.<java.io.BufferedReader: java.lang.String readLine()>()",
+            "goto label2",
+            "$r5 := @caughtexception",
+            "virtualinvoke $r1.<java.io.BufferedReader: void close()>()",
+            "throw $r5",
+            "label2:",
+            "$r3 = $r4",
+            "$z0 = $r4 != null",
+            "if $z0 == 0 goto label3",
+            "$r6 = <java.lang.System: java.io.PrintStream out>",
+            "virtualinvoke $r6.<java.io.PrintStream: void println(java.lang.String)>($r3)",
+            "goto label1",
+            "label3:",
+            "virtualinvoke $r1.<java.io.BufferedReader: void close()>()",
+            "return")
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
+}
---
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java9/PrivateMethodInterfaceImplTest.java
@@ -0,0 +1,48 @@
+package de.upb.swt.soot.test.java.bytecode.minimaltestsuite.java9;
+
+import static org.junit.Assert.assertTrue;
+
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.bytecode.minimaltestsuite.MinimalBytecodeTestSuiteBase;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/** @author Kaustubh Kelkar */
+public class PrivateMethodInterfaceImplTest extends MinimalBytecodeTestSuiteBase {
+
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "methodInterfaceImpl", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Test
+  public void defaultTest() {}
+
+  @Ignore
+  /** TODO WALA does not support Java9 constructs */
+  public void ignoreTest() {
+    super.defaultTest();
+    SootClass sootClass = loadClass(getDeclaredClassSignature());
+    assertTrue(
+        sootClass.getInterfaces().stream()
+            .anyMatch(
+                javaClassType ->
+                    javaClassType.getClassName().equalsIgnoreCase("PrivateMethodInterface")));
+  }
+
+  @Override
+  public List<String> expectedBodyStmts() {
+    return Stream.of(
+            "r0 := @this: PrivateMethodInterfaceImpl",
+            "interfaceinvoke r0.<PrivateMethodInterface: void methodInterface(int,int)>(4, 2)",
+            "return")
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
+}
