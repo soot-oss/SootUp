89b1d2c6ed8c49ac3af3d4086624cd4a4ca18d9c
hasitha <herajapakse@gmail.com>
2019-11-29 11:17:47 +0100

    InheritDataWithPublicMethodTest

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/typehierarchy/testcase/InheritDataWithPublicMethodTest.java
@@ -0,0 +1,62 @@
+package de.upb.swt.soot.test.java.sourcecode.typehierarchy.testcase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import categories.Java8Test;
+import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
+import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
+import de.upb.swt.soot.core.model.Body;
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.model.SootMethod;
+import de.upb.swt.soot.core.types.ClassType;
+import de.upb.swt.soot.test.java.sourcecode.frontend.Utils;
+import de.upb.swt.soot.test.java.sourcecode.typehierarchy.JavaTypeHierarchyBase;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/** @author: Hasitha Rajapakse * */
+@Category(Java8Test.class)
+public class InheritDataWithPublicMethodTest extends JavaTypeHierarchyBase {
+  @Test
+  public void method() {
+    ViewTypeHierarchy typeHierarchy =
+        (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
+    ClassType sootClassType = getClassType(customTestWatcher.getClassName());
+
+    assertEquals(typeHierarchy.superClassOf(sootClassType), getClassType("SuperClass"));
+
+    SootClass sootClass =
+        (SootClass)
+            customTestWatcher
+                .getView()
+                .getClass(
+                    customTestWatcher
+                        .getView()
+                        .getIdentifierFactory()
+                        .getClassType(customTestWatcher.getClassName()))
+                .get();
+    SootMethod sootMethod =
+        sootClass
+            .getMethod(
+                identifierFactory.getMethodSignature(
+                    "method", sootClassType, "void", Collections.emptyList()))
+            .get();
+    Body body = sootMethod.getBody();
+    assertNotNull(body);
+
+    List<String> actualStmts = Utils.bodyStmtsAsStrings(body);
+    List<String> expectedStmts =
+        Stream.of(
+                "r0 := @this: InheritDataWithPublicMethod",
+                "$i0 = specialinvoke r0.<SuperClass: int getnum()>()",
+                "return")
+            .collect(Collectors.toList());
+
+    assertEquals(expectedStmts, actualStmts);
+  }
+}
