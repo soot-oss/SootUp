11d1bda5630f3e18c946152f07305cc295503dbf
Kaustubh Kelkar <kskelkar@mail.uni-paderborn.de>
2019-12-05 18:44:17 +0100

    testCase: declareConstructorWithEnum WIP

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumWithConstructorTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/DeclareEnumWithConstructorTest.java
@@ -23,11 +23,23 @@ public class DeclareEnumWithConstructorTest extends MinimalTestSuiteBase {
     return identifierFactory.getMethodSignature(
         "getValue", getDeclaredClassSignature(), "int", Collections.emptyList());
   }
+  public MethodSignature getMainMethodSignature() {
+    return identifierFactory.getMethodSignature(
+            "main", getDeclaredClassSignature(), "void", Collections.singletonList("String[]"));
+  }
+
+  public MethodSignature getMainMethodSignature1() {
+    return identifierFactory.getMethodSignature(
+            "main", identifierFactory.getClassType("DeclareEnumWithConstructor$Number"), "void", Collections.singletonList("String[]"));
+  }
 
   @Override
   public void defaultTest() {
     SootMethod sootMethod = loadMethod(getInitMethodSignature());
     assertJimpleStmts(sootMethod, expectedBodyStmts());
+
+    sootMethod = loadMethod(getMainMethodSignature());
+    assertJimpleStmts(sootMethod, expectedMainBodyStmts());
 
     /**
      * TODO sootClass.isEnum() return false as it checks for if the DeclareEnumConstructor class.
@@ -41,5 +53,13 @@ public class DeclareEnumWithConstructorTest extends MinimalTestSuiteBase {
             "specialinvoke r0.<java.lang.Object: void <init>()>()",
             "return")
         .collect(Collectors.toList());
+  }
+
+  public List<String> expectedMainBodyStmts() {
+    return Stream.of(
+            "r0 := @this: DeclareEnumWithConstructor",
+            "specialinvoke r0.<java.lang.Object: void <init>()>()",
+            "return")
+            .collect(Collectors.toList());
   }
 }
