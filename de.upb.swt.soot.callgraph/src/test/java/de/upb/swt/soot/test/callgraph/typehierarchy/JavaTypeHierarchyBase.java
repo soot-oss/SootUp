2f6ffc168db6dc2d6313739b9d8b2435679b72ec
hasitha <herajapakse@gmail.com>
2019-12-12 10:46:17 +0100

    move typehierarchy testcases from sourcecode module to callgraph module

--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/typehierarchy/JavaTypeHierarchyBase.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/typehierarchy/JavaTypeHierarchyBase.java
@@ -1,75 +1,74 @@
-//package de.upb.swt.soot.test.java.sourcecode.typehierarchy;
-//
-//import de.upb.swt.soot.core.inputlocation.AnalysisInputLocation;
-//import de.upb.swt.soot.java.core.JavaIdentifierFactory;
-//import de.upb.swt.soot.java.core.JavaProject;
-//import de.upb.swt.soot.java.core.language.JavaLanguage;
-//import de.upb.swt.soot.java.core.types.JavaClassType;
-//import de.upb.swt.soot.java.core.views.JavaView;
-//import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
-//import java.util.Collections;
-//import org.junit.ClassRule;
-//import org.junit.Test;
-//import org.junit.rules.TestWatcher;
-//import org.junit.runner.Description;
-//
-///** @author: Hasitha Rajapakse * */
-//public class JavaTypeHierarchyBase {
-//
-//  static final String baseDir = "src/test/resources/javatypehierarchy/";
-//  protected JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
-//
-//  @ClassRule public static CustomTestWatcher customTestWatcher = new CustomTestWatcher();
-//
-//  public static class CustomTestWatcher extends TestWatcher {
-//    private String className = JavaTypeHierarchyBase.class.getSimpleName();
-//    private AnalysisInputLocation srcCode;
-//    private JavaView view;
-//    private JavaProject project;
-//
-//    @Override
-//    protected void starting(Description description) {
-//      String prevClassName = getClassName();
-//      setClassName(extractClassName(description.getClassName()));
-//      if (!prevClassName.equals(getClassName())) {
-//        srcCode =
-//            new JavaSourcePathAnalysisInputLocation(
-//                Collections.singleton(baseDir + "/" + getClassName()));
-//        project = JavaProject.builder(new JavaLanguage(8)).addClassPath(this.srcCode).build();
-//        setView(project.createFullView());
-//      }
-//    }
-//
-//    public String getClassName() {
-//      return className;
-//    }
-//
-//    private void setClassName(String className) {
-//      this.className = className;
-//    }
-//
-//    private void setView(JavaView view) {
-//      this.view = view;
-//    }
-//
-//    public JavaView getView() {
-//      return view;
-//    }
-//  }
-//
-//  public JavaClassType getClassType(String className) {
-//    return identifierFactory.getClassType(className);
-//  }
-//
-//  public static String extractClassName(String classPath) {
-//    String classPathArray = classPath.substring(classPath.lastIndexOf(".") + 1);
-//    String testDirectoryName = "";
-//    if (!classPathArray.isEmpty()) {
-//      testDirectoryName = classPathArray.substring(0, classPathArray.length() - 4);
-//    }
-//    return testDirectoryName;
-//  }
-//
-//  @Test
-//  public void method() {}
-//}
+package de.upb.swt.soot.test.callgraph.typehierarchy;
+
+import de.upb.swt.soot.core.inputlocation.AnalysisInputLocation;
+import de.upb.swt.soot.java.core.JavaIdentifierFactory;
+import de.upb.swt.soot.java.core.JavaProject;
+import de.upb.swt.soot.java.core.language.JavaLanguage;
+import de.upb.swt.soot.java.core.types.JavaClassType;
+import de.upb.swt.soot.java.core.views.JavaView;
+import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import java.util.Collections;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+/** @author: Hasitha Rajapakse * */
+public class JavaTypeHierarchyBase {
+  static final String baseDir = "src/test/resources/javatypehierarchy/";
+  protected JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
+
+  @ClassRule public static CustomTestWatcher customTestWatcher = new CustomTestWatcher();
+
+  public static class CustomTestWatcher extends TestWatcher {
+    private String className = JavaTypeHierarchyBase.class.getSimpleName();
+    private AnalysisInputLocation srcCode;
+    private JavaView view;
+    private JavaProject project;
+
+    @Override
+    protected void starting(Description description) {
+      String prevClassName = getClassName();
+      setClassName(extractClassName(description.getClassName()));
+      if (!prevClassName.equals(getClassName())) {
+        srcCode =
+            new JavaSourcePathAnalysisInputLocation(
+                Collections.singleton(baseDir + "/" + getClassName()));
+        project = JavaProject.builder(new JavaLanguage(8)).addClassPath(this.srcCode).build();
+        setView(project.createFullView());
+      }
+    }
+
+    public String getClassName() {
+      return className;
+    }
+
+    private void setClassName(String className) {
+      this.className = className;
+    }
+
+    private void setView(JavaView view) {
+      this.view = view;
+    }
+
+    public JavaView getView() {
+      return view;
+    }
+  }
+
+  public JavaClassType getClassType(String className) {
+    return identifierFactory.getClassType(className);
+  }
+
+  public static String extractClassName(String classPath) {
+    String classPathArray = classPath.substring(classPath.lastIndexOf(".") + 1);
+    String testDirectoryName = "";
+    if (!classPathArray.isEmpty()) {
+      testDirectoryName = classPathArray.substring(0, classPathArray.length() - 4);
+    }
+    return testDirectoryName;
+  }
+
+  @Test
+  public void method() {}
+}
--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/typehierarchy/testcase/InterfaceInheritanceTest.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/typehierarchy/testcase/InterfaceInheritanceTest.java
@@ -1,29 +1,30 @@
-//package de.upb.swt.soot.test.java.sourcecode.typehierarchy.testcase;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import categories.Java8Test;
-//import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
-//import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
-//import de.upb.swt.soot.core.types.ClassType;
-//import de.upb.swt.soot.test.java.sourcecode.typehierarchy.JavaTypeHierarchyBase;
-//import java.util.HashSet;
-//import java.util.Set;
-//import org.junit.Test;
-//import org.junit.experimental.categories.Category;
-//
-///** @author: Hasitha Rajapakse * */
-//@Category(Java8Test.class)
-//public class InterfaceInheritanceTest extends JavaTypeHierarchyBase {
-//  @Test
-//  public void method() {
-//    ViewTypeHierarchy typeHierarchy =
-//        (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
-//    Set<ClassType> interfaceSet = new HashSet<>();
-//    interfaceSet.add(getClassType("InterfaceA"));
-//    interfaceSet.add(getClassType("InterfaceB"));
-//    assertEquals(
-//        typeHierarchy.implementedInterfacesOf(getClassType(customTestWatcher.getClassName())),
-//        interfaceSet);
-//  }
-//}
+package de.upb.swt.soot.test.callgraph.typehierarchy.testcase;
+
+import static org.junit.Assert.assertEquals;
+
+import categories.Java8Test;
+import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
+import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
+import de.upb.swt.soot.core.types.ClassType;
+import java.util.HashSet;
+import java.util.Set;
+
+import de.upb.swt.soot.test.callgraph.typehierarchy.JavaTypeHierarchyBase;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/** @author: Hasitha Rajapakse * */
+@Category(Java8Test.class)
+public class InterfaceInheritanceTest extends JavaTypeHierarchyBase {
+  @Test
+  public void method() {
+    ViewTypeHierarchy typeHierarchy =
+        (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
+    Set<ClassType> interfaceSet = new HashSet<>();
+    interfaceSet.add(getClassType("InterfaceA"));
+    interfaceSet.add(getClassType("InterfaceB"));
+    assertEquals(
+        typeHierarchy.implementedInterfacesOf(getClassType(customTestWatcher.getClassName())),
+        interfaceSet);
+  }
+}
