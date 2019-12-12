c287a18ac6f2af4659be7b0cb2999191c027d064
Markus Schmidt <markus262@web.de>
2019-12-12 16:07:16 +0100

    wip

---
+++ de.upb.swt.soot.jimple.parser/src/main/java/de/upb/swt/soot/JimpleReader.java
@@ -0,0 +1,12 @@
+package de.upb.swt.soot;
+
+import de.upb.swt.soot.core.IdentifierFactory;
+
+public class JimpleReader /* implements JimpleparserListener */ {
+
+  public void setIdentifierFactory(IdentifierFactory identifierFactoryClass) {
+    // TODO
+  }
+
+  public void parse(String s) {}
+}
