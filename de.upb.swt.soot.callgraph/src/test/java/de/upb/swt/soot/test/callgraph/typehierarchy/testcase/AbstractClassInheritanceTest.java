2fca7f84e5e2cab499e188804d43ced418956d8e
hasitha <herajapakse@gmail.com>
2019-12-12 10:44:19 +0100

    remove dependancy to callgraph module

---
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/typehierarchy/testcase/AbstractClassInheritanceTest.java
@@ -0,0 +1,40 @@
+//package de.upb.swt.soot.test.java.sourcecode.typehierarchy.testcase;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
+//
+//import categories.Java8Test;
+//import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
+//import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
+//import de.upb.swt.soot.core.model.SootClass;
+//import de.upb.swt.soot.test.java.sourcecode.typehierarchy.JavaTypeHierarchyBase;
+//import org.junit.Test;
+//import org.junit.experimental.categories.Category;
+//
+///** @author: Hasitha Rajapakse * */
+//@Category(Java8Test.class)
+//public class AbstractClassInheritanceTest extends JavaTypeHierarchyBase {
+//  @Test
+//  public void method() {
+//    SootClass sootClass =
+//        (SootClass)
+//            customTestWatcher
+//                .getView()
+//                .getClass(
+//                    customTestWatcher
+//                        .getView()
+//                        .getIdentifierFactory()
+//                        .getClassType(customTestWatcher.getClassName()))
+//                .get();
+//    assertTrue(sootClass.hasSuperclass());
+//    SootClass superClass =
+//        (SootClass) customTestWatcher.getView().getClass(sootClass.getSuperclass().get()).get();
+//    assertTrue(superClass.isAbstract());
+//
+//    ViewTypeHierarchy typeHierarchy =
+//        (ViewTypeHierarchy) TypeHierarchy.fromView(customTestWatcher.getView());
+//    assertEquals(
+//        typeHierarchy.superClassOf(getClassType(customTestWatcher.getClassName())),
+//        getClassType("AbstractClass"));
+//  }
+//}
