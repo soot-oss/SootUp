8e7a7a786ae2065ed8a84affe38179017b66ec93
Kaustubh Kelkar <kskelkar@mail.uni-paderborn.de>
2020-01-15 14:06:57 +0100

    1. Using equiVal() method 2. WIP debugging:  java.lang.RuntimeException: Failed to convert <Classname : methodname>

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/model/SootMethod.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/model/SootMethod.java
@@ -133,6 +133,8 @@ public class SootMethod extends SootClassMember<MethodSignature> implements Meth
   /** Retrieves the active body for this methodRef. */
   @Nullable
   public Body getBody() {
+    if( "".equalsIgnoreCase(this._lazyBody.get().toString()))
+      System.out.println("Lazy body for the method: \n"+this._lazyBody.get());
     return this._lazyBody.get(); // TODO: [JMP] Refactor to return `.getAsOptional()`
   }
 
