a89d77dfd0ee65d4a8b4927231ae53a4cbf8838f
Markus Schmidt <markus262@web.de>
2020-01-16 14:38:58 +0100

    add ignored test

---
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/αρετηTest.java
@@ -0,0 +1,43 @@
+package de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.java6;
+
+import categories.Java8Test;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.test.java.sourcecode.minimaltestsuite.MinimalSourceTestSuiteBase;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Ignore;
+import org.junit.experimental.categories.Category;
+
+/** @author Kaustubh Kelkar */
+@Category(Java8Test.class)
+public class αρετηTest extends MinimalSourceTestSuiteBase {
+  @Override
+  public MethodSignature getMethodSignature() {
+    return identifierFactory.getMethodSignature(
+        "αρετηAsClassName", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  @Override
+  public void defaultTest() {}
+
+  @Ignore
+  public void ignoreTest() {
+    // FIXME: fails (due to missing unicode support?)
+  }
+
+  @Override
+  public List<String> expectedBodyStmts() {
+    // TODO: likely the Jimple lines need a change when it works until here (remove next line(assert
+    // false) then)
+    assert false;
+
+    return Stream.of(
+            "l0 := @this: \\u03b1\\u03c1\\u03b5\\u03c4\\u03b7",
+            "$stack1 = <java.lang.System: java.io.PrintStream; out>",
+            "virtualinvoke $stack1.<java.io.PrintStream: void println(java.lang.String)>(\"this is \\u03b1\\u03c1\\u03b5\\u03c4\\u03b7 class\")",
+            "return")
+        .collect(Collectors.toList());
+  }
+}
