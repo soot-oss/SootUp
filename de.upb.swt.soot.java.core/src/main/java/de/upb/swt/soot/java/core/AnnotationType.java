a3ef8d074394d5cc3de6fa0bc1eb71d2b35c5924
Markus Schmidt <markus262@web.de>
2020-01-29 15:37:04 +0100

    annotations type created for java.core

---
+++ de.upb.swt.soot.java.core/src/main/java/de/upb/swt/soot/java/core/AnnotationType.java
@@ -0,0 +1,25 @@
+package de.upb.swt.soot.java.core;
+
+import de.upb.swt.soot.core.signatures.PackageName;
+import de.upb.swt.soot.java.core.types.JavaClassType;
+
+/**
+ * convenience base class to represent Java Annotations (JSR-175)
+ *
+ * @author Markus Schmidt
+ */
+
+// TODO:[ms] move to a better place
+public class AnnotationType extends JavaClassType {
+
+  /**
+   * Internal: Constructs the fully-qualified ClassSignature. Instances should only be created by a
+   * {@link IdentifierFactory}
+   *
+   * @param className the simple name of the class, e.g., ClassA NOT my.package.ClassA
+   * @param packageName the corresponding package
+   */
+  public AnnotationType(String className, PackageName packageName) {
+    super("@interface_" + className, packageName);
+  }
+}
