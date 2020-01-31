f6e7ea78a96426ef69efe44695903bf6d4d97620
Kaustubh Kelkar <kskelkar@mail.uni-paderborn.de>
2020-01-31 18:52:41 +0100

    InitializeArraysWhileDeclaration fixed

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java6/InitializeArraysWhileDeclarationTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java6/InitializeArraysWhileDeclarationTest.java
@@ -18,97 +18,106 @@ public class InitializeArraysWhileDeclarationTest extends MinimalBytecodeTestSui
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (int[])[3]",
-            "$r1[0] = 1",
-            "$r1[1] = 2",
-            "$r1[2] = 3",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (int)[3]",
+            "$stack2[0] = 1",
+            "$stack2[1] = 2",
+            "$stack2[2] = 3",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("byteArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (byte[])[3]",
-            "$r1[0] = 4",
-            "$r1[1] = 5",
-            "$r1[2] = 6",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (byte)[3]",
+            "$stack2[0] = 4",
+            "$stack2[1] = 5",
+            "$stack2[2] = 6",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("shortArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (short[])[3]",
-            "$r1[0] = 10",
-            "$r1[1] = 20",
-            "$r1[2] = 30",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (short)[3]",
+            "$stack2[0] = 10",
+            "$stack2[1] = 20",
+            "$stack2[2] = 30",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("longArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (long[])[3]",
-            "$r1[0] = 547087L",
-            "$r1[1] = 564645L",
-            "$r1[2] = 654786L",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (long)[3]",
+            "$stack2[0] = 547087L",
+            "$stack2[1] = 564645L",
+            "$stack2[2] = 654786L",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("floatArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (float[])[4]",
-            "$r1[0] = 3.14F",
-            "$r1[1] = 5.46F",
-            "$r1[2] = 2.987F",
-            "$r1[3] = 4.87F",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (float)[4]",
+            "$stack2[0] = 3.14F",
+            "$stack2[1] = 5.46F",
+            "$stack2[2] = 2.987F",
+            "$stack2[3] = 4.87F",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("doubleArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (double[])[2]",
-            "$r1[0] = 6.765414",
-            "$r1[1] = 9.676565646",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (double)[2]",
+            "$stack2[0] = 6.765414",
+            "$stack2[1] = 9.676565646",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("booleanArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (boolean[])[2]",
-            "$r1[0] = 1",
-            "$r1[1] = 0",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (boolean)[2]",
+            "$stack2[0] = 1",
+            "$stack2[1] = 0",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("charArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (char[])[3]",
-            "$r1[0] = 65",
-            "$r1[1] = 98",
-            "$r1[2] = 38",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (char)[3]",
+            "$stack2[0] = 65",
+            "$stack2[1] = 98",
+            "$stack2[2] = 38",
+            "l1 = $stack2",
             "return"));
 
     method = loadMethod(getMethodSignature("stringArrays"));
     assertJimpleStmts(
         method,
         expectedBodyStmts(
-            "r0 := @this: InitializeArraysWhileDeclaration",
-            "$r1 = newarray (java.lang.String[])[2]",
-            "$r1[0] = \"Hello World\"",
-            "$r1[1] = \"Greetings\"",
+            "l0 := @this: InitializeArraysWhileDeclaration",
+            "$stack2 = newarray (java/lang/String)[2]",
+            "$stack2[0] = \"Hello World\"",
+            "$stack2[1] = \"Greetings\"",
+            "l1 = $stack2",
             "return"));
   }
 
