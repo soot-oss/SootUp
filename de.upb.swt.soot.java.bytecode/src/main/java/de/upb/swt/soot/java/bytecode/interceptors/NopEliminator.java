7550a7fdc998bf99f596fb6fe3753923e49e1bee
marcusna <marcus.nachtigall@upb.de>
2020-02-06 13:45:44 +0100

    implement NopEliminator

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/NopEliminator.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/NopEliminator.java
@@ -1,17 +1,47 @@
 package de.upb.swt.soot.java.bytecode.interceptors;
 
+import de.upb.swt.soot.core.jimple.basic.Trap;
+import de.upb.swt.soot.core.jimple.common.stmt.JNopStmt;
+import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.Body;
 import de.upb.swt.soot.core.transform.BodyInterceptor;
+
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 // https://github.com/Sable/soot/blob/master/src/main/java/soot/jimple/toolkits/scalar/NopEliminator.java
 
 public class NopEliminator implements BodyInterceptor {
 
+  /**
+   * Removes {@link JNopStmt}s from the passed body.
+   * @param originalBody The current body before transformation.
+   * @return The transformed body.
+   */
   @Nonnull
   @Override
   public Body interceptBody(@Nonnull Body originalBody) {
-    // TODO Implement
-    return originalBody;
+
+    List<Stmt> stmtList = originalBody.getStmts();
+    List<Stmt> copyList = new ArrayList<>();
+
+    for(Stmt stmt : stmtList){
+      if(stmt instanceof JNopStmt){
+        boolean keepNop = false;
+        if(stmtList.get(stmtList.size()-1) == stmt){
+          for(Trap trap : originalBody.getTraps()){
+            if(trap.getEndStmt() == stmt){
+              keepNop = true;
+            }
+          }
+        }
+        if(keepNop){
+          copyList.add(stmt);
+        }
+      }
+    }
+
+    return originalBody.withStmts(copyList);
   }
 }
