1be901150197fd6a63563c64492476d6aa3003b5
marcusna <marcus.nachtigall@upb.de>
2020-02-14 16:08:45 +0100

    fix style

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/NopEliminatorTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/NopEliminatorTest.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
