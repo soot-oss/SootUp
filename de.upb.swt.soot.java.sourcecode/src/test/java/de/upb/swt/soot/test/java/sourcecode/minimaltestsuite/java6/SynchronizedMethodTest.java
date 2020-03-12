4b4e7d06dd8a849dca50c85e659f7b59c8203e6e
Markus Schmidt <markus262@web.de>
2020-03-12 01:17:28 +0100

    re-ignore test

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/SynchronizedMethodTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java6/SynchronizedMethodTest.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.junit.Test;
+import org.junit.Ignore;
 
 public class SynchronizedMethodTest extends MinimalSourceTestSuiteBase {
 
@@ -19,7 +19,7 @@ public class SynchronizedMethodTest extends MinimalSourceTestSuiteBase {
         "run", getDeclaredClassSignature(), "void", Collections.emptyList());
   }
 
-  @Test
+  @Ignore
   public void test() {
     SootMethod method = loadMethod(getMethodSignature());
     assertJimpleStmts(method, expectedBodyStmts());
