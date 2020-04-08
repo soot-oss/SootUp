a41ded1085edb8c8098a28ef24ae9b331ca6ce4a
marcusna <marcus.nachtigall@upb.de>
2020-04-08 12:50:08 +0200

    fix style

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/validation/LocalsValidator.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/validation/LocalsValidator.java
@@ -11,20 +11,20 @@ public class LocalsValidator implements BodyValidator {
   @Override
   public void validate(Body body, List<ValidationException> exception) {
     for (Value v : body.getUses()) {
-        validateLocal(body, v, exception);
+      validateLocal(body, v, exception);
     }
-      for (Value v : body.getDefs()) {
-          validateLocal(body, v, exception);
-      }
+    for (Value v : body.getDefs()) {
+      validateLocal(body, v, exception);
+    }
   }
 
   private void validateLocal(Body body, Value v, List<ValidationException> exception) {
     Value value;
     if ((value = v) instanceof Local) {
       if (!body.getLocals().contains(value)) {
-          exception.add(
-                  new ValidationException(
-                          value, "Local not in chain : " + value + " in " + body.getMethod()));
+        exception.add(
+            new ValidationException(
+                value, "Local not in chain : " + value + " in " + body.getMethod()));
       }
     }
   }
