287212d226f06d8b2e39c0c0dc73da2c3e2387d5
marcusna <marcus.nachtigall@upb.de>
2020-04-27 16:48:22 +0200

    format style

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/DuplicateCatchAllTrapRemover.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/DuplicateCatchAllTrapRemover.java
@@ -4,10 +4,10 @@ import de.upb.swt.soot.core.jimple.basic.Trap;
 import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.Body;
 import de.upb.swt.soot.core.transform.BodyInterceptor;
-import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import javax.annotation.Nonnull;
 
 /**
  * Some compilers generate duplicate traps:
@@ -35,23 +35,29 @@ public class DuplicateCatchAllTrapRemover implements BodyInterceptor {
   @Nonnull
   @Override
   public Body interceptBody(@Nonnull Body originalBody) {
-    //Find two traps that use java.lang.Throwable as their type and that span the same code region
+    // Find two traps that use java.lang.Throwable as their type and that span the same code region
     Collection<Trap> originalTraps = originalBody.getTraps();
     List<Trap> traps = new ArrayList<>(originalBody.getTraps());
-    for (Trap trap1 : originalTraps){
-      if(trap1.getExceptionType().getClassName().equals("java.lang.Throwable")){
-        for (Trap trap2 : originalTraps){
-          if(trap1 != trap2 && trap1.getBeginStmt() == trap2.getBeginStmt() && trap1.getEndStmt() == trap2.getEndStmt()
-            && trap2.getExceptionType().getClassName().equals("java.lang.Throwable")){
+    for (Trap trap1 : originalTraps) {
+      if (trap1.getExceptionType().getClassName().equals("java.lang.Throwable")) {
+        for (Trap trap2 : originalTraps) {
+          if (trap1 != trap2
+              && trap1.getBeginStmt() == trap2.getBeginStmt()
+              && trap1.getEndStmt() == trap2.getEndStmt()
+              && trap2.getExceptionType().getClassName().equals("java.lang.Throwable")) {
             // Both traps (t1, t2) span the same code and catch java.lang.Throwable.
             // Check if one trap jumps to a target that then jumps to the target of the other trap
-            for(Trap trap3 : traps){
-              if(trap3 != trap1 && trap3 != trap2 && trap3.getExceptionType().getClassName().equals("java.lang.Throwable")){
-                if(trapCoversStmt(originalBody, trap3, trap1.getHandlerStmt()) && trap3.getHandlerStmt() == trap2.getHandlerStmt()){
+            for (Trap trap3 : traps) {
+              if (trap3 != trap1
+                  && trap3 != trap2
+                  && trap3.getExceptionType().getClassName().equals("java.lang.Throwable")) {
+                if (trapCoversStmt(originalBody, trap3, trap1.getHandlerStmt())
+                    && trap3.getHandlerStmt() == trap2.getHandlerStmt()) {
                   // c -> t1 -> t3 -> t2 && x -> t2
                   traps.remove(trap2);
                   break;
-                } else if (trapCoversStmt(originalBody, trap3, trap2.getHandlerStmt()) && trap3.getHandlerStmt() == trap1.getHandlerStmt()){
+                } else if (trapCoversStmt(originalBody, trap3, trap2.getHandlerStmt())
+                    && trap3.getHandlerStmt() == trap1.getHandlerStmt()) {
                   // c -> t2 -> t3 -> t1 && c -> t1
                   traps.remove(trap1);
                   break;
@@ -62,24 +68,25 @@ public class DuplicateCatchAllTrapRemover implements BodyInterceptor {
         }
       }
     }
-    System.out.println("Size of traps after transformation: " + traps.size());
     return originalBody.withTraps(traps);
   }
 
   /**
-   * Checks whether the given trap covers the given unit, i.e., there is an exceptional control flow from the given unit
-   * to the given trap
+   * Checks whether the given trap covers the given unit, i.e., there is an exceptional control flow
+   * from the given unit to the given trap
    *
    * @param body The body containing the stmt and the trap
    * @param trap The trap
    * @param stmt The unit
    * @return True if there can be an exceptional control flow from the given unit to the given trap
    */
-  private boolean trapCoversStmt(Body body, Trap trap, Stmt stmt){
+  private boolean trapCoversStmt(Body body, Trap trap, Stmt stmt) {
     List<Stmt> bodyStmts = body.getStmts();
-    List<Stmt> sequence = bodyStmts.subList(bodyStmts.indexOf(trap.getBeginStmt()), bodyStmts.indexOf(trap.getEndStmt()));
-    for(Stmt st : sequence){
-      if(st == stmt){
+    List<Stmt> sequence =
+        bodyStmts.subList(
+            bodyStmts.indexOf(trap.getBeginStmt()), bodyStmts.indexOf(trap.getEndStmt()));
+    for (Stmt st : sequence) {
+      if (st == stmt) {
         return true;
       }
     }
--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/DuplicateCatchAllTrapRemoverTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/DuplicateCatchAllTrapRemoverTest.java
@@ -1,5 +1,7 @@
 package de.upb.swt.soot.test.java.bytecode.interceptors;
 
+import static org.junit.Assert.*;
+
 import categories.Java8Test;
 import de.upb.swt.soot.core.jimple.basic.JTrap;
 import de.upb.swt.soot.core.jimple.basic.Local;
@@ -15,109 +17,108 @@ import de.upb.swt.soot.java.bytecode.interceptors.UnusedLocalEliminator;
 import de.upb.swt.soot.java.core.JavaIdentifierFactory;
 import de.upb.swt.soot.java.core.language.JavaJimple;
 import de.upb.swt.soot.java.core.types.JavaClassType;
+import java.util.*;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-
-import java.util.*;
-
-import static org.junit.Assert.*;
 
 @Category(Java8Test.class)
 public class DuplicateCatchAllTrapRemoverTest {
 
-    /** Tests the correct handling of an empty {@link Body}. */
-    @Test
-    public void testNoInput(){
-        Set<Local> locals = Collections.emptySet();
-        List<Trap> traps = Collections.emptyList();
-        List<Stmt> stmts = Collections.emptyList();
-        Body originalBody = new Body(locals, traps, stmts, null);
-        Body processedBody = new UnusedLocalEliminator().interceptBody(originalBody);
+  /** Tests the correct handling of an empty {@link Body}. */
+  @Test
+  public void testNoInput() {
+    Set<Local> locals = Collections.emptySet();
+    List<Trap> traps = Collections.emptyList();
+    List<Stmt> stmts = Collections.emptyList();
+    Body originalBody = new Body(locals, traps, stmts, null);
+    Body processedBody = new UnusedLocalEliminator().interceptBody(originalBody);
 
-        assertNotNull(processedBody);
-        assertArrayEquals(originalBody.getTraps().toArray(), processedBody.getTraps().toArray());
+    assertNotNull(processedBody);
+    assertArrayEquals(originalBody.getTraps().toArray(), processedBody.getTraps().toArray());
+  }
+
+  /** Test the correct removal of duplicate catch all traps. */
+  @Test
+  public void testRemoveDuplicate() {
+    Body originalBody = createBody(true);
+    Body processedBody = new DuplicateCatchAllTrapRemover().interceptBody(originalBody);
+
+    Collection<Trap> originalTraps = originalBody.getTraps();
+    Collection<Trap> processedTraps = processedBody.getTraps();
+
+    assertEquals(3, originalTraps.size());
+    assertEquals(2, processedTraps.size());
+    for (Trap trap : processedTraps) {
+      assertTrue(originalTraps.contains(trap));
+    }
+  }
+
+  /** Tests the correct handling of a {@link Body} without duplicate catch all traps. */
+  @Test
+  public void testRemoveNothing() {
+    Body originalBody = createBody(false);
+    Body processedBody = new DuplicateCatchAllTrapRemover().interceptBody(originalBody);
+
+    assertArrayEquals(originalBody.getTraps().toArray(), processedBody.getTraps().toArray());
+  }
+
+  /**
+   * Creates the {@link Body} for each test case. Depending on the parameter, it adds 0 or 1 Traps
+   * that should be removed
+   *
+   * @param containsDuplicate determines whether the Body contains a Trap that should be removed
+   * @return the created Body
+   */
+  private Body createBody(boolean containsDuplicate) {
+    JavaIdentifierFactory factory = JavaIdentifierFactory.getInstance();
+    JavaJimple javaJimple = JavaJimple.getInstance();
+    StmtPositionInfo noPositionInfo = StmtPositionInfo.createNoStmtPositionInfo();
+
+    JavaClassType objectType = factory.getClassType("java.lang.Object");
+    JavaClassType stringType = factory.getClassType("java.lang.String");
+    Local a = JavaJimple.newLocal("a", objectType);
+    Local b = JavaJimple.newLocal("b", stringType);
+    Set<Local> locals = ImmutableUtils.immutableSet(a, b);
+
+    Stmt strToA = JavaJimple.newAssignStmt(a, javaJimple.newStringConstant("str"), noPositionInfo);
+    Stmt bToA = JavaJimple.newAssignStmt(b, JavaJimple.newCastExpr(a, stringType), noPositionInfo);
+    Stmt ret = JavaJimple.newReturnStmt(b, noPositionInfo);
+    Stmt jump = JavaJimple.newGotoStmt(bToA, noPositionInfo);
+
+    List<Trap> traps = new ArrayList<>();
+    ExceptionType exceptionType = new ExceptionType();
+    Trap trap1 = new JTrap(exceptionType, strToA, bToA, jump);
+    traps.add(trap1);
+    Trap trap2 = new JTrap(exceptionType, strToA, bToA, ret);
+    traps.add(trap2);
+    if (containsDuplicate) {
+      Trap trap3 = new JTrap(exceptionType, strToA, bToA, ret);
+      traps.add(trap3);
+    }
+    List<Stmt> stmts = ImmutableUtils.immutableList(strToA, jump, bToA, ret);
+
+    return new Body(locals, traps, stmts, null);
+  }
+
+  private static class ExceptionType extends ClassType {
+    @Override
+    public boolean isBuiltInClass() {
+      return false;
     }
 
-    /** Test the correct removal of duplicate catch all traps. */
-    @Test
-    public void testRemoveDuplicate(){
-        Body originalBody = createBody(true);
-        Body processedBody = new DuplicateCatchAllTrapRemover().interceptBody(originalBody);
-
-        Collection<Trap> originalTraps = originalBody.getTraps();
-        Collection<Trap> processedTraps = processedBody.getTraps();
-
-        assertEquals(3, originalTraps.size());
-        assertEquals(2, processedTraps.size());
-        for(Trap trap : processedTraps){
-            assertTrue(originalTraps.contains(trap));
-        }
+    @Override
+    public String getFullyQualifiedName() {
+      return null;
     }
 
-    /** Tests the correct handling of a {@link Body} without duplicate catch all traps. */
-    @Test
-    public void testRemoveNothing(){
-        Body originalBody = createBody(false);
-        Body processedBody = new DuplicateCatchAllTrapRemover().interceptBody(originalBody);
-
-        assertArrayEquals(originalBody.getTraps().toArray(), processedBody.getTraps().toArray());
+    @Override
+    public String getClassName() {
+      return "java.lang.Throwable";
     }
 
-    /**
-     * Creates the {@link Body} for each test case. Depending on the parameter, it adds 0 or 1 Traps that should be removed
-     * @param containsDuplicate  determines whether the Body contains a Trap that should be removed
-     * @return the created Body
-     */
-    private Body createBody(boolean containsDuplicate){
-        JavaIdentifierFactory factory = JavaIdentifierFactory.getInstance();
-        JavaJimple javaJimple = JavaJimple.getInstance();
-        StmtPositionInfo noPositionInfo = StmtPositionInfo.createNoStmtPositionInfo();
-
-        JavaClassType objectType = factory.getClassType("java.lang.Object");
-        JavaClassType stringType = factory.getClassType("java.lang.String");
-        Local a = JavaJimple.newLocal("a", objectType);
-        Local b = JavaJimple.newLocal("b", stringType);
-        Set<Local> locals = ImmutableUtils.immutableSet(a, b);
-
-        Stmt strToA = JavaJimple.newAssignStmt(a, javaJimple.newStringConstant("str"), noPositionInfo);
-        Stmt bToA = JavaJimple.newAssignStmt(b, JavaJimple.newCastExpr(a, stringType), noPositionInfo);
-        Stmt ret = JavaJimple.newReturnStmt(b, noPositionInfo);
-        Stmt jump = JavaJimple.newGotoStmt(bToA, noPositionInfo);
-
-        List<Trap> traps = new ArrayList<>();
-        ExceptionType exceptionType = new ExceptionType();
-        Trap trap1 = new JTrap(exceptionType, strToA, bToA, jump);
-        traps.add(trap1);
-        Trap trap2 = new JTrap(exceptionType, strToA, bToA, ret);
-        traps.add(trap2);
-        if(containsDuplicate){
-            Trap trap3 = new JTrap(exceptionType, strToA, bToA, ret);
-            traps.add(trap3);
-        }
-        List<Stmt> stmts = ImmutableUtils.immutableList(strToA, jump, bToA, ret);
-
-        return new Body(locals, traps, stmts, null);
+    @Override
+    public PackageName getPackageName() {
+      return null;
     }
-
-    private static class ExceptionType extends ClassType{
-        @Override
-        public boolean isBuiltInClass() {
-            return false;
-        }
-
-        @Override
-        public String getFullyQualifiedName() {
-            return null;
-        }
-
-        @Override
-        public String getClassName() {
-            return "java.lang.Throwable";
-        }
-
-        @Override
-        public PackageName getPackageName() {
-            return null;
-        }
-    }
+  }
 }
