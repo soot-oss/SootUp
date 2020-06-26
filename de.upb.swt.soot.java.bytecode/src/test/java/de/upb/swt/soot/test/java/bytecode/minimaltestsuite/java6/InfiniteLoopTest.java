b1f5a6c4eb588e27d5da1901c070399637f5cd71
Markus Schmidt <markus262@web.de>
2020-06-26 18:09:55 +0200

    cleanup selfjump test

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java6/InfiniteLoopTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java6/InfiniteLoopTest.java
@@ -3,7 +3,6 @@ package de.upb.swt.soot.test.java.bytecode.minimaltestsuite.java6;
 import categories.Java8Test;
 import de.upb.swt.soot.core.model.SootMethod;
 import de.upb.swt.soot.core.signatures.MethodSignature;
-import de.upb.swt.soot.core.util.Utils;
 import de.upb.swt.soot.test.java.bytecode.minimaltestsuite.MinimalBytecodeTestSuiteBase;
 import java.util.Collections;
 import java.util.List;
@@ -29,7 +28,6 @@ public class InfiniteLoopTest extends MinimalBytecodeTestSuiteBase {
   @Test
   public void test() {
     SootMethod method = loadMethod(getMethodSignature());
-    Utils.printJimpleForTest(method);
     assertJimpleStmts(method, expectedBodyStmts());
   }
 }
