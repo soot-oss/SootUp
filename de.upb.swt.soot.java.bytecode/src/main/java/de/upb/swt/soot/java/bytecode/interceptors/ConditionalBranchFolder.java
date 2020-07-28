5b697894107f9875b341774750dd97b55d22db2b
marcusna <marcus.nachtigall@upb.de>
2020-07-28 15:37:10 +0200

    add ConditionalBranchFolderTest

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/ConditionalBranchFolder.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/ConditionalBranchFolder.java
@@ -24,20 +24,24 @@ public class ConditionalBranchFolder implements BodyInterceptor {
     Body.BodyBuilder builder = Body.builder(originalBody);
     Set<Stmt> nodes = originalBody.getStmtGraph().nodes();
 
-    for(Stmt stmt : nodes){
-      if(stmt instanceof JIfStmt){
+    for (Stmt stmt : nodes) {
+      if (stmt instanceof JIfStmt) {
         JIfStmt ifStmt = (JIfStmt) stmt;
         // check for constant-valued conditions
         Value condition = ifStmt.getCondition();
-        if(Evaluator.isValueConstantValued(condition)){
+        if (Evaluator.isValueConstantValued(condition)) {
           condition = Evaluator.getConstantValueOf(condition);
 
-          if(((IntConstant) condition).getValue() == 1){
-            // if condition always true, redirect all predecessors to the successor of the if-statements
+          if (((IntConstant) condition).getValue() == 1) {
+            // if condition always true, redirect all predecessors to the successor of the
+            // if-statements
             Stmt nextStmt = ifStmt.getTarget(originalBody);
-            for(Stmt predecessor : originalBody.getStmtGraph().predecessors(ifStmt)){
+            for (Stmt predecessor : originalBody.getStmtGraph().predecessors(ifStmt)) {
               builder.addFlow(predecessor, nextStmt);
               builder.removeFlow(predecessor, ifStmt);
+            }
+            for (Stmt successor : originalBody.getStmtGraph().successors(ifStmt)) {
+              builder.removeFlow(ifStmt, successor);
             }
           }
         }
