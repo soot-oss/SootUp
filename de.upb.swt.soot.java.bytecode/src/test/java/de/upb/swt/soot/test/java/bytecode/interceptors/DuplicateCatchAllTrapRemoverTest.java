cf82b2b08e5a7736c1768907484b8e7c458663f0
marcusna <marcus.nachtigall@upb.de>
2020-08-06 11:18:57 +0200

    add authors

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/DuplicateCatchAllTrapRemoverTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/DuplicateCatchAllTrapRemoverTest.java
@@ -19,9 +19,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-/**
- * @author Marcus Nachtigall
- */
+/** @author Marcus Nachtigall */
 @Category(Java8Test.class)
 public class DuplicateCatchAllTrapRemoverTest {
 
