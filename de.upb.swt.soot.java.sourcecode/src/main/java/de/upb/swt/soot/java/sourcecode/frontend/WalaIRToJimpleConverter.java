a2a790706527bd90da489f1deaae5044bb390cea
wangzun66 <wangzun666@gmail.com>
2020-08-18 22:53:32 +0200

    last part

--- de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
+++ de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
@@ -3,7 +3,7 @@ package de.upb.swt.soot.java.sourcecode.frontend;
  * #%L
  * Soot - a J*va Optimization Framework
  * %%
- * Copyright (C) 2019 Linghui Luo
+ * Copyright (C) 2018-2020 Linghui Luo, Christian Brüggemann, Markus Schmidt
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- de.upb.swt.soot.tests/src/main/java/SimpleSootClient.java
+++ de.upb.swt.soot.tests/src/main/java/SimpleSootClient.java
@@ -2,7 +2,7 @@
  * #%L
  * Soot - a J*va Optimization Framework
  * %%
- * Copyright (C) 2019 Linghui Luo, Ben Hermann
+ * Copyright (C) 2018-2020 Linghui Luo, Ben Hermann, Markus Schmidt and others
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
