a3386feb815a9cece78b850af7e6ffed910087c6
Kadiray Karakaya <kadiray.k@hotmail.com>
2020-10-16 15:52:03 +0200

    added license headers

--- de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/ClassHierarchyAlgorithm.java
+++ de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/ClassHierarchyAlgorithm.java
@@ -1,5 +1,27 @@
 package de.upb.swt.soot.callgraph;
 
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2019-2020 Linghui Luo, Christian Brüggemann, Ben Hermann, Markus Schmidt
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import de.upb.swt.soot.callgraph.typehierarchy.MethodDispatchResolver;
 import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
 import de.upb.swt.soot.core.frontend.AbstractClassSource;
@@ -23,13 +45,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 
-/**
- * This class implements CHA (Class Hierarchy Algorithm)
- *
- * @author Markus Schmidt
- * @author Christian Brüggemann
- * @author Ben Hermann
- */
 public class ClassHierarchyAlgorithm extends AbstractCallGraphAlgorithm {
   @Nonnull private final View view;
   @Nonnull private final TypeHierarchy hierarchy;
