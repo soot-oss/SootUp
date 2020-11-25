31a5ed06ffd82eac395837cfe2008543f62b757e
wangzun66 <wangzun666@gmail.com>
2020-11-25 12:40:18 +0100

    update ReplaceUseRefVisitor and its tests

--- de.upb.swt.soot.tests/src/test/java/de/upb/swt/soot/test/ReplaceUseRefVisitorTest.java
+++ de.upb.swt.soot.tests/src/test/java/de/upb/swt/soot/test/ReplaceUseRefVisitorTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import categories.Java8Test;
 import de.upb.swt.soot.core.jimple.basic.Local;
 import de.upb.swt.soot.core.jimple.basic.Value;
+import de.upb.swt.soot.core.jimple.common.constant.Constant;
+import de.upb.swt.soot.core.jimple.common.constant.IntConstant;
 import de.upb.swt.soot.core.jimple.common.ref.Ref;
 import de.upb.swt.soot.core.jimple.visitor.ReplaceUseRefVisitor;
 import de.upb.swt.soot.core.signatures.FieldSignature;
@@ -24,9 +26,15 @@ public class ReplaceUseRefVisitorTest {
   JavaClassType intType = factory.getClassType("int");
   JavaClassType arrayType = factory.getClassType("Array");
 
-  Local base = JavaJimple.newLocal("base", arrayType);
-  Local index = JavaJimple.newLocal("index", intType);
-  Local newUse = JavaJimple.newLocal("newUse", intType);
+  Local base = JavaJimple.newLocal("old_base", arrayType);
+  Local newBase = JavaJimple.newLocal("new_base", arrayType);
+
+  Constant conIndex = IntConstant.getInstance(0);
+  Constant conNewIndex = IntConstant.getInstance(1);
+
+  Local localIndex = JavaJimple.newLocal("index", intType);
+  Local localNewIndex = JavaJimple.newLocal("newIndex", intType);
+
 
   FieldSignature fieldSignature = new FieldSignature(arrayType, "field", intType);
 
@@ -35,27 +43,42 @@ public class ReplaceUseRefVisitorTest {
   public void testCaseArrayRef() {
 
     // replace base with newUse
-    ReplaceUseRefVisitor visitor = new ReplaceUseRefVisitor(base, newUse);
-    Ref ref = javaJimple.newArrayRef(base, index);
+    ReplaceUseRefVisitor visitor = new ReplaceUseRefVisitor(base, newBase);
+    Ref ref = javaJimple.newArrayRef(base, conIndex);
     ref.accept(visitor);
     Ref newRef = visitor.getNewRef();
 
     List<Value> expectedUses = new ArrayList<>();
-    expectedUses.add(newUse);
-    expectedUses.add(index);
+    expectedUses.add(newBase);
+    expectedUses.add(conIndex);
 
     assertTrue(newRef.getUses().equals(expectedUses));
+    expectedUses.clear();
 
-    // replace base two times
-    ref = javaJimple.newArrayRef(base, base);
+    // replace constant index with newUse
+    visitor = new ReplaceUseRefVisitor(conIndex, conNewIndex);
+    ref = javaJimple.newArrayRef(base, conIndex);
     ref.accept(visitor);
     newRef = visitor.getNewRef();
 
-    expectedUses.set(1, newUse);
+    expectedUses.add(base);
+    expectedUses.add(conNewIndex);
     assertTrue(newRef.getUses().equals(expectedUses));
+    expectedUses.clear();
+
+    //replace local index with newUse
+    visitor = new ReplaceUseRefVisitor(localIndex, localNewIndex);
+    ref = javaJimple.newArrayRef(base, localIndex);
+    ref.accept(visitor);
+    newRef = visitor.getNewRef();
+
+    expectedUses.add(base);
+    expectedUses.add(localNewIndex);
+    assertTrue(newRef.getUses().equals(expectedUses));
+    expectedUses.clear();
 
     // no matched use
-    ref = javaJimple.newArrayRef(index, index);
+    ref = javaJimple.newArrayRef(base, conIndex);
     ref.accept(visitor);
     newRef = visitor.getNewRef();
 
@@ -66,7 +89,7 @@ public class ReplaceUseRefVisitorTest {
   @Test
   public void testCaseInstanceFieldRef() {
 
-    ReplaceUseRefVisitor visitor = new ReplaceUseRefVisitor(base, newUse);
+    ReplaceUseRefVisitor visitor = new ReplaceUseRefVisitor(base, newBase);
 
     // replace base with newUse
     Ref ref = JavaJimple.newInstanceFieldRef(base, fieldSignature);
@@ -74,12 +97,12 @@ public class ReplaceUseRefVisitorTest {
     Ref newRef = visitor.getNewRef();
 
     List<Value> expectedUses = new ArrayList<>();
-    expectedUses.add(newUse);
+    expectedUses.add(newBase);
 
     assertTrue(newRef.getUses().equals(expectedUses));
 
     // no matched use
-    ref = JavaJimple.newInstanceFieldRef(index, fieldSignature);
+    ref = JavaJimple.newInstanceFieldRef(localIndex, fieldSignature);
     ref.accept(visitor);
     newRef = visitor.getNewRef();
 
