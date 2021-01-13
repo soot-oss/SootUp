f63c8d3f95e17f8c5d0db64f3d96ecede909f87a
wangzun66 <wangzun666@gmail.com>
2021-01-14 00:23:51 +0100

    1. version of UnreachableCodeEliminator

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/UnreachableCodeEliminator.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/UnreachableCodeEliminator.java
@@ -3,7 +3,7 @@ package de.upb.swt.soot.java.bytecode.interceptors;
  * #%L
  * Soot - a J*va Optimization Framework
  * %%
- * Copyright (C) 1997-2020 Raja Vallée-Rai, Christian Brüggemann
+ * Copyright (C) 1997-2020 Raja Vallée-Rai, Christian Brüggemann, Zun Wang
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,16 +20,68 @@ package de.upb.swt.soot.java.bytecode.interceptors;
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
+import de.upb.swt.soot.core.graph.StmtGraph;
+import de.upb.swt.soot.core.jimple.basic.Trap;
+import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.Body;
 import de.upb.swt.soot.core.transform.BodyInterceptor;
 import javax.annotation.Nonnull;
+import java.util.*;
 
-// https://github.com/Sable/soot/blob/master/src/main/java/soot/jimple/toolkits/scalar/UnreachableCodeEliminator.java
+/**
+ * A BodyInterceptor that removes all unreachable stmts from the given Body.
+ *
+ * @author Zun Wang
+ */
 
 public class UnreachableCodeEliminator implements BodyInterceptor {
 
   @Override
   public void interceptBody(@Nonnull Body.BodyBuilder builder) {
-    // TODO Implement
+
+    StmtGraph graph = builder.getStmtGraph();
+    List<Stmt> stmtsInBody = builder.getStmts();
+
+    List<Trap> traps = builder.getTraps();
+    boolean trapsReduced = false;
+
+    //store all stmts which has no predecessors in the graph
+    Deque<Stmt> startStmts = new ArrayDeque<>();
+    startStmts.addLast(stmtsInBody.get(0));
+    for(Trap trap : builder.getTraps()){
+      if(stmtsInBody.contains(trap.getBeginStmt())){
+        startStmts.addLast(trap.getBeginStmt());
+      }else{
+        traps.remove(trap);
+        trapsReduced = true;
+      }
+    }
+
+    //store all stmts which are reachable
+    Set<Stmt> reachableStmts = new HashSet<>();
+    while(!startStmts.isEmpty()){
+      Stmt stmt = startStmts.removeFirst();
+      if(reachableStmts.add(stmt)){
+        for(Stmt succ : graph.successors(stmt)){
+          startStmts.addFirst(succ);
+        }
+      }
+    }
+
+    //get all stmts which are unreachable
+    Set<Stmt> unreachableStmts = new HashSet<>();
+    for(Stmt stmt : stmtsInBody){
+      if(!reachableStmts.contains(stmt)){
+        unreachableStmts.add(stmt);
+      }
+    }
+
+    if(trapsReduced){
+      builder.setTraps(traps);
+    }
+
+
+    //TODO: build body with new list of units??
+
   }
 }
