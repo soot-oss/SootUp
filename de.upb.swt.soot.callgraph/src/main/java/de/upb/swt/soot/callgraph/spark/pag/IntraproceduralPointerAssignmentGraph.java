9d3d57cfa9c61b69ae6c9c6385dff087108776d9
kadirayk <kadiray.k@hotmail.com>
2021-01-20 22:17:16 +0100

    reflectionNewInstane check

--- de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/spark/pag/IntraproceduralPointerAssignmentGraph.java
+++ de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/spark/pag/IntraproceduralPointerAssignmentGraph.java
@@ -8,8 +8,10 @@ import de.upb.swt.soot.core.jimple.common.expr.JVirtualInvokeExpr;
 import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.Body;
 import de.upb.swt.soot.core.model.SootMethod;
+import de.upb.swt.soot.core.signatures.MethodSignature;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
+import sun.jvm.hotspot.debugger.cdbg.RefType;
 import sun.security.provider.certpath.Vertex;
 
 import javax.annotation.Nonnull;
@@ -41,7 +43,9 @@ public class IntraproceduralPointerAssignmentGraph {
         if(stmt.containsInvokeExpr()){
             AbstractInvokeExpr invokeExpr = stmt.getInvokeExpr();
             if(invokeExpr instanceof JVirtualInvokeExpr){
-                //TODO: reflection
+                if(!isReflectionNewInstance(invokeExpr)){
+                    return;
+                }
             } else if (!(invokeExpr instanceof JStaticInvokeExpr)){
                 return;
             }
@@ -50,6 +54,29 @@ public class IntraproceduralPointerAssignmentGraph {
 
     public DefaultDirectedGraph<SparkVertex, SparkEdge> getGraph(){
         return graph;
+    }
+
+    /**
+     * Checks whether the given invocation is for Class.newInstance()
+     *
+     * @param invokeExpr
+     * @return
+     */
+    private boolean isReflectionNewInstance(AbstractInvokeExpr invokeExpr){
+        // TODO: put this in a utility class?
+        if(invokeExpr instanceof JVirtualInvokeExpr){
+            JVirtualInvokeExpr virtualInvokeExpr = (JVirtualInvokeExpr) invokeExpr;
+            if (virtualInvokeExpr.getBase().getType() instanceof RefType) {
+                RefType rt = (RefType) virtualInvokeExpr.getBase().getType();
+                if (rt.getClass().getName().equals("java.lang.Class")) {
+                    MethodSignature signature = virtualInvokeExpr.getMethodSignature();
+                    if (signature.getName().equals("newInstance") && signature.getParameterTypes().size() == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 }
