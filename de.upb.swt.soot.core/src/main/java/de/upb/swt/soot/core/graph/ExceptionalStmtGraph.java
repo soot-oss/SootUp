b14137e3708ffcda0de9a6a7f8b690acb47fa982
wangzun66 <wangzun666@gmail.com>
2021-04-06 00:23:51 +0200

    add header

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/graph/ExceptionalStmtGraph.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/graph/ExceptionalStmtGraph.java
@@ -1,4 +1,25 @@
 package de.upb.swt.soot.core.graph;
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2020 Zun Wang
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 
 import de.upb.swt.soot.core.jimple.basic.Trap;
 import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
@@ -6,6 +27,7 @@ import java.util.List;
 import java.util.Set;
 import javax.annotation.Nonnull;
 
+/** @author Zun Wang */
 public final class ExceptionalStmtGraph extends StmtGraph {
 
   @Nonnull private final MutableExceptionalStmtGraph exceptionalStmtGraph;
