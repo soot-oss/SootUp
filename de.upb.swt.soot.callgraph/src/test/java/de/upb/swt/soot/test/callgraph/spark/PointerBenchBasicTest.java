6de13a1b329fff822afd1c6cebb46012d36dc712
kadirayk <kadiray.k@hotmail.com>
2021-04-10 17:01:11 +0200

    parameter test

--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/PointerBenchBasicTest.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/PointerBenchBasicTest.java
@@ -10,6 +10,7 @@ import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
 import de.upb.swt.soot.core.graph.ImmutableStmtGraph;
 import de.upb.swt.soot.core.jimple.basic.Local;
 import de.upb.swt.soot.core.jimple.basic.Value;
+import de.upb.swt.soot.core.jimple.common.ref.JParameterRef;
 import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.SootClass;
 import de.upb.swt.soot.core.model.SootMethod;
@@ -32,10 +33,8 @@ public class PointerBenchBasicTest {
 
     private JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
     private JavaClassType mainClassSignature;
-    private MethodSignature mainMethodSignature;
     private View view;
     private Spark spark;
-    private SootMethod targetMethod;
 
     public void setUp(String className) {
         String walaClassPath = "src/test/resources/spark/PointerBench";
@@ -56,33 +55,32 @@ public class PointerBenchBasicTest {
         view = javaProject.createOnDemandView();
 
         mainClassSignature = identifierFactory.getClassType(className);
-        mainMethodSignature =
+        MethodSignature mainMethodSignature =
                 identifierFactory.getMethodSignature(
                         "main", mainClassSignature, "void", Collections.singletonList("java.lang.String[]"));
-
-        SootClass mainClass = (SootClass) view.getClass(mainClassSignature).get();
-        Optional<SootMethod> mainMethod = mainClass.getMethod(mainMethodSignature);
-        assertTrue(mainMethodSignature + " not found in classloader", mainMethod.isPresent());
 
         final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
         CallGraphAlgorithm algorithm = new ClassHierarchyAnalysisAlgorithm(view, typeHierarchy);
         CallGraph callGraph = algorithm.initialize(Collections.singletonList(mainMethodSignature));
         spark = new Spark(view, callGraph);
         spark.analyze();
+    }
 
-        MethodSignature targetMethodSig =
-                identifierFactory.getMethodSignature(
-                        "main", mainClassSignature, "void", Collections.singletonList("java.lang.String[]"));
-
+    private SootMethod getTargetMethod(MethodSignature targetMethodSig){
+        SootClass mainClass = (SootClass) view.getClass(mainClassSignature).get();
         Optional<SootMethod> targetOpt = mainClass.getMethod(targetMethodSig);
         assertTrue(targetOpt.isPresent());
-        targetMethod = targetOpt.get();
+        return targetOpt.get();
     }
 
     @Test
     public void testSimpleAlias1() {
         setUp("basic.SimpleAlias1");
-        Map<Integer, Local> lineNumberToA = getLineNumberToLocalMap(targetMethod, "benchmark.objects.A");
+        MethodSignature targetMethodSig =
+                identifierFactory.getMethodSignature(
+                        "main", mainClassSignature, "void", Collections.singletonList("java.lang.String[]"));
+        SootMethod targetMethod = getTargetMethod(targetMethodSig);
+        Map<Integer, Local> lineNumberToA = getLineNumberToLocalMap(targetMethod, "benchmark.objects.A", new ArrayList<>());
 
         Local a = lineNumberToA.get(21);
         Local b = lineNumberToA.get(23);
@@ -101,8 +99,12 @@ public class PointerBenchBasicTest {
     @Test
     public void testBranching1() {
         setUp("basic.Branching1");
-        Map<Integer, Local> lineNumberToInt = getLineNumberToLocalMap(targetMethod, "int");
-        Map<Integer, Local> lineNumberToA = getLineNumberToLocalMap(targetMethod, "benchmark.objects.A");
+        MethodSignature targetMethodSig =
+                identifierFactory.getMethodSignature(
+                        "main", mainClassSignature, "void", Collections.singletonList("java.lang.String[]"));
+        SootMethod targetMethod = getTargetMethod(targetMethodSig);
+        Map<Integer, Local> lineNumberToInt = getLineNumberToLocalMap(targetMethod, "int", new ArrayList<>());
+        Map<Integer, Local> lineNumberToA = getLineNumberToLocalMap(targetMethod, "benchmark.objects.A", new ArrayList<>());
 
         Local i = lineNumberToInt.get(19);
 
@@ -128,14 +130,45 @@ public class PointerBenchBasicTest {
 
     }
 
-    private Map<Integer, Local> getLineNumberToLocalMap(SootMethod sootMethod, String typeName) {
+    @Test
+    public void testParameter1() {
+        setUp("basic.Parameter1");
+        MethodSignature targetMethodSig =
+                identifierFactory.getMethodSignature(
+                        "test", mainClassSignature, "void", Collections.singletonList("benchmark.objects.A"));
+        SootMethod targetMethod = getTargetMethod(targetMethodSig);
+        List<Local> params = new ArrayList<>();
+        Map<Integer, Local> lineNumberToA = getLineNumberToLocalMap(targetMethod, "benchmark.objects.A", params);
+
+        Local x = params.get(0);
+        Local b = lineNumberToA.get(19);
+
+        Set<Node> xPointsTo = spark.getPointsToSet(x);
+        Set<Node> bPointsTo = spark.getPointsToSet(b);
+
+        // a must point to 1 object
+        assertTrue(xPointsTo.size()==1);
+        // b must point to 1 object
+        assertTrue(bPointsTo.size()==1);
+        // a and b must point to same set of objects
+        assertTrue(xPointsTo.equals(bPointsTo));
+    }
+
+    private Map<Integer, Local> getLineNumberToLocalMap(SootMethod sootMethod, String typeName, List<Local> params) {
         final ImmutableStmtGraph stmtGraph = sootMethod.getBody().getStmtGraph();
         Map<Integer, Local> res = new HashMap<>();
         for (Stmt stmt : stmtGraph) {
             int line = stmt.getPositionInfo().getStmtPosition().getFirstLine();
             List<Value> defs = stmt.getDefs();
+            List<Value> uses = stmt.getUses();
             for (Value def : defs) {
                 if (def.getType().toString().equals(typeName) && def instanceof Local) {
+                    for(Value use: uses){
+                        // parameter mapping to local
+                        if(use instanceof JParameterRef && use.getType().toString().equals(typeName)){
+                            params.add((Local) def);
+                        }
+                    }
                     res.put(line, (Local) def);
                 }
             }
