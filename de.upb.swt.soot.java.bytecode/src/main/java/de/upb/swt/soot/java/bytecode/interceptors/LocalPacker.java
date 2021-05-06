075f1826180b6e048b2617f931314f024d0b2403
wangzun66 <wangzun666@gmail.com>
2021-05-06 10:25:28 +0200

    irrelevant change

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/LocalPacker.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/LocalPacker.java
@@ -253,7 +253,7 @@ public class LocalPacker implements BodyInterceptor {
   }
 
   /**
-   * Replace corresponding oldStmt with newStmt in BodyBuilder and visitList
+   * Replace corresponding oldStmt with newStmt in BodyBuilder
    *
    * @param builder
    * @param oldStmt
