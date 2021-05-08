8ee634f4168cc15a4836a241e11238fedcb7554a
Bastian Haverkamp <bastihav@mail.upb.de>
2021-05-08 12:27:17 +0200

    add expected body

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java15/MultilineStringsTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/minimaltestsuite/java15/MultilineStringsTest.java
@@ -36,7 +36,10 @@ public class MultilineStringsTest extends MinimalBytecodeTestSuiteBase {
    */
   @Override
   public List<String> expectedBodyStmts() {
-    return Stream.of("").collect(Collectors.toCollection(ArrayList::new));
+    return Stream.of("l0 := @this: MultilineStrings",
+        "l1 = \"<html>\\n    <body>\\n        <p>Hello, world</p>\\n    </body>\\n</html>\\n\"",
+        "return"
+        ).collect(Collectors.toCollection(ArrayList::new));
   }
 
   @Test
