768c77ef588abbab0d48ebf14eaeaa97efad32e9
wangzun66 <wangzun666@gmail.com>
2021-05-19 12:44:31 +0200

    add header

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/jimple/common/expr/JPhiExpr.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/jimple/common/expr/JPhiExpr.java
@@ -1,5 +1,27 @@
 package de.upb.swt.soot.core.jimple.common.expr;
 
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 1997-2021 Raja Vallée-Rai, Christian Brüggemann, Zun Wang
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import de.upb.swt.soot.core.jimple.Jimple;
 import de.upb.swt.soot.core.jimple.basic.JimpleComparator;
 import de.upb.swt.soot.core.jimple.basic.Local;
@@ -13,6 +35,7 @@ import de.upb.swt.soot.core.util.printer.StmtPrinter;
 import java.util.*;
 import javax.annotation.Nonnull;
 
+/**@Zun Wang*/
 public final class JPhiExpr implements Expr, Copyable {
 
   private final Local[] args;
