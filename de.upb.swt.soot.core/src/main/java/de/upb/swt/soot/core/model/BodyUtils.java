37fd370bdd69c18002b3e0c532aea8403308a58b
wangzun66 <wangzun666@gmail.com>
2021-05-26 16:08:59 +0200

    add new function in BodyUtils

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/model/BodyUtils.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/model/BodyUtils.java
@@ -21,7 +21,9 @@ package de.upb.swt.soot.core.model;
  * #L%
  */
 import de.upb.swt.soot.core.graph.StmtGraph;
+import de.upb.swt.soot.core.jimple.basic.JTrap;
 import de.upb.swt.soot.core.jimple.basic.Local;
+import de.upb.swt.soot.core.jimple.basic.Trap;
 import de.upb.swt.soot.core.jimple.basic.Value;
 import de.upb.swt.soot.core.jimple.common.stmt.AbstractDefinitionStmt;
 import de.upb.swt.soot.core.jimple.common.stmt.JAssignStmt;
@@ -161,5 +163,40 @@ public class BodyUtils {
       return ((JIdentityStmt) oldStmt).withLocal(newDef);
     }
     throw new RuntimeException("The given stmt must be JAssignStmt or JIdentityStmt!");
+  }
+
+  /**
+   * Replace corresponding oldStmt with newStmt in BodyBuilder
+   *
+   * @param builder
+   * @param oldStmt
+   * @param newStmt
+   */
+  public static void replaceStmtInBuilder(Body.BodyBuilder builder, Stmt oldStmt, Stmt newStmt) {
+    builder.replaceStmt(oldStmt, newStmt);
+    adaptTraps(builder, oldStmt, newStmt);
+  }
+  /**
+   * Fit the modified stmt in Traps
+   *
+   * @param builder a bodybuilder, use it to modify Trap
+   * @param oldStmt a Stmt which maybe a beginStmt or endStmt in a Trap
+   * @param newStmt a modified stmt to replace the oldStmt.
+   */
+  private static void adaptTraps(
+          @Nonnull Body.BodyBuilder builder, @Nonnull Stmt oldStmt, @Nonnull Stmt newStmt) {
+    List<Trap> traps = new ArrayList<>(builder.getStmtGraph().getTraps());
+    for (ListIterator<Trap> iterator = traps.listIterator(); iterator.hasNext(); ) {
+      Trap trap = iterator.next();
+      JTrap jtrap = (JTrap) trap;
+      if (oldStmt.equivTo(trap.getBeginStmt())) {
+        Trap newTrap = jtrap.withBeginStmt(newStmt);
+        iterator.set(newTrap);
+      } else if (oldStmt.equivTo(trap.getEndStmt())) {
+        Trap newTrap = jtrap.withEndStmt(newStmt);
+        iterator.set(newTrap);
+      }
+    }
+    builder.setTraps(traps);
   }
 }
