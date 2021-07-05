29258d1ec9888fd4220f663c4109c5c8397cdcbf
kadirayk <kadiray.k@hotmail.com>
2021-07-05 09:52:55 +0200

    DexlibAbstractInstruction

---
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/frontend/apk/tag/Host.java
@@ -0,0 +1,44 @@
+package de.upb.swt.soot.java.bytecode.frontend.apk.tag;
+
+import java.util.List;
+
+/**
+ * A "taggable" object. Implementing classes can have arbitrary labelled data attached to them.
+ *
+ * One example of a tag would be to store Boolean values, associated with array accesses, indicating whether bounds checks
+ * can be omitted.
+ *
+ * @see Tag
+ */
+public interface Host {
+    /** Gets a list of tags associated with the current object. */
+    public List<Tag> getTags();
+
+    /** Returns the tag with the given name. */
+    public Tag getTag(String aName);
+
+    /** Adds a tag. */
+    public void addTag(Tag t);
+
+    /** Removes the first tag with the given name. */
+    public void removeTag(String name);
+
+    /** Returns true if this host has a tag with the given name. */
+    public boolean hasTag(String aName);
+
+    /** Removes all the tags from this host. */
+    public void removeAllTags();
+
+    /** Adds all the tags from h to this host. */
+    public void addAllTagsOf(Host h);
+
+    /**
+     * Returns the Java source line number if available. Returns -1 if not.
+     */
+    public int getJavaSourceStartLineNumber();
+
+    /**
+     * Returns the Java source line column if available. Returns -1 if not.
+     */
+    public int getJavaSourceStartColumnNumber();
+}
---
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/frontend/apk/tag/LineNumberTag.java
@@ -0,0 +1,61 @@
+package de.upb.swt.soot.java.bytecode.frontend.apk.tag;
+
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2001 Feng Qian
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+public class LineNumberTag implements Tag {
+
+    public static final String IDENTIFIER = "LineNumberTag";
+
+    /* it is a u2 value representing line number. */
+    protected int line_number;
+
+    public LineNumberTag(int ln) {
+        this.line_number = ln;
+    }
+
+    @Override
+    public String getName() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public byte[] getValue() {
+        byte[] v = new byte[2];
+        v[0] = (byte) (line_number / 256);
+        v[1] = (byte) (line_number % 256);
+        return v;
+    }
+
+    public int getLineNumber() {
+        return line_number;
+    }
+
+    public void setLineNumber(int value) {
+        line_number = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(line_number);
+    }
+}
