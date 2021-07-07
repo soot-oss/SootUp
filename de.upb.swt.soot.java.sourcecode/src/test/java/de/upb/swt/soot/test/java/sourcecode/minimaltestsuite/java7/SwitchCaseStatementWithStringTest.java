faa2fbed3facbfa945593048d8950ccdc98e29eb
Bastian Haverkamp <bastihav@mail.upb.de>
2021-07-07 15:17:20 +0200

    fix sourcecode switch statement and add test cases

--- de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java7/SwitchCaseStatementWithStringTest.java
+++ de.upb.swt.soot.java.sourcecode/src/test/java/de/upb/swt/soot/test/java/sourcecode/minimaltestsuite/java7/SwitchCaseStatementWithStringTest.java
@@ -52,9 +52,76 @@ public class SwitchCaseStatementWithStringTest extends MinimalSourceTestSuiteBas
         .collect(Collectors.toList());
   }
 
+  public MethodSignature getMethodSignature2() {
+    return identifierFactory.getMethodSignature(
+        "switchCaseStatementInt", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  public List<String> expectedBodyStmts2() {
+    return Stream.of(
+        "r0 := @this: SwitchCaseStatementWithString",
+        "$i0 = 2",
+        "$u0 = null",
+        "switch($i0)",
+        "case 1: goto label3",
+        "case 2: goto label1",
+        "case 3: goto label2",
+        "default: goto label3",
+        "label1:",
+        "$u0 = \"number 2 detected\"",
+        "goto label4",
+        "label2:",
+        "$u0 = \"number 3 detected\"",
+        "goto label4",
+        "label3:",
+        "$u0 = \"number 1 detected\"",
+        "goto label4",
+        "label4:",
+        "return")
+        .collect(Collectors.toList());
+  }
+
+  public MethodSignature getMethodSignature3() {
+    return identifierFactory.getMethodSignature(
+        "switchCaseStatementEnum", getDeclaredClassSignature(), "void", Collections.emptyList());
+  }
+
+  public List<String> expectedBodyStmts3() {
+    return Stream.of(
+        "r0 := @this: SwitchCaseStatementWithString",
+        "$r1 = \"RED\"",
+        "$r2 = \"\"",
+        "$r3 = staticinvoke <Color: Color valueOf(java.lang.String)>($r1)",
+        "$r4 = <Color: Color RED>",
+        "if $r3 == $r4 goto label2",
+        "$r5 = <Color: Color GREEN>",
+        "if $r3 == $r5 goto label1",
+        "goto label3",
+        "label1:",
+        "$r7 = <Color: Color GREEN>",
+        "$r2 = \"color green detected\"",
+        "goto label4",
+        "label2:",
+        "$r6 = <Color: Color RED>",
+        "$r2 = \"color red detected\"",
+        "goto label4",
+        "label3:",
+        "$r2 = \"invalid color\"",
+        "goto label4",
+        "label4:",
+        "return")
+        .collect(Collectors.toList());
+  }
+
   @Test
   public void test() {
     SootMethod method = loadMethod(getMethodSignature());
     assertJimpleStmts(method, expectedBodyStmts());
+
+    SootMethod method2 = loadMethod(getMethodSignature2());
+    assertJimpleStmts(method2, expectedBodyStmts2());
+
+    SootMethod method3 = loadMethod(getMethodSignature3());
+    assertJimpleStmts(method3, expectedBodyStmts3());
   }
 }
