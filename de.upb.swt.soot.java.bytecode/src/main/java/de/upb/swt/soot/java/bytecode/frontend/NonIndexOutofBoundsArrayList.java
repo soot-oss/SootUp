ebf385ae537844da6d3cc01d6abecacf68dbf63f
Markus Schmidt <markus262@web.de>
2021-07-14 16:35:48 +0200

    fix header

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/frontend/NonIndexOutofBoundsArrayList.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/frontend/NonIndexOutofBoundsArrayList.java
@@ -1,9 +1,32 @@
 package de.upb.swt.soot.java.bytecode.frontend;
 
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 1999-2020 Markus Schmidt
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import java.util.*;
 
 /**
- * *
+ * a modified ArrayList which grows instead of throwing IndexOutOfBounds and behaves more like a
+ * (growing) array.
  *
  * @author Markus Schmidt
  */
