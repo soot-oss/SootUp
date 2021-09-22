5afec331c5743806f0b3c1e124015aa7b0514e5c
Bastian Haverkamp <bastihav@mail.upb.de>
2021-09-22 15:58:19 +0200

    only ignore insn if this is not a void method

--- de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
+++ de.upb.swt.soot.java.sourcecode/src/main/java/de/upb/swt/soot/java/sourcecode/frontend/WalaIRToJimpleConverter.java
@@ -490,11 +490,15 @@ public class WalaIRToJimpleConverter {
             new InstructionConverter(this, methodSignature, walaMethod, localGenerator);
         HashMap<Integer, Stmt> index2Stmt = new HashMap<>();
         Stmt stmt = null;
+        boolean isVoidMethod = walaMethod.getReturnType().equals(TypeReference.Void);
+
         for (SSAInstruction inst : insts) {
 
           // WALA sets target of goto to -1 if the goto points to a dead block
-          if (inst instanceof SSAGotoInstruction) {
-            if (((SSAGotoInstruction) inst).getTarget() == -1) continue;
+          if (!isVoidMethod && inst instanceof SSAGotoInstruction) {
+            if (((SSAGotoInstruction) inst).getTarget() == -1) {
+              continue;
+            }
           }
 
           List<Stmt> retStmts = instConverter.convertInstruction(inst, index2Stmt);
@@ -512,7 +516,7 @@ public class WalaIRToJimpleConverter {
         }
 
         // add return void stmt for methods with return type being void
-        if (walaMethod.getReturnType().equals(TypeReference.Void)) {
+        if (isVoidMethod) {
           Stmt ret;
           final boolean isImplicitLastStmtTargetOfBranchStmt = instConverter.hasJumpTarget(-1);
           final boolean validMethodLeaving =
