1758074a685e2b68680572c60fd512f1b8f3e2a5
atulpundir88 <apundir@campus.uni-paderborn.de>
2022-01-15 13:00:40 +0100

    Changing == to equals for comparing starting statement

--- de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/graph/StmtGraph.java
+++ de.upb.swt.soot.core/src/main/java/de/upb/swt/soot/core/graph/StmtGraph.java
@@ -243,7 +243,7 @@ public abstract class StmtGraph implements Iterable<Stmt> {
       final int successorCount = successors.size();
 
       if (predecessors(stmt).size() == 0) {
-        if (!(stmt == getStartingStmt()
+        if (!(stmt.equals(getStartingStmt())
             || getTraps().stream()
                 .map(Trap::getHandlerStmt)
                 .anyMatch(handler -> handler == stmt))) {
