7c3a18211420b76457707f0d3c57840cb5f54f68
zunhoho <wangzun666@gmail.com>
2022-02-23 12:16:38 +0100

    test case for AliasProp

--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/propagater/AliasPropagatorTest.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/propagater/AliasPropagatorTest.java
@@ -7,6 +7,9 @@ import de.upb.swt.soot.callgraph.algorithm.ClassHierarchyAnalysisAlgorithm;
 import de.upb.swt.soot.callgraph.model.CallGraph;
 import de.upb.swt.soot.callgraph.spark.builder.SparkOptions;
 import de.upb.swt.soot.callgraph.spark.pag.PointerAssignmentGraph;
+import de.upb.swt.soot.callgraph.spark.pag.nodes.FieldReferenceNode;
+import de.upb.swt.soot.callgraph.spark.pag.nodes.Node;
+import de.upb.swt.soot.callgraph.spark.pag.nodes.VariableNode;
 import de.upb.swt.soot.callgraph.spark.solver.AliasPropagator;
 import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
 import de.upb.swt.soot.core.signatures.MethodSignature;
@@ -17,7 +20,11 @@ import de.upb.swt.soot.java.core.language.JavaLanguage;
 import de.upb.swt.soot.java.core.types.JavaClassType;
 import de.upb.swt.soot.java.core.views.JavaView;
 import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AliasPropagatorTest {
@@ -28,7 +35,46 @@ public class AliasPropagatorTest {
     PointerAssignmentGraph pag = buildPAG("propagator.AliasProp");
     AliasPropagator propagator = new AliasPropagator(pag);
     propagator.propagate();
-    System.out.println("abc");
+    pag = propagator.getPag();
+    for (VariableNode node : pag.getVariableNodes()) {
+      if (node.toString().contains("$r4")) {
+        Assert.assertEquals(2, node.getPointsToSet().size());
+        for (Node an : node.getPointsToSet()) {
+          Assert.assertTrue(an.toString().contains("benchmark.objects.B"));
+        }
+      }
+      if (node.toString().contains("$u0")) {
+        Assert.assertEquals(2, node.getPointsToSet().size());
+        for (Node an : node.getPointsToSet()) {
+          Assert.assertTrue(an.toString().contains("benchmark.objects.O"));
+        }
+      }
+    }
+
+    for (Set<FieldReferenceNode> frSet : pag.getStoreEdges().values()) {
+      for (FieldReferenceNode frNode : frSet) {
+        if (frNode.toString().contains("$u0")) {
+          Assert.assertEquals(1, frNode.getPointsToSet().size());
+          for (Node an : frNode.getPointsToSet()) {
+            Assert.assertTrue(an.toString().contains("benchmark.objects.B"));
+          }
+        }
+        if (frNode.toString().contains("r0")
+            && frNode.toString().contains("benchmark.objects.B f")) {
+          Assert.assertEquals(1, frNode.getPointsToSet().size());
+          for (Node an : frNode.getPointsToSet()) {
+            Assert.assertTrue(an.toString().contains("benchmark.objects.B"));
+          }
+        }
+        if (frNode.toString().contains("r0")
+            && frNode.toString().contains("benchmark.objects.B g")) {
+          Assert.assertEquals(1, frNode.getPointsToSet().size());
+          for (Node an : frNode.getPointsToSet()) {
+            Assert.assertTrue(an.toString().contains("benchmark.objects.B"));
+          }
+        }
+      }
+    }
   }
 
   private PointerAssignmentGraph buildPAG(String className) {
@@ -52,9 +98,10 @@ public class AliasPropagatorTest {
     JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
     JavaClassType mainClassSignature = identifierFactory.getClassType(className);
 
+    List<String> parameters = new ArrayList<>();
+    parameters.add("java.lang.String[]");
     MethodSignature methodSignature =
-        identifierFactory.getMethodSignature(
-            "test", mainClassSignature, "void", Collections.emptyList());
+        identifierFactory.getMethodSignature("main", mainClassSignature, "void", parameters);
 
     final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
     CallGraphAlgorithm algorithm = new ClassHierarchyAnalysisAlgorithm(view, typeHierarchy);
