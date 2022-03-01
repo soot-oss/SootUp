b5f4efdf9c364ccf0ab78baee8ec8363487fb64f
zunhoho <wangzun666@gmail.com>
2022-03-01 22:25:49 +0100

    build PAG with JimpleView

--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/propagater/AliasPropagatorTest.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/spark/propagater/AliasPropagatorTest.java
@@ -1,6 +1,5 @@
 package de.upb.swt.soot.test.callgraph.spark.propagater;
 
-import static junit.framework.TestCase.fail;
 
 import de.upb.swt.soot.callgraph.algorithm.CallGraphAlgorithm;
 import de.upb.swt.soot.callgraph.algorithm.ClassHierarchyAnalysisAlgorithm;
@@ -13,13 +12,12 @@ import de.upb.swt.soot.callgraph.spark.pag.nodes.VariableNode;
 import de.upb.swt.soot.callgraph.spark.solver.AliasPropagator;
 import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
 import de.upb.swt.soot.core.signatures.MethodSignature;
-import de.upb.swt.soot.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
 import de.upb.swt.soot.java.core.JavaIdentifierFactory;
-import de.upb.swt.soot.java.core.JavaProject;
-import de.upb.swt.soot.java.core.language.JavaLanguage;
 import de.upb.swt.soot.java.core.types.JavaClassType;
-import de.upb.swt.soot.java.core.views.JavaView;
-import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import de.upb.swt.soot.jimple.parser.JimpleAnalysisInputLocation;
+import de.upb.swt.soot.jimple.parser.JimpleProject;
+import de.upb.swt.soot.jimple.parser.JimpleView;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -79,34 +77,24 @@ public class AliasPropagatorTest {
 
   private PointerAssignmentGraph buildPAG(String className) {
 
-    double version = Double.parseDouble(System.getProperty("java.specification.version"));
-    if (version > 1.8) {
-      fail("The rt.jar is not available after Java 8. You are using version " + version);
-    }
-
-    JavaProject javaProject =
-        JavaProject.builder(new JavaLanguage(8))
-            .addInputLocation(
-                new JavaClassPathAnalysisInputLocation(
-                    System.getProperty("java.home") + "/lib/rt.jar"))
-            .addInputLocation(
-                new JavaSourcePathAnalysisInputLocation("src/test/resources/spark/PointerBench"))
-            .build();
-
-    JavaView view = javaProject.createOnDemandView();
+    String resourceDir = "src/test/resources/spark/PointerBench/propagator-jimple/";
+    JimpleAnalysisInputLocation location = new JimpleAnalysisInputLocation(Paths.get(resourceDir));
+    JimpleView view = new JimpleProject(location).createOnDemandView();
+    System.out.println(view.getClasses());
 
     JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
-    JavaClassType mainClassSignature = identifierFactory.getClassType(className);
+    JavaClassType mainClassType = identifierFactory.getClassType(className);
 
     List<String> parameters = new ArrayList<>();
     parameters.add("java.lang.String[]");
     MethodSignature methodSignature =
-        identifierFactory.getMethodSignature("main", mainClassSignature, "void", parameters);
+        identifierFactory.getMethodSignature("main", mainClassType, "void", parameters);
 
     final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
     CallGraphAlgorithm algorithm = new ClassHierarchyAnalysisAlgorithm(view, typeHierarchy);
     CallGraph callGraph = algorithm.initialize(Collections.singletonList(methodSignature));
     PointerAssignmentGraph pag = new PointerAssignmentGraph(view, callGraph, new SparkOptions());
+
     return pag;
   }
 }
