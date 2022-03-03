2e99dc82ab7ea6f8bc8d6f5b8089ef42b291ce1d
Jonas Klauke <Jonas.Klauke@upb.de>
2022-03-03 13:34:20 +0100

    format

--- de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/RapidTypeAnalysisAlgorithm.java
+++ de.upb.swt.soot.callgraph/src/main/java/de/upb/swt/soot/callgraph/RapidTypeAnalysisAlgorithm.java
@@ -85,7 +85,7 @@ public class RapidTypeAnalysisAlgorithm extends AbstractCallGraphAlgorithm {
             .flatMap(clazz -> clazz.getMethod(targetMethodSignature.getSubSignature()))
             .orElseGet(() -> findMethodInHierarchy(view, targetMethodSignature));
 
-    if (targetMethod==null
+    if (targetMethod == null
         || Modifier.isStatic(targetMethod.getModifiers())
         || (invokeExpr instanceof JSpecialInvokeExpr)) {
       return result;
