31fdf8d0a850b14852e523cfe80df4a8ab51445e
atulpundir88 <apundir@campus.uni-paderborn.de>
2022-04-06 15:16:24 +0200

    Adding test files

---
+++ de.upb.swt.soot.tests/src/test/java/categories/ParallelTest.java
@@ -0,0 +1,4 @@
+package categories;
+
+public interface ParallelTest {
+}
---
+++ de.upb.swt.soot.tests/src/test/java/de/upb/swt/soot/test/CallGraphTest.java
@@ -0,0 +1,9 @@
+package de.upb.swt.soot.test;
+
+import categories.Java8Test;
+import categories.ParallelTest;
+import org.junit.experimental.categories.Category;
+
+@Category(ParallelTest.class)
+public class CallGraphTest {
+}
