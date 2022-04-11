129e0043e184692f65b0e9dbf3399ae62e6c245f
atulpundir88 <apundir@campus.uni-paderborn.de>
2022-04-11 15:31:11 +0200

    Adding a new test file for tests that take more time to execute

--- de.upb.swt.soot.tests/src/test/java/de/upb/swt/soot/test/CallGraphTest.java
+++ de.upb.swt.soot.tests/src/test/java/de/upb/swt/soot/test/CallGraphTest.java
@@ -1,9 +1,93 @@
 package de.upb.swt.soot.test;
 
-import categories.Java8Test;
-import categories.ParallelTest;
+import static junit.framework.TestCase.*;
+
+import categories.SlowTest;
+import de.upb.swt.soot.callgraph.AbstractCallGraphAlgorithm;
+import de.upb.swt.soot.callgraph.CallGraph;
+import de.upb.swt.soot.callgraph.RapidTypeAnalysisAlgorithm;
+import de.upb.swt.soot.callgraph.typehierarchy.TypeHierarchy;
+import de.upb.swt.soot.callgraph.typehierarchy.ViewTypeHierarchy;
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.model.SootMethod;
+import de.upb.swt.soot.core.signatures.MethodSignature;
+import de.upb.swt.soot.core.types.ClassType;
+import de.upb.swt.soot.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import de.upb.swt.soot.java.core.JavaIdentifierFactory;
+import de.upb.swt.soot.java.core.JavaProject;
+import de.upb.swt.soot.java.core.language.JavaLanguage;
+import de.upb.swt.soot.java.core.types.JavaClassType;
+import de.upb.swt.soot.java.core.views.JavaView;
+import de.upb.swt.soot.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import java.util.Collections;
+import java.util.Optional;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Category(ParallelTest.class)
+@Category(SlowTest.class)
 public class CallGraphTest {
+
+  protected JavaIdentifierFactory identifierFactory = JavaIdentifierFactory.getInstance();
+  protected JavaClassType mainClassSignature;
+  protected MethodSignature mainMethodSignature;
+  private AbstractCallGraphAlgorithm algorithm;
+
+  protected RapidTypeAnalysisAlgorithm createAlgorithm(JavaView view, TypeHierarchy typeHierarchy) {
+    return new RapidTypeAnalysisAlgorithm(view, typeHierarchy);
+  }
+
+  private JavaView createViewForClassPath(String classPath) {
+    JavaProject javaProject =
+        JavaProject.builder(new JavaLanguage(8))
+            .addInputLocation(
+                new JavaClassPathAnalysisInputLocation(
+                    System.getProperty("java.home") + "/lib/rt.jar"))
+            .addInputLocation(new JavaSourcePathAnalysisInputLocation(classPath))
+            .build();
+    return javaProject.createOnDemandView();
+  }
+
+  CallGraph loadCallGraph(String testDirectory, String className) {
+    double version = Double.parseDouble(System.getProperty("java.specification.version"));
+    if (version > 1.8) {
+      fail("The rt.jar is not available after Java 8. You are using version " + version);
+    }
+
+    String classPath = "src/test/resources/callgraph/" + testDirectory;
+
+    // JavaView view = viewToClassPath.computeIfAbsent(classPath, this::createViewForClassPath);
+    JavaView view = createViewForClassPath(classPath);
+
+    mainClassSignature = identifierFactory.getClassType(className);
+    mainMethodSignature =
+        identifierFactory.getMethodSignature(
+            "main", mainClassSignature, "void", Collections.singletonList("java.lang.String[]"));
+
+    SootClass<?> sc = view.getClass(mainClassSignature).get();
+    Optional<SootMethod> m =
+        (Optional<SootMethod>) sc.getMethod(mainMethodSignature.getSubSignature());
+    assertTrue(mainMethodSignature + " not found in classloader", m.isPresent());
+
+    final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
+    algorithm = createAlgorithm(view, typeHierarchy);
+    CallGraph cg = algorithm.initialize(Collections.singletonList(mainMethodSignature));
+
+    assertTrue(
+        mainMethodSignature + " is not found in CallGraph", cg.containsMethod(mainMethodSignature));
+    assertNotNull(cg);
+    return cg;
+  }
+
+  @Test
+  public void testRTA() {
+    CallGraph cg = loadCallGraph("Misc", "HelloWorld");
+
+    ClassType clazzType = JavaIdentifierFactory.getInstance().getClassType("java.io.PrintStream");
+
+    MethodSignature method =
+        identifierFactory.getMethodSignature(
+            "println", clazzType, "void", Collections.singletonList("java.lang.String"));
+
+    assertTrue(cg.containsCall(mainMethodSignature, method));
+  }
 }
