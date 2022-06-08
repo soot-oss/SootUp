4088231ddc6a82cd5b847088dc7bab1aa208d079
zunhoho <wangzun666@gmail.com>
2022-06-08 09:50:42 +0200

    delete HierarchyUtils.java

--- de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/typeresolving/HierarchyUtilsTest.java
+++ de.upb.swt.soot.java.bytecode/src/test/java/de/upb/swt/soot/test/java/bytecode/interceptors/typeresolving/HierarchyUtilsTest.java
@@ -2,10 +2,24 @@ package de.upb.swt.soot.test.java.bytecode.interceptors.typeresolving;
 
 import org.junit.Test;
 
+import java.io.*;
+
 public class HierarchyUtilsTest {
 
     @Test
     public void test(){
+
+        //test isSubType-ArrayType part
+        I[][] iiarr = new O[10][10];
+        Object[] obarr = new Object[0][0];
+
+        obarr = new double[1][1][1][1];
+        obarr = new Serializable[1][1][1];
+
+
+        Cloneable[][] ssarr = new Serializable[1][1][1];
+
+
         double b = 1.0;
         float f = 2;
         long l = 1;
@@ -14,6 +28,14 @@ public class HierarchyUtilsTest {
         b = s;
         C c = new O();
         I io = new O();
+        E e = E.A;
+        e = null;
+        O[] oa = new O[10];
+        double[][] daa = new double[10][10];
+        double[] da = new double[10];
+
+
+        b = i;
 
     }
 
@@ -28,5 +50,11 @@ public class HierarchyUtilsTest {
     public class O implements I, C {
 
     }
+
+
+    public enum E{
+        A, B;
+    }
+
 
 }
