db52e8f5a9982c86b1eb165cb6218b9fc66e7592
zunhoho <wangzun666@gmail.com>
2022-11-03 12:24:33 +0100

    add new functions in Typing

--- de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/typeresolving/Typing.java
+++ de.upb.swt.soot.java.bytecode/src/main/java/de/upb/swt/soot/java/bytecode/interceptors/typeresolving/Typing.java
@@ -23,17 +23,25 @@ package de.upb.swt.soot.java.bytecode.interceptors.typeresolving;
 import de.upb.swt.soot.core.jimple.basic.Local;
 import de.upb.swt.soot.core.types.Type;
 import de.upb.swt.soot.java.bytecode.interceptors.typeresolving.types.BottomType;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashMap;
 import javax.annotation.Nonnull;
 
 public class Typing {
   private HashMap<Local, Type> local2Type = new HashMap<>();
+  private BitSet stmtsIDList;
 
   public Typing(@Nonnull Collection<Local> locals) {
     for (Local local : locals) {
       local2Type.put(local, BottomType.getInstance());
     }
+    stmtsIDList = new BitSet();
+  }
+
+  public Typing(@Nonnull Typing typing, BitSet stmtsIDList) {
+    this.local2Type = new HashMap<>(typing.local2Type);
+    this.stmtsIDList = stmtsIDList;
   }
 
   public Type getType(Local local) {
@@ -46,5 +54,60 @@ public class Typing {
 
   public Collection<Local> getLocals() {
     return local2Type.keySet();
+  }
+
+  public HashMap<Local, Type> getMap() {
+    return this.local2Type;
+  }
+
+  public void setStmtsIDList(BitSet bitSet) {
+    this.stmtsIDList = bitSet;
+  }
+
+  public BitSet getStmtsIDList() {
+    return this.stmtsIDList;
+  }
+
+  /**
+   * This method is used to compare two {@link Typing}s that have same locals' set, but with
+   * different types.
+   *
+   * @return 0: same to the given <code>typing</code>. 1: more general to the given <code>typing
+   *     </code>. -1: more specific to the given <code>typing</code>. -2: cannot be compared to the
+   *     given <code>typing</code>. One local have two different types which have no ancestor
+   *     relationship. 2: cannot be compared to the given <code>typing</code>. One local's type is
+   *     more specific than the given typing, but another local's type is more general than the
+   *     given typing.
+   */
+  public int compare(Typing typing, BytecodeHierarchy hierarchy, Collection<Local> localsToIgnore) {
+    if (!typing.getLocals().equals(this.getLocals())) {
+      throw new RuntimeException("The compared typings should have the same locals' set!");
+    }
+    int ret = 0;
+    for (Local local : this.local2Type.keySet()) {
+      if (!localsToIgnore.contains(local)) {
+        Type ta = getType(local);
+        Type tb = typing.getType(local);
+
+        int cmp;
+        if (ta.equals(tb)) {
+          cmp = 0;
+        } else if (hierarchy.isAncestor(ta, tb)) {
+          cmp = 1;
+        } else if (hierarchy.isAncestor(tb, ta)) {
+          cmp = -1;
+        } else {
+          return -2;
+        }
+
+        if ((cmp == 1 && ret == -1) || (cmp == -1 && ret == 1)) {
+          return 2;
+        }
+        if (ret == 0) {
+          ret = cmp;
+        }
+      }
+    }
+    return ret;
   }
 }
