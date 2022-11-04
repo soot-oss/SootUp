99c6203550828b0b4ad35620b71c1a02467f3180
Jonas Klauke <Jonas.Klauke@upb.de>
2022-11-04 15:27:09 +0100

    adapted the CHA testcase to the changes of Example1

--- de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/ClassHierarchyAnalysisAlgorithmTest.java
+++ de.upb.swt.soot.callgraph/src/test/java/de/upb/swt/soot/test/callgraph/ClassHierarchyAnalysisAlgorithmTest.java
@@ -29,10 +29,25 @@ public class ClassHierarchyAnalysisAlgorithmTest
     return new ClassHierarchyAnalysisAlgorithm(view, typeHierarchy);
   }
 
+  /**
+   * Testing the call graph generation using CHA on a code example
+   *
+   * <p>In this testcase, the call graph of Example1 in folder {@link callgraph.Misc} is created
+   * using CHA. The testcase expects a call from main to the constructors of B,C, and E The virtual
+   * call print is resolved to all subtypes of A, A.print B.print, C.print, D.print and E.print,
+   * Overall, 8 calls are expected in the main method. the constructor of B is called
+   * directly and indirectly by C, since B is the super class of C.
+   */
   @Test
   public void testMiscExample1() {
-    /** We expect constructors for B and C We expect A.print(), B.print(), C.print(), D.print() */
     CallGraph cg = loadCallGraph("Misc", "example1.Example");
+
+    MethodSignature constructorA =
+        identifierFactory.getMethodSignature(
+            identifierFactory.getClassType("example1.A"),
+            "<init>",
+            "void",
+            Collections.emptyList());
 
     MethodSignature constructorB =
         identifierFactory.getMethodSignature(
@@ -44,6 +59,20 @@ public class ClassHierarchyAnalysisAlgorithmTest
     MethodSignature constructorC =
         identifierFactory.getMethodSignature(
             identifierFactory.getClassType("example1.C"),
+            "<init>",
+            "void",
+            Collections.emptyList());
+
+    MethodSignature constructorD =
+        identifierFactory.getMethodSignature(
+            identifierFactory.getClassType("example1.D"),
+            "<init>",
+            "void",
+            Collections.emptyList());
+
+    MethodSignature constructorE =
+        identifierFactory.getMethodSignature(
+            identifierFactory.getClassType("example1.E"),
             "<init>",
             "void",
             Collections.emptyList());
@@ -76,26 +105,41 @@ public class ClassHierarchyAnalysisAlgorithmTest
             "void",
             Collections.singletonList("java.lang.Object"));
 
+    MethodSignature methodE =
+        identifierFactory.getMethodSignature(
+            identifierFactory.getClassType("example1.E"),
+            "print",
+            "void",
+            Collections.singletonList("java.lang.Object"));
+
     assertTrue(cg.containsCall(mainMethodSignature, constructorB));
     assertTrue(cg.containsCall(mainMethodSignature, constructorC));
+    assertTrue(cg.containsCall(mainMethodSignature, constructorE));
+
+    assertFalse(cg.containsCall(mainMethodSignature, constructorA));
+    assertFalse(cg.containsCall(mainMethodSignature, constructorD));
 
     assertTrue(cg.containsCall(mainMethodSignature, methodA));
     assertTrue(cg.containsCall(mainMethodSignature, methodB));
     assertTrue(cg.containsCall(mainMethodSignature, methodC));
     assertTrue(cg.containsCall(mainMethodSignature, methodD));
+    assertTrue(cg.containsCall(mainMethodSignature, methodE));
 
-    assertEquals(6, cg.callsFrom(mainMethodSignature).size());
+    assertEquals(8, cg.callsFrom(mainMethodSignature).size());
 
     assertEquals(2, cg.callsTo(constructorB).size());
     assertEquals(1, cg.callsTo(constructorC).size());
+    assertEquals(1, cg.callsTo(constructorE).size());
     assertEquals(1, cg.callsTo(methodA).size());
     assertEquals(1, cg.callsTo(methodB).size());
     assertEquals(1, cg.callsTo(methodC).size());
     assertEquals(1, cg.callsTo(methodD).size());
+    assertEquals(1, cg.callsTo(methodE).size());
 
     assertEquals(0, cg.callsFrom(methodA).size());
     assertEquals(0, cg.callsFrom(methodB).size());
     assertEquals(0, cg.callsFrom(methodC).size());
     assertEquals(0, cg.callsFrom(methodD).size());
+    assertEquals(0, cg.callsFrom(methodE).size());
   }
 }
