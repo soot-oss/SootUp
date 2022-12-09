9ded5c4621a605e937a6b15531081749f8e2de7f
Markus Schmidt <markus262@web.de>
2022-12-09 14:47:01 +0100

    change ifelsetree to typevisitor-switch

--- sootup.core/src/main/java/sootup/core/jimple/basic/LocalGenerator.java
+++ sootup.core/src/main/java/sootup/core/jimple/basic/LocalGenerator.java
@@ -28,11 +28,8 @@ import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import sootup.core.jimple.Jimple;
-import sootup.core.types.PrimitiveType;
-import sootup.core.types.ReferenceType;
-import sootup.core.types.Type;
-import sootup.core.types.UnknownType;
-import sootup.core.types.VoidType;
+import sootup.core.jimple.visitor.AbstractTypeVisitor;
+import sootup.core.types.*;
 
 /**
  * Generates locals for Body.
@@ -43,13 +40,13 @@ import sootup.core.types.VoidType;
 public class LocalGenerator {
   private final Set<Local> locals;
   @Nullable private Local thisLocal;
-  private final Map<Integer, Local> paraLocals = new HashMap<>();
+  private final Map<Integer, Local> parameterLocals = new HashMap<>();
+  NamingSwitch ns = new NamingSwitch(new StringBuilder(7).append("$"));
 
   /**
    * Creates Locals {@link Local} with a standard naming scheme. If a Set of Locals is provided, the
    * LocalGenerator checks whether the name is already taken.
    */
-  // TODO: [ms] make it private - access it via BodyBuilder?
   public LocalGenerator(@Nonnull Set<Local> existingLocals) {
     locals = existingLocals;
   }
@@ -73,50 +70,24 @@ public class LocalGenerator {
   }
 
   public Local generateParameterLocal(@Nonnull Type type, int index) {
-    if (!this.paraLocals.containsKey(index)) {
+    if (!this.parameterLocals.containsKey(index)) {
       Local paraLocal = generate(type, false);
-      this.paraLocals.put(index, paraLocal);
+      this.parameterLocals.put(index, paraLocal);
     }
-    return this.paraLocals.get(index);
+    return this.parameterLocals.get(index);
   }
 
   private Local generate(@Nonnull Type type, boolean isField) {
 
-    StringBuilder name = new StringBuilder(7);
+    StringBuilder name = ns.getResult();
+
     // non-field Locals traditionally begin with "$"
-    name.append("$");
     String localName;
-    // determine locals name
-    //noinspection SuspiciousMethodCalls
+    // determine locals name - name collision free
     do {
       // non-field Locals traditionally begin with "$"
       name.setLength(isField ? 0 : 1);
-
-      if (type.equals(PrimitiveType.getInt())) {
-        appendNextIntName(name);
-      } else if (type.equals(PrimitiveType.getByte())) {
-        appendNextByteName(name);
-      } else if (type.equals(PrimitiveType.getShort())) {
-        appendNextShortName(name);
-      } else if (type.equals(PrimitiveType.getBoolean())) {
-        appendNextBooleanName(name);
-      } else if (type.equals(VoidType.getInstance())) {
-        appendNextVoidName(name);
-      } else if (type.equals(PrimitiveType.getChar())) {
-        appendNextCharName(name);
-      } else if (type.equals(PrimitiveType.getDouble())) {
-        appendNextDoubleName(name);
-      } else if (type.equals(PrimitiveType.getFloat())) {
-        appendNextFloatName(name);
-      } else if (type.equals(PrimitiveType.getLong())) {
-        appendNextLongName(name);
-      } else if (type instanceof ReferenceType) {
-        appendNextRefLikeTypeName(name);
-      } else if (type.equals(UnknownType.getInstance())) {
-        appendNextUnknownTypeName(name);
-      } else {
-        throw new RuntimeException("Unhandled Type of Local variable to Generate");
-      }
+      type.accept(ns);
 
       localName = name.toString();
     } while (locals.contains(localName));
@@ -124,60 +95,95 @@ public class LocalGenerator {
     return createLocal(localName, type);
   }
 
-  private int tempInt = 0;
-  private int tempVoid = 0;
-  private int tempBoolean = 0;
-  private int tempLong = 0;
-  private int tempDouble = 0;
-  private int tempFloat = 0;
-  private int tempRefLikeType = 0;
-  private int tempByte = 0;
-  private int tempShort = 0;
-  private int tempChar = 0;
-  private int tempUnknownType = 0;
+  private static class NamingSwitch extends AbstractTypeVisitor<StringBuilder> {
+    private int tempInt = 0;
+    private int tempVoid = 0;
+    private int tempBoolean = 0;
+    private int tempLong = 0;
+    private int tempDouble = 0;
+    private int tempFloat = 0;
+    private int tempRefLikeType = 0;
+    private int tempByte = 0;
+    private int tempShort = 0;
+    private int tempChar = 0;
+    private int tempUnknownType = 0;
+    private StringBuilder str;
 
-  private void appendNextIntName(StringBuilder name) {
-    name.append("i").append(tempInt++);
-  }
+    private NamingSwitch(@Nonnull StringBuilder str) {
+      this.str = str;
+    }
 
-  private void appendNextCharName(StringBuilder name) {
-    name.append("c").append(tempChar++);
-  }
+    @Override
+    public void caseBooleanType() {
+      str.append("z").append(tempBoolean++);
+    }
 
-  private void appendNextVoidName(StringBuilder name) {
-    name.append("v").append(tempVoid++);
-  }
+    @Override
+    public void caseByteType() {
+      str.append("b").append(tempByte++);
+    }
 
-  private void appendNextByteName(StringBuilder name) {
-    name.append("b").append(tempByte++);
-  }
+    @Override
+    public void caseCharType() {
+      str.append("c").append(tempChar++);
+    }
 
-  private void appendNextShortName(StringBuilder name) {
-    name.append("s").append(tempShort++);
-  }
+    @Override
+    public void caseShortType() {
+      str.append("s").append(tempShort++);
+    }
 
-  private void appendNextBooleanName(StringBuilder name) {
-    name.append("z").append(tempBoolean++);
-  }
+    @Override
+    public void caseIntType() {
+      str.append("i").append(tempInt++);
+    }
 
-  private void appendNextDoubleName(StringBuilder name) {
-    name.append("d").append(tempDouble++);
-  }
+    @Override
+    public void caseLongType() {
+      str.append("l").append(tempLong++);
+    }
 
-  private void appendNextFloatName(StringBuilder name) {
-    name.append("f").append(tempFloat++);
-  }
+    @Override
+    public void caseDoubleType() {
+      str.append("d").append(tempDouble++);
+    }
 
-  private void appendNextLongName(StringBuilder name) {
-    name.append("l").append(tempLong++);
-  }
+    @Override
+    public void caseFloatType() {
+      str.append("f").append(tempFloat++);
+    }
 
-  private void appendNextRefLikeTypeName(StringBuilder name) {
-    name.append("r").append(tempRefLikeType++);
-  }
+    @Override
+    public void caseArrayType() {
+      defaultCaseType();
+    }
 
-  private void appendNextUnknownTypeName(StringBuilder name) {
-    name.append("u").append(tempUnknownType++);
+    @Override
+    public void caseClassType(@Nonnull ClassType classType) {
+      str.append("r").append(tempRefLikeType++);
+    }
+
+    @Override
+    public void caseNullType() {
+      defaultCaseType();
+    }
+
+    @Override
+    public void caseVoidType() {
+      // TODO: how does a local with a voidtype make sense..? but obviously there was code/ a letter
+      // assigned for it in old soot..
+      str.append("v").append(tempVoid++);
+    }
+
+    @Override
+    public void caseUnknownType() {
+      str.append("u").append(tempUnknownType++);
+    }
+
+    @Override
+    public void defaultCaseType() {
+      throw new IllegalStateException("Unhandled Type of Local variable to Generate!");
+    }
   }
 
   private Local createLocal(String name, Type sootType) {
@@ -196,6 +202,6 @@ public class LocalGenerator {
   }
 
   public Local getParameterLocal(int i) {
-    return this.paraLocals.get(i);
+    return this.parameterLocals.get(i);
   }
 }
