bc7dbda0cbc38b19bc26b17cdc57f5bc5f979118
Stefan Schott <stefanschott95@gmail.com>
2023-04-06 16:30:34 +0200

    fix jimple parser to be able to parse letters from foreign languages in identifiers

---
+++ sootup.jimple.parser/src/test/java/sootup/jimple/parser/javatestsuite/java6/AlphaBetaGammaTest.java
@@ -0,0 +1,27 @@
+package sootup.jimple.parser.javatestsuite.java6;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import sootup.core.model.SootMethod;
+import sootup.core.signatures.MethodSignature;
+import sootup.jimple.parser.categories.Java8Test;
+import sootup.jimple.parser.javatestsuite.JimpleTestSuiteBase;
+
+import java.util.Collections;
+
+@Category(Java8Test.class)
+public class AlphaBetaGammaTest extends JimpleTestSuiteBase {
+
+    public MethodSignature getMethodSignature() {
+        return identifierFactory.getMethodSignature(
+                getDeclaredClassSignature(), "αβγ", "void", Collections.emptyList());
+    }
+
+    @Test
+    public void test() {
+        SootMethod method = loadMethod(getMethodSignature());
+        Assert.assertEquals("αβγ", method.getName());
+    }
+
+}
