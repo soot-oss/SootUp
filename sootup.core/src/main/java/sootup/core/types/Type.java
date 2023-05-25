b02b06a66171b04537afe5ffb63e6a1d3d288f48
Jonas Klauke <Jonas.Klauke@upb.de>
2023-05-25 11:46:53 +0200

    fmt

--- sootup.core/src/main/java/sootup/core/types/Type.java
+++ sootup.core/src/main/java/sootup/core/types/Type.java
@@ -47,8 +47,8 @@ public abstract class Type implements Acceptor<TypeVisitor> {
   }
 
   /**
-   * This method is used to make an array type for the given type. If the given type
-   * is an array type, then increase its dimension with given dim
+   * This method is used to make an array type for the given type. If the given type is an array
+   * type, then increase its dimension with given dim
    */
   public static ArrayType makeArrayType(Type type, int dim) {
     if (type instanceof ArrayType) {
