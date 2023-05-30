fdedfe6429081055c71154e44cbb30232d029d67
palaniappan1 <palaniappan.pp@gmail.com>
2023-05-30 14:24:13 +0200

    Store Visible Annotations

---
+++ sootup.java.bytecode/src/test/java/sootup/java/bytecode/minimaltestsuite/java6/VisibleAnnotationTest.java
@@ -0,0 +1,28 @@
+package sootup.java.bytecode.minimaltestsuite.java6;
+
+import org.junit.Test;
+import sootup.core.signatures.PackageName;
+import sootup.java.bytecode.minimaltestsuite.MinimalBytecodeTestSuiteBase;
+import sootup.java.core.AnnotationUsage;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.language.JavaJimple;
+import sootup.java.core.types.AnnotationType;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisibleAnnotationTest extends MinimalBytecodeTestSuiteBase {
+
+    @Test
+    public void testVisibleAnnotationOnClassOrAnnotation() {
+        JavaSootClass sootClass = loadClass(getDeclaredClassSignature());
+
+        assertEquals(
+                Arrays.asList(
+                        new AnnotationUsage(
+                                new AnnotationType("InterfaceVisibleAnnotation", new PackageName(""), false),
+                                Collections.emptyMap())),
+                sootClass.getAnnotations(Optional.of(customTestWatcher.getJavaView())));
+    }
+}
