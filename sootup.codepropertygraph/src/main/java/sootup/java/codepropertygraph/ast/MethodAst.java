24cfc9c462054a8950cf521b71a03b19e197f66c
michaelyoukeim <110854872+michaelyoukeim@users.noreply.github.com>
2023-11-23 08:42:35 +0100

    Updated the AST test

--- sootup.codepropertygraph/src/main/java/sootup/java/codepropertygraph/ast/MethodAst.java
+++ sootup.codepropertygraph/src/main/java/sootup/java/codepropertygraph/ast/MethodAst.java
@@ -3,6 +3,7 @@ package sootup.java.codepropertygraph.ast;
 import java.util.List;
 import java.util.Set;
 import sootup.core.jimple.common.stmt.*;
+import sootup.core.model.Body;
 import sootup.core.model.Modifier;
 import sootup.core.model.SootMethod;
 import sootup.core.types.Type;
@@ -13,13 +14,19 @@ public class MethodAst {
   private final List<Type> parameterTypes;
   private final List<Stmt> bodyStmts;
   private final Type returnType;
+  private final Body body;
 
   public MethodAst(SootMethod method) {
+    body = method.getBody();
     name = method.getName();
     modifiers = method.getModifiers();
     parameterTypes = method.getParameterTypes();
     bodyStmts = method.getBody().getStmts();
     returnType = method.getReturnType();
+  }
+
+  public Body getBody() {
+    return body;
   }
 
   public String getName() {
--- sootup.codepropertygraph/src/test/java/sootup/java/codepropertygraph/ast/MethodAstTest.java
+++ sootup.codepropertygraph/src/test/java/sootup/java/codepropertygraph/ast/MethodAstTest.java
@@ -1,26 +1,155 @@
 package sootup.java.codepropertygraph.ast;
 
+import static org.junit.Assert.*;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Optional;
-import org.junit.Assert;
+import java.util.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import sootup.core.IdentifierFactory;
+import sootup.core.jimple.common.stmt.*;
+import sootup.core.model.Modifier;
 import sootup.core.model.SootMethod;
 import sootup.core.signatures.MethodSignature;
 import sootup.core.types.ClassType;
+import sootup.core.types.PrimitiveType;
+import sootup.core.types.Type;
 import sootup.core.views.View;
 import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
 import sootup.java.core.JavaProject;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.language.JavaLanguage;
 
+@RunWith(Parameterized.class)
 public class MethodAstTest {
   private static final Path MINIMAL_TEST_SUITE_DIR =
       Paths.get("..//shared-test-resources/miniTestSuite/java6/binary");
 
   private static final String CLASS_NAME = "IfElseStatement";
+  private final String methodName;
+  private final String expectedName;
+  private final Set<Modifier> expectedModifiers;
+  private final List<Type> expectedParameterTypes;
+  private final List<Stmt> expectedBodyStmts;
+  private final Type expectedReturnType;
+
+  public MethodAstTest(
+      String methodName,
+      String expectedName,
+      Set<Modifier> expectedModifiers,
+      List<Type> expectedParameterTypes,
+      List<Stmt> expectedBodyStmts,
+      Type expectedReturnType) {
+    this.methodName = methodName;
+    this.expectedName = expectedName;
+    this.expectedModifiers = expectedModifiers;
+    this.expectedParameterTypes = expectedParameterTypes;
+    this.expectedBodyStmts = expectedBodyStmts;
+    this.expectedReturnType = expectedReturnType;
+  }
+
+  @Parameterized.Parameters(name = "TestAstMethod({0})")
+  public static Object[][] data() {
+    Object expectedModifiers1 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes1 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts1 = null;
+    Object expectedReturnType1 = PrimitiveType.IntType.getInstance();
+
+    Object expectedModifiers2 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes2 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts2 = null;
+    Object expectedReturnType2 = PrimitiveType.IntType.getInstance();
+
+    Object expectedModifiers3 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes3 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts3 = null;
+    Object expectedReturnType3 = PrimitiveType.IntType.getInstance();
+
+    Object expectedModifiers4 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes4 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts4 = null;
+    Object expectedReturnType4 = PrimitiveType.IntType.getInstance();
+
+    Object expectedModifiers5 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes5 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts5 = null;
+    Object expectedReturnType5 = PrimitiveType.IntType.getInstance();
+
+    Object expectedModifiers6 = EnumSet.of(Modifier.PUBLIC);
+    Object expectedParameterTypes6 = Collections.singletonList(PrimitiveType.IntType.getInstance());
+    Object expectedBodyStmts6 = null;
+    Object expectedReturnType6 = PrimitiveType.IntType.getInstance();
+
+    return new Object[][] {
+      {
+        "ifStatement",
+        "ifStatement",
+        expectedModifiers1,
+        expectedParameterTypes1,
+        expectedBodyStmts1,
+        expectedReturnType1
+      },
+      {
+        "ifElseStatement",
+        "ifElseStatement",
+        expectedModifiers2,
+        expectedParameterTypes2,
+        expectedBodyStmts2,
+        expectedReturnType2
+      },
+      {
+        "ifElseCascadingStatement",
+        "ifElseCascadingStatement",
+        expectedModifiers3,
+        expectedParameterTypes3,
+        expectedBodyStmts3,
+        expectedReturnType3
+      },
+      {
+        "ifElseCascadingInElseStatement",
+        "ifElseCascadingInElseStatement",
+        expectedModifiers4,
+        expectedParameterTypes4,
+        expectedBodyStmts4,
+        expectedReturnType4
+      },
+      {
+        "ifElseCascadingElseIfStatement",
+        "ifElseCascadingElseIfStatement",
+        expectedModifiers5,
+        expectedParameterTypes5,
+        expectedBodyStmts5,
+        expectedReturnType5
+      },
+      {
+        "ifElseCascadingElseIfInElseStatement",
+        "ifElseCascadingElseIfInElseStatement",
+        expectedModifiers6,
+        expectedParameterTypes6,
+        expectedBodyStmts6,
+        expectedReturnType6
+      }
+    };
+  }
+
+  @Test
+  public void testMethodAst() {
+    Optional<? extends SootMethod> methodOpt = getMinimalTestSuiteMethod(methodName);
+    assertTrue(methodOpt.isPresent());
+    SootMethod method = methodOpt.get();
+    MethodAst methodAst = new MethodAst(method);
+
+    assertEquals(expectedName, methodAst.getName());
+    assertEquals(expectedModifiers, methodAst.getModifiers());
+    assertEquals(expectedParameterTypes, methodAst.getParameterTypes());
+    // assertEquals(expectedBodyStmts, methodAst.getBodyStmts());
+    assertEquals(expectedReturnType, methodAst.getReturnType());
+
+    System.out.println(methodName);
+    System.out.println(AstToGraphConverter.convert(methodAst).toDotFormat());
+  }
 
   Optional<? extends SootMethod> getMinimalTestSuiteMethod(String methodName) {
     PathBasedAnalysisInputLocation inputLocation =
@@ -34,35 +163,5 @@ public class MethodAstTest {
         projectIdentifierFactory.getMethodSignature(
             appClass, methodName, "int", Collections.singletonList("int"));
     return view.getMethod(methodSignature);
-  }
-
-  @org.junit.Before
-  public void setUp() {}
-
-  @Test
-  public void testAstForIfStatement() {
-    Optional<? extends SootMethod> method = getMinimalTestSuiteMethod("ifStatement");
-    Assert.assertTrue(method.isPresent());
-    MethodAst ast = new MethodAst(method.get());
-    AstGraph astGraph = AstToGraphConverter.convert(ast);
-    System.out.println(astGraph.toDotFormat());
-  }
-
-  @Test
-  public void testAstForIfElseStatement() {
-    Optional<? extends SootMethod> method = getMinimalTestSuiteMethod("ifElseStatement");
-    Assert.assertTrue(method.isPresent());
-    MethodAst ast = new MethodAst(method.get());
-    AstGraph astGraph = AstToGraphConverter.convert(ast);
-    System.out.println(astGraph.toDotFormat());
-  }
-
-  @Test
-  public void testAstForIfElseIfStatement() {
-    Optional<? extends SootMethod> method = getMinimalTestSuiteMethod("ifElseIfStatement");
-    Assert.assertTrue(method.isPresent());
-    MethodAst ast = new MethodAst(method.get());
-    AstGraph astGraph = AstToGraphConverter.convert(ast);
-    System.out.println(astGraph.toDotFormat());
   }
 }
