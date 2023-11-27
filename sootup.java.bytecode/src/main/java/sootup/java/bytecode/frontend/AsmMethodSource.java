2ad8d879754ab684ae61ea0182242792ac4de0ac
Markus Schmidt <markus262@web.de>
2023-11-27 16:38:19 +0100

    fix: handle desc which can only be an array or an object -> missing L in the beginning of desc

--- sootup.java.bytecode/src/main/java/sootup/java/bytecode/frontend/AsmMethodSource.java
+++ sootup.java.bytecode/src/main/java/sootup/java/bytecode/frontend/AsmMethodSource.java
@@ -1177,7 +1177,7 @@ public class AsmMethodSource extends JSRInlinerAdapter implements BodySource {
           }
         case CHECKCAST:
           {
-            val = Jimple.newCastExpr(op1.toImmediate(), AsmUtil.toJimpleType(insn.desc));
+            val = Jimple.newCastExpr(op1.toImmediate(), AsmUtil.arrayTypetoJimpleType(insn.desc));
             break;
           }
         case INSTANCEOF:
