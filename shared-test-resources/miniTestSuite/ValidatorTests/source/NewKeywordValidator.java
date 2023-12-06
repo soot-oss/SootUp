30bde646fa309e78a7fc26841a04ef6e68af3ed2
akshitad11 <akshitad@mail.uni-paderborn.de>
2023-12-06 18:10:21 +0100

    Deleting files, jimple file used for testing

--- shared-test-resources/miniTestSuite/ValidatorTests/source/NewKeywordValidator.java
+++
@@ -1,9 +0,0 @@
-public class NewKeywordValidator {
-
-    int x = 5;
-
-    public static void main(String[] args) {
-        NewKeywordValidator myObj = new NewKeywordValidator();
-        System.out.println(myObj.x);
-    }
-}
