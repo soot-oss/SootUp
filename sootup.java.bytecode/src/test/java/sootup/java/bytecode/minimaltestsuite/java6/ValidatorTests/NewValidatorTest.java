d8e357a7d95d82b62461db2743548fa85deec61e
Kadiray Karakaya <kadiray.k@hotmail.com>
2023-12-19 13:00:59 +0100

    use equals instead of contains

--- sootup.java.bytecode/src/test/java/sootup/java/bytecode/minimaltestsuite/java6/ValidatorTests/NewValidatorTest.java
+++ sootup.java.bytecode/src/test/java/sootup/java/bytecode/minimaltestsuite/java6/ValidatorTests/NewValidatorTest.java
@@ -14,18 +14,18 @@ import sootup.core.signatures.PackageName;
 import sootup.core.types.ClassType;
 import sootup.core.validation.NewValidator;
 import sootup.core.validation.ValidationException;
-import sootup.java.bytecode.minimaltestsuite.MinimalBytecodeTestSuiteBase;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.views.JavaView;
 import sootup.jimple.parser.JimpleAnalysisInputLocation;
 import sootup.jimple.parser.JimpleProject;
 import sootup.jimple.parser.JimpleView;
 
-public class NewValidatorTest extends MinimalBytecodeTestSuiteBase {
+public class NewValidatorTest {
 
   NewValidator validator;
   Body body;
   JavaView view;
+  Collection<SootClass<?>> classes;
 
   @Before
   public void Setup() {
@@ -88,12 +88,6 @@ public class NewValidatorTest extends MinimalBytecodeTestSuiteBase {
     assertFalse(classSource1.isPresent());
     final Optional<SootClass<?>> classSource2 = jv1.getClass(classTypeNewValidator_fail);
     assertFalse(classSource2.isPresent());
-  }
-
-  @Test
-  public void testNewValidator() {
-    List<ValidationException> validationExceptions_success;
-    List<ValidationException> validationExceptions_fail;
 
     String classPath = "src/test/java/resources/jimple";
     AnalysisInputLocation<JavaSootClass> jimpleInputLocation =
@@ -101,39 +95,49 @@ public class NewValidatorTest extends MinimalBytecodeTestSuiteBase {
 
     JimpleView view = new JimpleProject(jimpleInputLocation).createView();
 
-    Collection<SootClass<?>> classes = new HashSet<>(); // Set to track the classes to check
+    classes = new HashSet<>(); // Set to track the classes to check
 
     for (SootClass<?> aClass : view.getClasses()) {
       if (!aClass.isLibraryClass()) {
         classes.add(aClass);
       }
     }
+  }
+
+  @Test
+  public void testNewValidatorSuccess() {
+    List<ValidationException> validationExceptions_success;
 
     validationExceptions_success =
         validator.validate(
             classes.stream()
-                .filter(c -> c.getType().getClassName().contains("NewValidator"))
+                .filter(c -> c.getType().getClassName().equals("NewValidator"))
                 .findFirst()
                 .get()
                 .getMethods()
                 .stream()
-                .filter(m -> m.getName().contains("newValidator"))
+                .filter(m -> m.getName().equals("newValidator"))
                 .map(m -> m.getBody())
                 .findFirst()
                 .get(),
             view);
 
     assertEquals(0, validationExceptions_success.size());
+  }
+
+  @Test
+  public void testNewValidatorFailure() {
+    List<ValidationException> validationExceptions_fail;
 
     validationExceptions_fail =
         validator.validate(
             classes.stream()
-                .filter(c -> c.getType().getClassName().contains("NewValidator_fail"))
+                .filter(c -> c.getType().getClassName().equals("NewValidator_fail"))
                 .findFirst()
                 .get()
                 .getMethods()
                 .stream()
-                .filter(m -> m.getName().contains("newValidator_fail"))
+                .filter(m -> m.getName().equals("newValidator_fail"))
                 .map(m -> m.getBody())
                 .findFirst()
                 .get(),
