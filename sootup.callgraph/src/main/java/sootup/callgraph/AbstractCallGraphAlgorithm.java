54191f111328f96e2eedc5421e3ac06db0be0005
M.Schmidt <markus262@web.de>
2023-12-21 15:04:24 +0100

    not throwing an error if not necessary when nothing was added to the CallGraph

--- sootup.callgraph/src/main/java/sootup/callgraph/AbstractCallGraphAlgorithm.java
+++ sootup.callgraph/src/main/java/sootup/callgraph/AbstractCallGraphAlgorithm.java
@@ -313,16 +313,20 @@ public abstract class AbstractCallGraphAlgorithm implements CallGraphAlgorithm {
   @Nonnull
   @Override
   public CallGraph addClass(@Nonnull CallGraph oldCallGraph, @Nonnull JavaClassType classType) {
-    MutableCallGraph updated = oldCallGraph.copy();
-
     SootClass<?> clazz = view.getClassOrThrow(classType);
     Set<MethodSignature> newMethodSignatures =
-        clazz.getMethods().stream().map(Method::getSignature).collect(Collectors.toSet());
+        clazz.getMethods().stream()
+            .map(Method::getSignature)
+            .filter(methodSig -> !oldCallGraph.containsMethod(methodSig))
+            .collect(Collectors.toSet());
 
-    if (newMethodSignatures.stream().anyMatch(oldCallGraph::containsMethod)) {
-      // FIXME: [ms] handle better - remove from entry point signatures in this case
-      throw new IllegalArgumentException("CallGraph already contains methods from " + classType);
+    // were all the added method signatures already visited in the CallGraph? i.e. is there
+    // something to add?
+    if (newMethodSignatures.isEmpty()) {
+      return oldCallGraph;
     }
+
+    MutableCallGraph updated = oldCallGraph.copy();
 
     // Step 1: Add edges from the new methods to other methods
     Deque<MethodSignature> workList = new ArrayDeque<>(newMethodSignatures);
