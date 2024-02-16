843d6e63ecb6a4ba9bb26741f059b9097fdbaf6b
akshitad11 <akshitad@mail.uni-paderborn.de>
2024-02-16 19:27:13 +0100

    Test changes

--- sootup.tests/src/test/java/sootup/tests/validator/ClassModifiersValidatorTest.java
+++ sootup.tests/src/test/java/sootup/tests/validator/ClassModifiersValidatorTest.java
@@ -34,9 +34,8 @@ public class ClassModifiersValidatorTest {
     classModifiersValidator = new ClassModifiersValidator();
   }
 
-  @Test
-  public void testClassModifiersValidator_success() {
-    List<ValidationException> validationExceptions_success = new ArrayList<>();
+  public JavaSootClass testClassCreatorWithModifiers(EnumSet<ClassModifier> modifierEnumSet) {
+
     JavaView view = new JavaView(Collections.singletonList(new EagerInputLocation()));
     ClassType type = view.getIdentifierFactory().getClassType("java.lang.String");
 
@@ -84,147 +83,55 @@ public class ClassModifiersValidatorTest {
                 Collections.emptySet(),
                 Collections.singleton(dummyMainMethod),
                 NoPositionInformation.getInstance(),
-                EnumSet.of(
-                    ClassModifier.ANNOTATION, ClassModifier.INTERFACE, ClassModifier.ABSTRACT),
+                modifierEnumSet,
                 Collections.emptyList(),
                 Collections.emptyList(),
                 Collections.emptyList()),
             SourceType.Application);
-
     assertEquals(mainClass.getMethods().size(), 1);
 
-    classModifiersValidator.validate(mainClass, validationExceptions_success);
+    return mainClass;
+  }
+
+  @Test
+  public void testClassModifiersValidator_success() {
+    List<ValidationException> validationExceptions_success = new ArrayList<>();
+
+    JavaSootClass javaSootClass =
+        testClassCreatorWithModifiers(
+            EnumSet.of(ClassModifier.ANNOTATION, ClassModifier.INTERFACE, ClassModifier.ABSTRACT));
+
+    classModifiersValidator.validate(javaSootClass, validationExceptions_success);
 
     assertEquals(0, validationExceptions_success.size());
   }
 
   @Test
   public void testClassModifiersValidator_fail1() {
-    List<ValidationException> validationExceptions_fail = new ArrayList<>();
-    JavaView view = new JavaView(Collections.singletonList(new EagerInputLocation()));
-    ClassType type = view.getIdentifierFactory().getClassType("java.lang.String");
+    List<ValidationException> validationExceptions_fail1 = new ArrayList<>();
 
-    LocalGenerator generator = new LocalGenerator(new HashSet<>());
-    MethodSignature methodSignature =
-        view.getIdentifierFactory()
-            .getMethodSignature("dummyMain", "main", "void", Collections.emptyList());
-    Body.BodyBuilder bodyBuilder = Body.builder();
+    JavaSootClass javaSootClass =
+        testClassCreatorWithModifiers(
+            EnumSet.of(
+                ClassModifier.INTERFACE,
+                ClassModifier.ENUM,
+                ClassModifier.FINAL,
+                ClassModifier.SUPER));
 
-    final JIdentityStmt firstStmt =
-        Jimple.newIdentityStmt(
-            generator.generateLocal(type),
-            Jimple.newParameterRef(type, 0),
-            StmtPositionInfo.getNoStmtPositionInfo());
-    final JReturnVoidStmt returnVoidStmt =
-        new JReturnVoidStmt(StmtPositionInfo.getNoStmtPositionInfo());
+    classModifiersValidator.validate(javaSootClass, validationExceptions_fail1);
 
-    Body body =
-        bodyBuilder
-            .setStartingStmt(firstStmt)
-            .addFlow(firstStmt, returnVoidStmt)
-            .setMethodSignature(methodSignature)
-            .setLocals(generator.getLocals())
-            .build();
-    assertEquals(1, body.getLocalCount());
-
-    JavaSootMethod dummyMainMethod =
-        new JavaSootMethod(
-            new OverridingBodySource(methodSignature, body),
-            methodSignature,
-            EnumSet.of(MethodModifier.PUBLIC, MethodModifier.STATIC),
-            Collections.emptyList(),
-            Collections.emptyList(),
-            NoPositionInformation.getInstance());
-
-    JavaSootClass mainClass =
-        new JavaSootClass(
-            new OverridingJavaClassSource(
-                new EagerInputLocation(),
-                null,
-                view.getIdentifierFactory().getClassType("dummyMain"),
-                null,
-                Collections.emptySet(),
-                null,
-                Collections.emptySet(),
-                Collections.singleton(dummyMainMethod),
-                NoPositionInformation.getInstance(),
-                EnumSet.of(
-                    ClassModifier.INTERFACE,
-                    ClassModifier.ENUM,
-                    ClassModifier.FINAL,
-                    ClassModifier.SUPER),
-                Collections.emptyList(),
-                Collections.emptyList(),
-                Collections.emptyList()),
-            SourceType.Application);
-
-    assertEquals(mainClass.getMethods().size(), 1);
-
-    classModifiersValidator.validate(mainClass, validationExceptions_fail);
-
-    assertEquals(4, validationExceptions_fail.size());
+    assertEquals(4, validationExceptions_fail1.size());
   }
 
   @Test
   public void testClassModifiersValidator_fail2() {
-    List<ValidationException> validationExceptions_fail = new ArrayList<>();
-    JavaView view = new JavaView(Collections.singletonList(new EagerInputLocation()));
-    ClassType type = view.getIdentifierFactory().getClassType("java.lang.String");
+    List<ValidationException> validationExceptions_fail2 = new ArrayList<>();
 
-    LocalGenerator generator = new LocalGenerator(new HashSet<>());
-    MethodSignature methodSignature =
-        view.getIdentifierFactory()
-            .getMethodSignature("dummyMain", "main", "void", Collections.emptyList());
-    Body.BodyBuilder bodyBuilder = Body.builder();
+    JavaSootClass javaSootClass =
+        testClassCreatorWithModifiers(EnumSet.of(ClassModifier.ANNOTATION));
 
-    final JIdentityStmt firstStmt =
-        Jimple.newIdentityStmt(
-            generator.generateLocal(type),
-            Jimple.newParameterRef(type, 0),
-            StmtPositionInfo.getNoStmtPositionInfo());
-    final JReturnVoidStmt returnVoidStmt =
-        new JReturnVoidStmt(StmtPositionInfo.getNoStmtPositionInfo());
+    classModifiersValidator.validate(javaSootClass, validationExceptions_fail2);
 
-    Body body =
-        bodyBuilder
-            .setStartingStmt(firstStmt)
-            .addFlow(firstStmt, returnVoidStmt)
-            .setMethodSignature(methodSignature)
-            .setLocals(generator.getLocals())
-            .build();
-    assertEquals(1, body.getLocalCount());
-
-    JavaSootMethod dummyMainMethod =
-        new JavaSootMethod(
-            new OverridingBodySource(methodSignature, body),
-            methodSignature,
-            EnumSet.of(MethodModifier.PUBLIC, MethodModifier.STATIC),
-            Collections.emptyList(),
-            Collections.emptyList(),
-            NoPositionInformation.getInstance());
-
-    JavaSootClass mainClass =
-        new JavaSootClass(
-            new OverridingJavaClassSource(
-                new EagerInputLocation(),
-                null,
-                view.getIdentifierFactory().getClassType("dummyMain"),
-                null,
-                Collections.emptySet(),
-                null,
-                Collections.emptySet(),
-                Collections.singleton(dummyMainMethod),
-                NoPositionInformation.getInstance(),
-                EnumSet.of(ClassModifier.ANNOTATION),
-                Collections.emptyList(),
-                Collections.emptyList(),
-                Collections.emptyList()),
-            SourceType.Application);
-
-    assertEquals(mainClass.getMethods().size(), 1);
-
-    classModifiersValidator.validate(mainClass, validationExceptions_fail);
-
-    assertEquals(1, validationExceptions_fail.size());
+    assertEquals(1, validationExceptions_fail2.size());
   }
 }
