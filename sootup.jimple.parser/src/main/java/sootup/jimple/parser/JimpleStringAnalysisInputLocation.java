40c8a4215f5eada988acd316545ca6c16a0266e7
M.Schmidt <markus262@web.de>
2024-02-22 12:48:47 +0100

    add header, improve tests

--- sootup.jimple.parser/src/main/java/sootup/jimple/parser/JimpleStringAnalysisInputLocation.java
+++ sootup.jimple.parser/src/main/java/sootup/jimple/parser/JimpleStringAnalysisInputLocation.java
@@ -1,5 +1,27 @@
 package sootup.jimple.parser;
 
+/*-
+ * #%L
+ * Soot
+ * %%
+ * Copyright (C) 2018-2024 Markus Schmidt
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
@@ -16,24 +38,35 @@ import sootup.core.transform.BodyInterceptor;
 import sootup.core.types.ClassType;
 import sootup.core.views.View;
 
+/**
+ * This AnalysisInputLocation encapsulates and represents a single Jimple "file" - the contents of
+ * the Class are given via String.
+ *
+ * <p>see JimpleStringAnalysisInputLocationTest for an example.
+ */
 public class JimpleStringAnalysisInputLocation implements AnalysisInputLocation {
 
   @Nonnull final Path path = Paths.get("only-in-memory.jimple");
   @Nonnull final List<BodyInterceptor> bodyInterceptors;
   @Nonnull private final OverridingClassSource classSource;
+  @Nonnull final SourceType sourceType;
 
-  public JimpleStringAnalysisInputLocation(@Nonnull String data) {
-    this(data, Collections.emptyList());
+  public JimpleStringAnalysisInputLocation(@Nonnull String jimpleFileContents) {
+    this(jimpleFileContents, SourceType.Application, Collections.emptyList());
   }
 
   public JimpleStringAnalysisInputLocation(
-      @Nonnull String data, List<BodyInterceptor> bodyInterceptors) {
+      @Nonnull String jimpleFileContents,
+      @Nonnull SourceType sourceType,
+      @Nonnull List<BodyInterceptor> bodyInterceptors) {
     this.bodyInterceptors = bodyInterceptors;
+    this.sourceType = sourceType;
 
     try {
       JimpleConverter jimpleConverter = new JimpleConverter();
-      classSource = jimpleConverter.run(CharStreams.fromString(data), this, path, bodyInterceptors);
-
+      classSource =
+          jimpleConverter.run(
+              CharStreams.fromString(jimpleFileContents), this, path, bodyInterceptors);
     } catch (Exception e) {
       throw new IllegalArgumentException("No valid Jimple given.", e);
     }
@@ -55,7 +88,7 @@ public class JimpleStringAnalysisInputLocation implements AnalysisInputLocation 
   @Nonnull
   @Override
   public SourceType getSourceType() {
-    return SourceType.Application;
+    return sourceType;
   }
 
   @Nonnull
--- sootup.jimple.parser/src/test/java/sootup/jimple/parser/JimpleStringAnalysisInputLocationTest.java
+++ sootup.jimple.parser/src/test/java/sootup/jimple/parser/JimpleStringAnalysisInputLocationTest.java
@@ -1,11 +1,38 @@
 package sootup.jimple.parser;
 
+/*-
+ * #%L
+ * Soot
+ * %%
+ * Copyright (C) 2018-2024 Markus Schmidt
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collections;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.VoidType;
 import sootup.core.views.View;
+import sootup.jimple.parser.categories.Java8Test;
 
+@Category(Java8Test.class)
 public class JimpleStringAnalysisInputLocationTest {
 
   @Test(expected = IllegalArgumentException.class)
@@ -31,5 +58,14 @@ public class JimpleStringAnalysisInputLocationTest {
 
     View view = new JimpleView(Collections.singletonList(analysisInputLocation));
     assertTrue(view.getClass(analysisInputLocation.getClassType()).isPresent());
+
+    MethodSignature methodSig =
+        view.getIdentifierFactory()
+            .getMethodSignature(
+                analysisInputLocation.getClassType(),
+                "banana",
+                VoidType.getInstance(),
+                Collections.emptyList());
+    assertTrue(view.getMethod(methodSig).isPresent());
   }
 }
