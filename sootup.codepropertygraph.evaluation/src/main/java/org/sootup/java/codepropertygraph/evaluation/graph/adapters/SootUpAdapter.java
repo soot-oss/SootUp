43a73cc63ec4dcd34b6cdf23a7728bdc0d007718
michaelyoukeim <110854872+michaelyoukeim@users.noreply.github.com>
2024-02-25 03:09:10 +0100

    Excluded switch statements from eval

--- sootup.codepropertygraph.evaluation/src/main/java/org/sootup/java/codepropertygraph/evaluation/graph/adapters/SootUpAdapter.java
+++ sootup.codepropertygraph.evaluation/src/main/java/org/sootup/java/codepropertygraph/evaluation/graph/adapters/SootUpAdapter.java
@@ -1,9 +1,10 @@
 package org.sootup.java.codepropertygraph.evaluation.graph.adapters;
 
-import java.util.Objects;
 import sootup.core.jimple.common.ref.JArrayRef;
 import sootup.core.jimple.common.stmt.JAssignStmt;
+import sootup.core.jimple.common.stmt.JIdentityStmt;
 import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.jimple.javabytecode.stmt.JSwitchStmt;
 import sootup.java.codepropertygraph.propertygraph.*;
 
 public class SootUpAdapter {
@@ -11,24 +12,20 @@ public class SootUpAdapter {
     PropertyGraph cfgGraph = new PropertyGraph();
 
     for (PropertyGraphEdge edge : sootUpCfg.getEdges()) {
-      if (Objects.equals(edge.getLabel(), "DDG")
-          && edge.getSource() instanceof StmtPropertyGraphNode
-          && edge.getSource() instanceof StmtPropertyGraphNode) {
-        StmtPropertyGraphNode src = (StmtPropertyGraphNode) edge.getSource();
-        StmtPropertyGraphNode dst = (StmtPropertyGraphNode) edge.getDestination();
-        Stmt srcStmt = src.getStmt();
-        Stmt dstStmt = dst.getStmt();
-        if (dstStmt instanceof JAssignStmt
-            && ((JAssignStmt) dstStmt).getLeftOp() instanceof JArrayRef
-            && srcStmt.getDefs().contains(dstStmt.getArrayRef().getUses().get(0))) {
-          continue;
-        }
-      }
+      StmtPropertyGraphNode src = (StmtPropertyGraphNode) edge.getSource();
+      StmtPropertyGraphNode dst = (StmtPropertyGraphNode) edge.getDestination();
+      Stmt srcStmt = src.getStmt();
+      Stmt dstStmt = dst.getStmt();
 
-      if (!edge.getSource().getName().contains(" := ")
-          && !edge.getDestination().getName().contains(" := ")) {
-        cfgGraph.addEdge(edge.getSource(), edge.getDestination(), edge.getLabel());
-      }
+      if (edge.getLabel().equals("DDG")
+          && dstStmt instanceof JAssignStmt
+          && ((JAssignStmt) dstStmt).getLeftOp() instanceof JArrayRef
+          && srcStmt.getDefs().contains(dstStmt.getArrayRef().getUses().get(0))) continue;
+
+      if ((srcStmt instanceof JIdentityStmt) || (dstStmt instanceof JIdentityStmt)) continue;
+      if ((srcStmt instanceof JSwitchStmt) || (dstStmt instanceof JSwitchStmt)) continue;
+
+      cfgGraph.addEdge(edge.getSource(), edge.getDestination(), edge.getLabel());
     }
 
     return cfgGraph;
