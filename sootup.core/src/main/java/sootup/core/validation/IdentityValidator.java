99be4597c9d5d0438165a52a686a186f5c77b5db
Jonas Klauke <Jonas.Klauke@upb.de>
2024-02-29 14:11:04 +0100

    migrate to juint 5

--- sootup.core/src/main/java/sootup/core/validation/IdentityValidator.java
+++ sootup.core/src/main/java/sootup/core/validation/IdentityValidator.java
@@ -25,7 +25,6 @@ package sootup.core.validation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import sootup.core.jimple.common.ref.JParameterRef;
 import sootup.core.jimple.common.ref.JThisRef;
 import sootup.core.jimple.common.stmt.JIdentityStmt;
@@ -41,65 +40,84 @@ import sootup.core.views.View;
  */
 public class IdentityValidator implements BodyValidator {
 
-    /**
-     * Checks whether each ParameterRef and ThisRef is used exactly once.
-     *
-     * @return
-     */
-    @Override
-    public List<ValidationException> validate(Body body, View view) {
-        List<ValidationException> exceptions = new ArrayList<>();
+  /**
+   * Checks whether each ParameterRef and ThisRef is used exactly once.
+   *
+   * @return
+   */
+  @Override
+  public List<ValidationException> validate(Body body, View view) {
+    List<ValidationException> exceptions = new ArrayList<>();
 
-        boolean hasThisLocal = false;
-        Optional<? extends SootMethod> optionalSootMethod = view.getMethod(body.getMethodSignature());
-        if (!optionalSootMethod.isPresent()) {
-            exceptions.add(new ValidationException(body.getMethodSignature(), "There is no corresponding SootMethod in the given view for the provided method signature."));
-            return exceptions;
-        }
-
-        SootMethod method = optionalSootMethod.get();
-        int paramCount = method.getParameterCount();
-        boolean[] parameterRefs = new boolean[paramCount];
-
-        for (Stmt stmt : body.getStmts()) {
-            if (stmt instanceof JIdentityStmt) {
-                JIdentityStmt id = (JIdentityStmt) stmt;
-                if (id.getRightOp() instanceof JThisRef) {
-                    hasThisLocal = true;
-                }
-
-                if (id.getRightOp() instanceof JParameterRef) {
-                    JParameterRef ref = (JParameterRef) id.getRightOp();
-                    if (ref.getIndex() < 0 || ref.getIndex() >= paramCount) {
-                        if (paramCount == 0)
-                            exceptions.add(new ValidationException(id, "This methodRef has no parameters, so no parameter reference is allowed"));
-                        else
-                            exceptions.add(new ValidationException(id, String.format("Parameter reference index must be between 0 and %d (inclusive)", paramCount - 1)));
-                    } else {
-                        if (parameterRefs[ref.getIndex()])
-                            exceptions.add(new ValidationException(id, String.format("Only one local for parameter %d is allowed", ref.getIndex())));
-                        parameterRefs[ref.getIndex()] = true;
-                    }
-                }
-            }
-        }
-
-        if (!method.isStatic() && !hasThisLocal) {
-            exceptions.add(new ValidationException(body,
-                    String.format("The methodRef %s is not static, but does not have a this local", body.getMethodSignature())));
-        }
-
-        for (int i = 0; i < paramCount; i++) {
-            if (!parameterRefs[i]) {
-                exceptions.add(new ValidationException(body,
-                        String.format("There is no parameter local for parameter number %d", i)));
-            }
-        }
-        return exceptions;
+    boolean hasThisLocal = false;
+    Optional<? extends SootMethod> optionalSootMethod = view.getMethod(body.getMethodSignature());
+    if (!optionalSootMethod.isPresent()) {
+      exceptions.add(
+          new ValidationException(
+              body.getMethodSignature(),
+              "There is no corresponding SootMethod in the given view for the provided method signature."));
+      return exceptions;
     }
 
-    @Override
-    public boolean isBasicValidator() {
-        return true;
+    SootMethod method = optionalSootMethod.get();
+    int paramCount = method.getParameterCount();
+    boolean[] parameterRefs = new boolean[paramCount];
+
+    for (Stmt stmt : body.getStmts()) {
+      if (stmt instanceof JIdentityStmt) {
+        JIdentityStmt id = (JIdentityStmt) stmt;
+        if (id.getRightOp() instanceof JThisRef) {
+          hasThisLocal = true;
+        }
+
+        if (id.getRightOp() instanceof JParameterRef) {
+          JParameterRef ref = (JParameterRef) id.getRightOp();
+          if (ref.getIndex() < 0 || ref.getIndex() >= paramCount) {
+            if (paramCount == 0)
+              exceptions.add(
+                  new ValidationException(
+                      id,
+                      "This methodRef has no parameters, so no parameter reference is allowed"));
+            else
+              exceptions.add(
+                  new ValidationException(
+                      id,
+                      String.format(
+                          "Parameter reference index must be between 0 and %d (inclusive)",
+                          paramCount - 1)));
+          } else {
+            if (parameterRefs[ref.getIndex()])
+              exceptions.add(
+                  new ValidationException(
+                      id,
+                      String.format("Only one local for parameter %d is allowed", ref.getIndex())));
+            parameterRefs[ref.getIndex()] = true;
+          }
+        }
+      }
     }
+
+    if (!method.isStatic() && !hasThisLocal) {
+      exceptions.add(
+          new ValidationException(
+              body,
+              String.format(
+                  "The methodRef %s is not static, but does not have a this local",
+                  body.getMethodSignature())));
+    }
+
+    for (int i = 0; i < paramCount; i++) {
+      if (!parameterRefs[i]) {
+        exceptions.add(
+            new ValidationException(
+                body, String.format("There is no parameter local for parameter number %d", i)));
+      }
+    }
+    return exceptions;
+  }
+
+  @Override
+  public boolean isBasicValidator() {
+    return true;
+  }
 }
--- sootup.tests/src/test/java/sootup/tests/validator/IdentityValidatorTest.java
+++ sootup.tests/src/test/java/sootup/tests/validator/IdentityValidatorTest.java
@@ -1,7 +1,15 @@
 package sootup.tests.validator;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import sootup.core.model.Body;
 import sootup.core.model.SootClass;
 import sootup.core.model.SourceType;
@@ -12,118 +20,115 @@ import sootup.core.validation.ValidationException;
 import sootup.jimple.parser.JimpleAnalysisInputLocation;
 import sootup.jimple.parser.JimpleView;
 
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
+@Tag("Java8")
 public class IdentityValidatorTest {
 
-    IdentityValidator identityValidator;
-    JimpleView jimpleView;
-    Collection<SootClass> classes;
+  IdentityValidator identityValidator;
+  JimpleView jimpleView;
+  Collection<SootClass> classes;
 
-    @Before
-    public void Setup() {
+  @BeforeEach
+  public void Setup() {
 
-        identityValidator = new IdentityValidator();
+    identityValidator = new IdentityValidator();
 
-        ClassType classTypeFieldRefValidator =
-                new ClassType() {
-                    @Override
-                    public boolean isBuiltInClass() {
-                        return false;
-                    }
+    ClassType classTypeFieldRefValidator =
+        new ClassType() {
+          @Override
+          public boolean isBuiltInClass() {
+            return false;
+          }
 
-                    @Override
-                    public String getFullyQualifiedName() {
-                        return "jimple.IdentityValidator";
-                    }
+          @Override
+          public String getFullyQualifiedName() {
+            return "jimple.IdentityValidator";
+          }
 
-                    @Override
-                    public String getClassName() {
-                        return "IdentityValidator";
-                    }
+          @Override
+          public String getClassName() {
+            return "IdentityValidator";
+          }
 
-                    @Override
-                    public PackageName getPackageName() {
-                        return new PackageName("jimple");
-                    }
-                };
+          @Override
+          public PackageName getPackageName() {
+            return new PackageName("jimple");
+          }
+        };
 
-        String classPath = "src/test/resources/validator/jimple";
-        JimpleAnalysisInputLocation jimpleInputLocation =
-                new JimpleAnalysisInputLocation(Paths.get(classPath), SourceType.Application);
+    String classPath = "src/test/resources/validator/jimple";
+    JimpleAnalysisInputLocation jimpleInputLocation =
+        new JimpleAnalysisInputLocation(Paths.get(classPath), SourceType.Application);
 
-        jimpleView = new JimpleView(jimpleInputLocation);
-        final Optional<SootClass> classSource1 = jimpleView.getClass(classTypeFieldRefValidator);
-        assertFalse(classSource1.isPresent());
-    }
+    jimpleView = new JimpleView(jimpleInputLocation);
+    final Optional<SootClass> classSource1 = jimpleView.getClass(classTypeFieldRefValidator);
+    assertFalse(classSource1.isPresent());
+  }
 
-    @Test
-    public void testThisRefSuccess() {
-        List<ValidationException> validationExceptions_success = identityValidator.validate(
-                getBody("<IdentityValidator: void <init>()>"), jimpleView);
+  @Test
+  public void testThisRefSuccess() {
+    List<ValidationException> validationExceptions_success =
+        identityValidator.validate(getBody("<IdentityValidator: void <init>()>"), jimpleView);
 
-        assertEquals(0, validationExceptions_success.size());
-    }
+    assertEquals(0, validationExceptions_success.size());
+  }
 
-    @Test
-    public void testParameterRefSuccess() {
-        List<ValidationException> validationExceptions_success = identityValidator.validate(
-                getBody("<IdentityValidator: void testParameterRefSuccess(int)>"), jimpleView);
+  @Test
+  public void testParameterRefSuccess() {
+    List<ValidationException> validationExceptions_success =
+        identityValidator.validate(
+            getBody("<IdentityValidator: void testParameterRefSuccess(int)>"), jimpleView);
 
-        assertEquals(0, validationExceptions_success.size());
-    }
+    assertEquals(0, validationExceptions_success.size());
+  }
 
-    @Test
-    public void testNoThisrRef() {
-        List<ValidationException> validationExceptions_success = identityValidator.validate(
-                getBody("<IdentityValidator: void testNoThisrRef(int)>"), jimpleView);
+  @Test
+  public void testNoThisrRef() {
+    List<ValidationException> validationExceptions_success =
+        identityValidator.validate(
+            getBody("<IdentityValidator: void testNoThisrRef(int)>"), jimpleView);
 
-        assertEquals(1, validationExceptions_success.size());
-    }
+    assertEquals(1, validationExceptions_success.size());
+  }
 
-    @Test
-    public void testParameterRefMultiLocals() {
-        List<ValidationException> validationExceptions_success = identityValidator.validate(
-                getBody("<IdentityValidator: void testParameterRefMultiLocals(int)>"), jimpleView);
+  @Test
+  public void testParameterRefMultiLocals() {
+    List<ValidationException> validationExceptions_success =
+        identityValidator.validate(
+            getBody("<IdentityValidator: void testParameterRefMultiLocals(int)>"), jimpleView);
 
-        assertEquals(1, validationExceptions_success.size());
-    }
+    assertEquals(1, validationExceptions_success.size());
+  }
 
-    @Test
-    public void testParameterRefNoLocal() {
-        List<ValidationException> validationExceptions_success = identityValidator.validate(
-                getBody("<IdentityValidator: void testParameterRefNoLocal(int)>"), jimpleView);
+  @Test
+  public void testParameterRefNoLocal() {
+    List<ValidationException> validationExceptions_success =
+        identityValidator.validate(
+            getBody("<IdentityValidator: void testParameterRefNoLocal(int)>"), jimpleView);
 
-        assertEquals(1, validationExceptions_success.size());
-    }
+    assertEquals(1, validationExceptions_success.size());
+  }
 
-    /*
-        Unable to test failure cases for out-of-index parameterRef (including the special case where no parameterRef is allowed).
-        Attempting to access parameterRef out of bounds may result in Jimple load failure.
+  /*
+     Unable to test failure cases for out-of-index parameterRef (including the special case where no parameterRef is allowed).
+     Attempting to access parameterRef out of bounds may result in Jimple load failure.
 
-        Example:
-               public void testNoParameterRefAllowed()
-                {
-                    LocalsValidator l0;
-                    unknown l1;
+     Example:
+            public void testNoParameterRefAllowed()
+             {
+                 LocalsValidator l0;
+                 unknown l1;
 
-                    l0 := @this: IdentityValidator;
-                    l1 = @parameter0: int;
+                 l0 := @this: IdentityValidator;
+                 l1 = @parameter0: int;
 
-                    return;
-                }
-     */
+                 return;
+             }
+  */
 
-    Body getBody(String methodSignature) {
-        return jimpleView.getMethod(jimpleView.getIdentifierFactory()
-                .parseMethodSignature(methodSignature) )
-                .get().getBody();
-    }
+  Body getBody(String methodSignature) {
+    return jimpleView
+        .getMethod(jimpleView.getIdentifierFactory().parseMethodSignature(methodSignature))
+        .get()
+        .getBody();
+  }
 }
