a0b5225d5ff16b93a4832ba0cd71e9ee167702b3
M.Schmidt <markus262@web.de>
2024-06-11 20:25:12 +0200

    fix the case with multiple dependend .java files to compile

--- sootup.java.bytecode/src/main/java/sootup/java/bytecode/inputlocation/OTFCompileAnalysisInputLocation.java
+++ sootup.java.bytecode/src/main/java/sootup/java/bytecode/inputlocation/OTFCompileAnalysisInputLocation.java
@@ -1,5 +1,18 @@
 package sootup.java.bytecode.inputlocation;
 
+import static java.lang.System.currentTimeMillis;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.tools.*;
 import sootup.core.frontend.SootClassSource;
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.model.SourceType;
@@ -8,192 +21,172 @@ import sootup.core.types.ClassType;
 import sootup.core.views.View;
 import sootup.java.core.interceptors.BytecodeBodyInterceptors;
 
-import javax.annotation.Nonnull;
-import javax.tools.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileTime;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.lang.System.currentTimeMillis;
-
-/**
- * e.g. to simplify creating testcases - no manual compilation step is required
- *
- * <p>TODO: cache compilation results if src did not change
- */
+/** e.g. to simplify creating testcases - no manual compilation step is required */
 public class OTFCompileAnalysisInputLocation implements AnalysisInputLocation {
 
-    private final List<AnalysisInputLocation> inputLocations = new ArrayList<>();
+  private final AnalysisInputLocation inputLocation;
 
-    /**
-     * for Java file contents as a String i.e. not as a File on the filesystem
-     */
-    public OTFCompileAnalysisInputLocation(String fileName, String compilationUnitsContent) {
-        this(
-                fileName,
-                compilationUnitsContent,
-                SourceType.Application,
-                BytecodeBodyInterceptors.Default.getBodyInterceptors());
-    }
+  /** for Java file contents as a String i.e. not as a File on the filesystem */
+  public OTFCompileAnalysisInputLocation(String fileName, String compilationUnitsContent) {
+    this(
+        fileName,
+        compilationUnitsContent,
+        SourceType.Application,
+        BytecodeBodyInterceptors.Default.getBodyInterceptors());
+  }
 
-    public OTFCompileAnalysisInputLocation(
-            String fileName,
-            String compilationUnitsContent,
-            @Nonnull SourceType srcType,
-            @Nonnull List<BodyInterceptor> bodyInterceptors) {
-        Path compile = compile(fileName, compilationUnitsContent);
-        inputLocations.add(PathBasedAnalysisInputLocation.create(compile, srcType, bodyInterceptors));
-    }
+  public OTFCompileAnalysisInputLocation(
+      String fileName,
+      String compilationUnitsContent,
+      @Nonnull SourceType srcType,
+      @Nonnull List<BodyInterceptor> bodyInterceptors) {
+    Path compile = compile(fileName, compilationUnitsContent);
+    inputLocation = PathBasedAnalysisInputLocation.create(compile, srcType, bodyInterceptors);
+  }
 
-    /**
-     * existing .java files
-     */
-    public OTFCompileAnalysisInputLocation(Path dotJavaFile) {
-        this(Collections.singletonList(dotJavaFile));
-    }
+  /** existing .java files */
+  public OTFCompileAnalysisInputLocation(Path dotJavaFile) {
+    this(Collections.singletonList(dotJavaFile));
+  }
 
-    public OTFCompileAnalysisInputLocation(List<Path> dotJavaFile) {
-        this(
-                dotJavaFile,
-                SourceType.Application,
-                BytecodeBodyInterceptors.Default.getBodyInterceptors());
-    }
+  public OTFCompileAnalysisInputLocation(List<Path> dotJavaFile) {
+    this(
+        dotJavaFile,
+        SourceType.Application,
+        BytecodeBodyInterceptors.Default.getBodyInterceptors());
+  }
 
-    public OTFCompileAnalysisInputLocation(
-            @Nonnull List<Path> dotJavaFiles,
-            @Nonnull SourceType srcType,
-            @Nonnull List<BodyInterceptor> bodyInterceptors) {
-        dotJavaFiles.forEach(
-                file -> {
-                    Path compile = compile(file.toFile());
-                    inputLocations.add(
-                            PathBasedAnalysisInputLocation.create(compile, srcType, bodyInterceptors));
-                });
-    }
+  public OTFCompileAnalysisInputLocation(
+      @Nonnull List<Path> dotJavaFiles,
+      @Nonnull SourceType srcType,
+      @Nonnull List<BodyInterceptor> bodyInterceptors) {
+    Path compile = compile(dotJavaFiles);
+    inputLocation = PathBasedAnalysisInputLocation.create(compile, srcType, bodyInterceptors);
+  }
 
-    @Nonnull
-    @Override
-    public Optional<? extends SootClassSource> getClassSource(
-            @Nonnull ClassType type, @Nonnull View view) {
-        return inputLocations
-                .parallelStream()
-                .map(il -> il.getClassSource(type, view))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst();
-    }
+  @Nonnull
+  @Override
+  public Optional<? extends SootClassSource> getClassSource(
+      @Nonnull ClassType type, @Nonnull View view) {
+    return inputLocation.getClassSource(type, view);
+  }
 
-    @Nonnull
-    @Override
-    public Collection<? extends SootClassSource> getClassSources(@Nonnull View view) {
-        return inputLocations.stream()
-                .flatMap(il -> il.getClassSources(view).stream())
-                .collect(Collectors.toList());
-    }
+  @Nonnull
+  @Override
+  public Collection<? extends SootClassSource> getClassSources(@Nonnull View view) {
+    return inputLocation.getClassSources(view);
+  }
 
-    @Nonnull
-    @Override
-    public SourceType getSourceType() {
-        return inputLocations.get(0).getSourceType();
-    }
+  @Nonnull
+  @Override
+  public SourceType getSourceType() {
+    return inputLocation.getSourceType();
+  }
 
-    @Nonnull
-    @Override
-    public List<BodyInterceptor> getBodyInterceptors() {
-        // hint: all referenced inputlocations have the same settings
-        return inputLocations.get(0).getBodyInterceptors();
-    }
+  @Nonnull
+  @Override
+  public List<BodyInterceptor> getBodyInterceptors() {
+    // hint: all referenced inputlocations have the same settings
+    return inputLocation.getBodyInterceptors();
+  }
 
-    private static Path getTempDirectory(String fileName) throws IOException {
-        return Files.createTempDirectory("sootup-otfcompile-" + fileName.hashCode());
-    }
+  private static Path getTempDirectory(String fileName) throws IOException {
+    return Files.createTempDirectory("sootup-otfcompile-" + fileName.hashCode());
+  }
 
-    static Path compile(String fileName, String fileContent) {
-        try {
-            Path tmp = getTempDirectory(fileName);
-            Path path = tmp.resolve(fileName.hashCode() + "/");
-            boolean dirWasCreated = path.toFile().mkdirs();
-            Path srcFile = tmp.resolve(fileName);
+  static Path compile(String fileName, String fileContent) {
+    try {
+      Path tmp = getTempDirectory(fileName);
+      Path path = tmp.resolve(fileName.hashCode() + "/");
+      boolean dirWasCreated = path.toFile().mkdirs();
+      Path srcFile = tmp.resolve(fileName);
 
-            if(dirWasCreated) {
-                Files.write(srcFile, fileContent.getBytes());
-            }else{
-                // when the directory with the same content.hashcode() already exists, check its content as well.
-                byte[] bytes = Files.readAllBytes(srcFile);
-                if( !new String(bytes).equals(fileContent)){
-                    // only write when sth actually changed
-                    Files.write(srcFile, fileContent.getBytes());
-                }
-            }
-            return compile(srcFile.toFile());
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+      if (dirWasCreated) {
+        Files.write(srcFile, fileContent.getBytes());
+      } else {
+        // when the directory with the same content.hashcode() already exists, check its content as
+        // well.
+        byte[] bytes = Files.readAllBytes(srcFile);
+        if (!new String(bytes).equals(fileContent)) {
+          // only write when sth actually changed
+          Files.write(srcFile, fileContent.getBytes());
         }
+      }
+
+      /* TODO: don't save source as file - make use of JavaFileObjectImpl
+      int i = name.lastIndexOf('.');
+      String packageName = i < 0 ? "" : name.substring(0, i);
+      String className = i < 0 ? name : name.substring(i + 1);
+      JavaFileObjectImpl javaFileObject = new JavaFileObjectImpl(className, sourceCode);
+      javaFileManager.putFileForInput(StandardLocation.SOURCE_PATH, packageName,
+              className + ClassUtils.JAVA_EXTENSION, javaFileObject);
+      */
+
+      return compile(Collections.singletonList(srcFile));
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Nonnull
-    static Path compile(File... srcFiles) {
-        // based on
-        // https://stackoverflow.com/questions/39239285/how-to-get-list-of-class-files-generated-by-javacompiler-compilationtask
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+  @Nonnull
+  static Path compile(List<Path> srcFiles) {
 
-        // create key for temp dir / caching
-        StringBuilder sb = new StringBuilder();
-        for (File srcFile : srcFiles) {
-            sb.append(srcFile);
-        }
-        String concatenatedFileNames = sb.toString();
-
-        try {
-            Path binDirpath = getTempDirectory(concatenatedFileNames).resolve("bin/");
-            File binDir = binDirpath.toFile();
-            if (!binDir.mkdirs()) {
-                // bin dir already exists -> check modified time
-                FileTime binDirLastModifiedTime = Files.getLastModifiedTime(binDirpath);
-                boolean cacheDirty = false;
-                for (File srcFile : srcFiles) {
-                    if (Files.getLastModifiedTime(srcFile.toPath()).compareTo(binDirLastModifiedTime) > 0 ) {
-                        cacheDirty = true;
-                    }
-                }
-                if(!cacheDirty){
-                    return binDirpath;
-                }
-            }
-
-            JavaFileManager.Location location = StandardLocation.CLASS_OUTPUT;
-            fileManager.setLocation(location, Collections.singleton(binDir));
-
-            Writer writer = new StringWriter();
-            JavaCompiler.CompilationTask task =
-                    compiler.getTask(
-                            writer, fileManager, null, null, null, fileManager.getJavaFileObjects(srcFiles));
-
-            if (task.call()) {
-        /*
-        Set<JavaFileObject.Kind> clazzType = Collections.singleton(JavaFileObject.Kind.CLASS);
-        for (JavaFileObject jfo : fileManager.list(location, "", clazzType, true)) {
-          compiledFiles.add(Paths.get(jfo.toUri()));
-        }*/
-                // update modified timestamp of bin/
-                Files.setLastModifiedTime(binDirpath, FileTime.fromMillis( currentTimeMillis() ));
-
-                return binDir.toPath();
-            } else {
-                throw new IllegalArgumentException("Could not compile the given input.\n " + writer);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    // create key for temp dir / caching
+    StringBuilder sb = new StringBuilder();
+    for (Path srcFile : srcFiles) {
+      sb.append(srcFile);
     }
+    String concatenatedFileNames = sb.toString();
+
+    try {
+      Path binDirpath = getTempDirectory(concatenatedFileNames).resolve("bin/");
+      File binDir = binDirpath.toFile();
+      boolean binDirCreated = binDir.mkdirs();
+      if (!binDirCreated) {
+        // bin dir already exists -> check modified time
+        FileTime binDirLastModifiedTime = Files.getLastModifiedTime(binDirpath);
+        boolean cacheDirty = false;
+        for (Path srcFile : srcFiles) {
+          if (Files.getLastModifiedTime(srcFile).compareTo(binDirLastModifiedTime) > 0) {
+            cacheDirty = true;
+          }
+        }
+        if (!cacheDirty) {
+          return binDirpath;
+        }
+      }
+
+      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+      StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+      fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Collections.singleton(binDir));
+
+      File[] files = new File[srcFiles.size()];
+      srcFiles.stream().map(Path::toFile).collect(Collectors.toList()).toArray(files);
+      Iterable<? extends JavaFileObject> javaFileObjects = fileManager.getJavaFileObjects(files);
+
+      try (Writer writer = new StringWriter()) {
+        JavaCompiler.CompilationTask task =
+            compiler.getTask(writer, fileManager, null, null, null, javaFileObjects);
+
+        if (task.call()) {
+          /* collect all generated .class files
+          Set<JavaFileObject.Kind> clazzType = Collections.singleton(JavaFileObject.Kind.CLASS);
+          for (JavaFileObject jfo : fileManager.list(location, "", clazzType, true)) {
+            compiledFiles.add(Paths.get(jfo.toUri()));
+          }*/
+          if (!binDirCreated) {
+            // update modified timestamp of bin/
+            Files.setLastModifiedTime(binDirpath, FileTime.fromMillis(currentTimeMillis()));
+          }
+          return binDir.toPath();
+        } else {
+          throw new IllegalArgumentException("Could not compile the given input.\n " + writer);
+        }
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
