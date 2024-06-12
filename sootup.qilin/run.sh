1b7f9fd2c66f1452e54f51833003e08ecaa68d50
Kadiray Karakaya <kadiray.k@hotmail.com>
2024-06-12 16:49:51 +0200

    snapshot

--- sootup.qilin/run.sh
+++ sootup.qilin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean compile assembly:single
-mv target/sootup.qilin-1.3.0-jar-with-dependencies.jar ../artifact/Qilin-0.9.4-SNAPSHOT.jar
+mv target/sootup.qilin-1.3.1-SNAPSHOT-jar-with-dependencies.jar ../artifact/Qilin-0.9.4-SNAPSHOT.jar
