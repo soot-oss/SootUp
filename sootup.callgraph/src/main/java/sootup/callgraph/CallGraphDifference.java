e67873602b20a42e0ca3bca685f4a4ba822c44b4
sahilagichani <sahilagichani@gmail.com>
2024-07-12 15:00:15 +0200

    added license mvn -B org.codehaus.mojo:license-maven-plugin:check-file-header

--- sootup.callgraph/src/main/java/sootup/callgraph/CallGraphDifference.java
+++ sootup.callgraph/src/main/java/sootup/callgraph/CallGraphDifference.java
@@ -6,6 +6,28 @@ import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import sootup.core.signatures.MethodSignature;
 
+/*-
+ * #%L
+ * Soot
+ * %%
+ * Copyright (C) 2024 Sahil Agichani
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 public class CallGraphDifference {
 
   private final CallGraph baseCallGraph;
