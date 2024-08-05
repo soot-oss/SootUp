e6b53acbae157b1231bdbff1ee888d0304050692
Markus Schmidt <markus262@web.de>
2024-08-05 12:58:10 +0200

    style

--- sootup.analysis/src/main/java/sootup/analysis/intraprocedural/FlowAnalysis.java
+++ sootup.analysis/src/main/java/sootup/analysis/intraprocedural/FlowAnalysis.java
@@ -453,7 +453,7 @@ public abstract class FlowAnalysis<A> extends AbstractFlowAnalysis<A> {
         Orderer.newUniverse(
             graph,
             isForward ? AnalysisDirection.FORWARD : AnalysisDirection.BACKWARD,
-                entryInitialFlow());
+            entryInitialFlow());
     initFlow(universe, inFlow, outFlow);
 
     Queue<Entry<A>> q = UniverseSortedPriorityQueue.of(universe);
