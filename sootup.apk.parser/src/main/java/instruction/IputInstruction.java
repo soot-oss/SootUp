816b4b614e8a1d407bc1e8e11ca811cca5cbeb2a
Palaniappan Muthuraman <palaniappan.pp@gmail.com>
2024-08-26 17:45:09 +0200

    removed some unwanted code, and fixed javadoc

--- sootup.apk.parser/src/main/java/instruction/IputInstruction.java
+++ sootup.apk.parser/src/main/java/instruction/IputInstruction.java
@@ -1,6 +1,5 @@
 package instruction;
 
-import Util.DexUtil;
 import main.DexBody;
 import org.jf.dexlib2.iface.instruction.Instruction;
 import org.jf.dexlib2.iface.instruction.ReferenceInstruction;
@@ -10,7 +9,6 @@ import sootup.core.jimple.Jimple;
 import sootup.core.jimple.basic.Local;
 import sootup.core.jimple.common.ref.JInstanceFieldRef;
 import sootup.core.jimple.common.stmt.JAssignStmt;
-import sootup.core.types.Type;
 
 public class IputInstruction extends FieldInstruction {
   @Override
@@ -23,18 +21,12 @@ public class IputInstruction extends FieldInstruction {
         Jimple.newInstanceFieldRef(
             body.getRegisterLocal(object), getSootFieldRef(f).getFieldSignature());
     Local sourceValue = body.getRegisterLocal(source);
-    JAssignStmt jAssignStmt = getAssignStmt(body, sourceValue, jInstanceFieldRef);
+    JAssignStmt jAssignStmt = getAssignStmt(sourceValue, jInstanceFieldRef);
     setStmt(jAssignStmt);
     body.add(jAssignStmt);
   }
 
   public IputInstruction(Instruction instruction, int codeAddress) {
     super(instruction, codeAddress);
-  }
-
-  @Override
-  protected Type getTargetType(DexBody body) {
-    FieldReference f = (FieldReference) ((ReferenceInstruction) instruction).getReference();
-    return DexUtil.toSootType(f.getType(), 0);
   }
 }
--- sootup.apk.parser/src/main/java/instruction/SputInstruction.java
+++ sootup.apk.parser/src/main/java/instruction/SputInstruction.java
@@ -1,6 +1,5 @@
 package instruction;
 
-import Util.DexUtil;
 import main.DexBody;
 import org.jf.dexlib2.iface.instruction.Instruction;
 import org.jf.dexlib2.iface.instruction.OneRegisterInstruction;
@@ -10,7 +9,6 @@ import sootup.core.jimple.Jimple;
 import sootup.core.jimple.basic.Local;
 import sootup.core.jimple.common.ref.JStaticFieldRef;
 import sootup.core.jimple.common.stmt.JAssignStmt;
-import sootup.core.types.Type;
 
 public class SputInstruction extends FieldInstruction {
   @Override
@@ -20,15 +18,9 @@ public class SputInstruction extends FieldInstruction {
     JStaticFieldRef instanceField =
         Jimple.newStaticFieldRef(getStaticSootFieldRef(f).getFieldSignature());
     Local sourceValue = body.getRegisterLocal(source);
-    JAssignStmt assign = getAssignStmt(body, sourceValue, instanceField);
+    JAssignStmt assign = getAssignStmt(sourceValue, instanceField);
     setStmt(assign);
     body.add(assign);
-  }
-
-  @Override
-  protected Type getTargetType(DexBody body) {
-    FieldReference f = (FieldReference) ((ReferenceInstruction) instruction).getReference();
-    return DexUtil.toSootType(f.getType(), 0);
   }
 
   public SputInstruction(Instruction instruction, int codeAddress) {
