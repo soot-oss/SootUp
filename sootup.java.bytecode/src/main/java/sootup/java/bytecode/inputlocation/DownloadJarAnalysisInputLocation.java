0b08a8e693bcb3ad50c3b5fd66ca9aada4a6e704
Palaniappan Muthuraman <palaniappan.pp@gmail.com>
2024-08-29 12:39:37 +0200

    Added new inputlocation

---
+++ sootup.java.bytecode/src/main/java/sootup/java/bytecode/inputlocation/DownloadJarAnalysisInputLocation.java
@@ -0,0 +1,51 @@
+package sootup.java.bytecode.inputlocation;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import sootup.core.model.SourceType;
+import sootup.core.transform.BodyInterceptor;
+
+public class DownloadJarAnalysisInputLocation extends ArchiveBasedAnalysisInputLocation {
+
+  private static final int BUFFER_SIZE = 1024;
+
+  public DownloadJarAnalysisInputLocation(
+      String downloadURL, List<BodyInterceptor> bodyInterceptors, Collection<Path> ignoredPaths) {
+    super(
+        downloadAndConstructPath(downloadURL), SourceType.Library, bodyInterceptors, ignoredPaths);
+  }
+
+  private static Path downloadAndConstructPath(String downloadURL) {
+    HttpURLConnection connection;
+    String tempDirPath = System.getProperty("java.io.tmpdir");
+    String filename = downloadURL.substring(downloadURL.lastIndexOf("/") + 1);
+    File file = new File(tempDirPath, filename);
+    try {
+      URL url = new URL(downloadURL);
+      connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      int responseCode = connection.getResponseCode();
+      if (responseCode != HttpURLConnection.HTTP_OK) {
+        throw new IOException("HTTP request failed with response code " + responseCode);
+      }
+      try (InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+          OutputStream outputStream = Files.newOutputStream(Paths.get(file.getAbsolutePath()))) {
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer, 0, BUFFER_SIZE)) != -1) {
+          outputStream.write(buffer, 0, bytesRead);
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return file.exists() ? file.toPath() : null;
+  }
+}
