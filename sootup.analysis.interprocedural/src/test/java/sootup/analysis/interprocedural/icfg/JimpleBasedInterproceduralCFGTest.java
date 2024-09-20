00ffaaf88a0e8fe8384bf961bcb1a6165f031022
Kadiray Karakaya <kadiray.k@hotmail.com>
2024-09-20 14:41:36 +0200

    fix

--- sootup.analysis.interprocedural/src/test/java/sootup/analysis/interprocedural/icfg/JimpleBasedInterproceduralCFGTest.java
+++ sootup.analysis.interprocedural/src/test/java/sootup/analysis/interprocedural/icfg/JimpleBasedInterproceduralCFGTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import sootup.core.inputlocation.AnalysisInputLocation;
