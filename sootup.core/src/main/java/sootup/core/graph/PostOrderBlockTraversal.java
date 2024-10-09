3fcafe0d8e8ae74334b4b8e72cae163f1fc2080d
zunwang <wangzun666@gmail.com>
2024-10-09 16:32:00 +0200

    add interface BlockTraversal

--- sootup.core/src/main/java/sootup/core/graph/PostOrderBlockTraversal.java
+++ sootup.core/src/main/java/sootup/core/graph/PostOrderBlockTraversal.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import javax.annotation.Nonnull;
 
-public class PostOrderBlockTraversal {
+public class PostOrderBlockTraversal implements BlockTraversal {
 
   private final BasicBlock<?> startNode;
 
@@ -44,7 +44,7 @@ public class PostOrderBlockTraversal {
   }
 
   public BlockIterator iterator() {
-    return new BlockIterator(startNode);
+    return new PostOrderBlockIterator(startNode);
   }
 
   @Nonnull
@@ -54,55 +54,5 @@ public class PostOrderBlockTraversal {
                 new PostOrderBlockTraversal(cfg).iterator(), Spliterator.ORDERED),
             false)
         .collect(Collectors.toList());
-  }
-
-  public static class BlockIterator implements Iterator<BasicBlock<?>> {
-    private final Stack<Frame> stack = new Stack<>();
-    private final Set<BasicBlock<?>> visited = new HashSet<>();
-
-    public BlockIterator(@Nonnull BasicBlock<?> startNode) {
-      visitNode(startNode);
-      stack.push(
-          new Frame(startNode, ((List<BasicBlock<?>>) startNode.getSuccessors()).iterator()));
-    }
-
-    private boolean visitNode(@Nonnull BasicBlock<?> node) {
-      return visited.add(node);
-    }
-
-    @Override
-    public boolean hasNext() {
-      return !stack.isEmpty();
-    }
-
-    @Override
-    public BasicBlock<?> next() {
-      if (!hasNext()) {
-        throw new NoSuchElementException("There is no more block.");
-      }
-      while (!stack.isEmpty()) {
-        Frame frame = stack.peek();
-        if (frame.succIterator.hasNext()) {
-          BasicBlock<?> succ = frame.succIterator.next();
-          if (visitNode(succ)) {
-            stack.push(new Frame(succ, ((List<BasicBlock<?>>) succ.getSuccessors()).iterator()));
-          }
-        } else {
-          stack.pop();
-          return frame.node;
-        }
-      }
-      return null;
-    }
-
-    private static class Frame {
-      final BasicBlock<?> node;
-      final Iterator<BasicBlock<?>> succIterator;
-
-      Frame(BasicBlock<?> node, Iterator<BasicBlock<?>> childIterator) {
-        this.node = node;
-        this.succIterator = childIterator;
-      }
-    }
   }
 }
