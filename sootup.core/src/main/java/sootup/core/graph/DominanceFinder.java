fda4bffe76b27fa1fc435ce48d8cdaf4d54a230a
zunwang <wangzun666@gmail.com>
2024-10-10 12:18:51 +0200

    repair DominanceFinder

--- sootup.core/src/main/java/sootup/core/graph/DominanceFinder.java
+++ sootup.core/src/main/java/sootup/core/graph/DominanceFinder.java
@@ -26,7 +26,6 @@ import java.util.*;
 import javax.annotation.Nonnull;
 
 /**
- * @author Zun Wang
  * @see <a
  *     href="https://www.researchgate.net/publication/2569680_A_Simple_Fast_Dominance_Algorithm">
  *     https://www.researchgate.net/publication/2569680_A_Simple_Fast_Dominance_Algorithm </a>
@@ -38,79 +37,33 @@ public class DominanceFinder {
   private int[] doms;
   private ArrayList<Integer>[] domFrontiers;
 
-  protected AnalysisDirection direction;
-
-  public enum AnalysisDirection {
-    BACKWARD {
-      @Override
-      @Nonnull
-      List<? extends BasicBlock<?>> getPredecessors(BasicBlock<?> block) {
-        return block.getSuccessors();
-      }
-
-      @Nonnull
-      @Override
-      List<BasicBlock<?>> getSortedBlocks(StmtGraph<?> blockGraph) {
-        return Collections.unmodifiableList(new BackwardsStmtGraph(blockGraph).getBlocksSorted());
-      }
-    },
-    FORWARD {
-      @Override
-      @Nonnull
-      List<? extends BasicBlock<?>> getPredecessors(BasicBlock<?> block) {
-        return block.getPredecessors();
-      }
-
-      @Nonnull
-      @Override
-      List<BasicBlock<?>> getSortedBlocks(StmtGraph<?> blockGraph) {
-        return Collections.unmodifiableList(blockGraph.getBlocksSorted());
-      }
-    };
-
-    @Nonnull
-    abstract List<? extends BasicBlock<?>> getPredecessors(BasicBlock<?> block);
-
-    @Nonnull
-    abstract List<BasicBlock<?>> getSortedBlocks(StmtGraph<?> blockGraph);
-  }
-
   public DominanceFinder(@Nonnull StmtGraph<?> blockGraph) {
-    this(blockGraph, AnalysisDirection.FORWARD);
-  }
 
-  protected DominanceFinder(@Nonnull StmtGraph<?> blockGraph, AnalysisDirection direction) {
-    this.direction = direction;
-
-    // we're locked into providing a List<BasicBlock<?>>, not a List<? extends BasicBlock<?>>, so
-    // we'll use the block iterator directly (which provides this type) rather than
-    // #getBlocksSorted.
-    blocks = direction.getSortedBlocks(blockGraph);
-
-    // assign each block a integer id. The starting block must have id 0; rely on
-    // getBlocksSorted to have put the starting block first.
+    // blocks should be in reverse post order
+    blocks = ReversePostOrderBlockTraversal.getBlocksSorted(blockGraph);
     for (int i = 0; i < blocks.size(); i++) {
       BasicBlock<?> block = blocks.get(i);
       blockToIdx.put(block, i);
     }
-    final BasicBlock<?> startingStmtBlock = blocks.get(0);
 
     // initialize doms
+    final BasicBlock<?> rootBlock = blockGraph.getStartingStmtBlock();
+    int rootBlockId = blockToIdx.get(rootBlock);
     doms = new int[blocks.size()];
     Arrays.fill(doms, -1);
-    doms[0] = 0;
+    doms[rootBlockId] = rootBlockId;
 
     // calculate immediate dominator for each block
     boolean isChanged = true;
     while (isChanged) {
       isChanged = false;
       for (BasicBlock<?> block : blocks) {
-        if (block.equals(startingStmtBlock)) {
+        if (block.equals(rootBlock)) {
           continue;
         }
         int blockIdx = blockToIdx.get(block);
-        List<BasicBlock<?>> preds = new ArrayList<>(direction.getPredecessors(block));
         // ms: should not be necessary preds.addAll(block.getExceptionalPredecessors());
+        List<BasicBlock<?>> preds = new ArrayList<>(block.getPredecessors());
         int newIdom = getFirstDefinedBlockPredIdx(preds);
         if (!preds.isEmpty() && newIdom != -1) {
           BasicBlock<?> processed = blocks.get(newIdom);
@@ -131,24 +84,21 @@ public class DominanceFinder {
       }
     }
 
-    // startBlockId should not have immediate dominator, actually.
-    doms[0] = -1;
-
     // initialize domFrontiers
-    domFrontiers = new ArrayList[blockGraph.getBlocks().size()];
+    domFrontiers = new ArrayList[blocks.size()];
     for (int i = 0; i < domFrontiers.length; i++) {
       domFrontiers[i] = new ArrayList<>();
     }
 
     // calculate dominance frontiers for each block
     for (BasicBlock<?> block : blocks) {
-      List<BasicBlock<?>> preds = new ArrayList<>(direction.getPredecessors(block));
+      List<BasicBlock<?>> preds = new ArrayList<>(block.getPredecessors());
       // ms: should not be necessary  preds.addAll(block.getExceptionalPredecessors());
       if (preds.size() > 1) {
         int blockId = blockToIdx.get(block);
         for (BasicBlock<?> pred : preds) {
           int predId = blockToIdx.get(pred);
-          while (predId != -1 && predId != doms[blockId]) {
+          while (predId != doms[blockId]) {
             domFrontiers[predId].add(blockId);
             predId = doms[predId];
           }
