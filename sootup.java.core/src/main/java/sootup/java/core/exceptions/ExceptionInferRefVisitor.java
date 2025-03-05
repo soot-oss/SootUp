83916000cc2d3c46821516c2b553c34b4083d482
zunwang <wangzun666@gmail.com>
2025-03-05 09:28:21 +0100

    add 3 visitors for ExceptionInfer

--- sootup.java.core/src/main/java/sootup/java/core/exceptions/ExceptionInferRefVisitor.java
+++ sootup.java.core/src/main/java/sootup/java/core/exceptions/ExceptionInferRefVisitor.java
@@ -1,52 +1,60 @@
 package sootup.java.core.exceptions;
 
+import javax.annotation.Nonnull;
 import sootup.core.jimple.common.ref.*;
 import sootup.core.jimple.visitor.AbstractRefVisitor;
-
-import javax.annotation.Nonnull;
+import sootup.core.util.ImmutableUtils;
 
 public class ExceptionInferRefVisitor extends AbstractRefVisitor {
 
-    protected ExceptionInferResult result = ExceptionInferResult.createDefaultResult();;
+  private ExceptionInferResult result;
 
-    public ExceptionInferRefVisitor() {}
+  public ExceptionInferRefVisitor() {}
 
-    @Override
-    public void caseStaticFieldRef(@Nonnull JStaticFieldRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseStaticFieldRef(@Nonnull JStaticFieldRef ref) {
+    result = new ExceptionInferResult(ExceptionInferResult.ErrorType.INITIALIZATION_ERROR);
+  }
 
-    @Override
-    public void caseInstanceFieldRef(@Nonnull JInstanceFieldRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseInstanceFieldRef(@Nonnull JInstanceFieldRef ref) {
+    result =
+        new ExceptionInferResult(
+            ImmutableUtils.immutableSet(
+                ExceptionInferResult.ErrorType.RESOLVE_FIELD_ERROR,
+                ExceptionInferResult.ExceptionType.NUll_POINTER_EXCEPTION));
+  }
 
-    @Override
-    public void caseArrayRef(@Nonnull JArrayRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseArrayRef(@Nonnull JArrayRef ref) {
+    result =
+        new ExceptionInferResult(
+            ImmutableUtils.immutableSet(
+                ExceptionInferResult.ExceptionType.INDEX_OUT_OF_BOUNDS_EXCEPTION,
+                ExceptionInferResult.ExceptionType.NUll_POINTER_EXCEPTION));
+  }
 
-    @Override
-    public void caseParameterRef(@Nonnull JParameterRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseParameterRef(@Nonnull JParameterRef ref) {
+    defaultCaseRef(ref);
+  }
 
-    @Override
-    public void caseCaughtExceptionRef(@Nonnull JCaughtExceptionRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseCaughtExceptionRef(@Nonnull JCaughtExceptionRef ref) {
+    defaultCaseRef(ref);
+  }
 
-    @Override
-    public void caseThisRef(@Nonnull JThisRef ref) {
-        defaultCaseRef(ref);
-    }
+  @Override
+  public void caseThisRef(@Nonnull JThisRef ref) {
+    defaultCaseRef(ref);
+  }
 
+  @Override
+  public void defaultCaseRef(@Nonnull Ref ref) {
+    result = ExceptionInferResult.createEmptyException();
+  }
 
-    @Override
-    public void defaultCaseRef(@Nonnull Ref ref) { }
-
-    public ExceptionInferResult getResult() {
-        return result;
-    }
-
+  public ExceptionInferResult getResult() {
+    return result;
+  }
 }
---
+++ sootup.java.core/src/main/java/sootup/java/core/exceptions/ExceptionInferStmtVisitor.java
@@ -0,0 +1,131 @@
+package sootup.java.core.exceptions;
+
+import javax.annotation.Nonnull;
+import sootup.core.jimple.basic.Value;
+import sootup.core.jimple.common.expr.Expr;
+import sootup.core.jimple.common.ref.JArrayRef;
+import sootup.core.jimple.common.ref.Ref;
+import sootup.core.jimple.common.stmt.*;
+import sootup.core.jimple.javabytecode.stmt.*;
+import sootup.core.jimple.visitor.AbstractStmtVisitor;
+import sootup.core.typehierarchy.TypeHierarchy;
+import sootup.core.types.ClassType;
+import sootup.core.types.UnknownType;
+import sootup.core.util.ImmutableUtils;
+
+public class ExceptionInferStmtVisitor extends AbstractStmtVisitor {
+
+  private ExceptionInferResult result;
+  private final TypeHierarchy hierarchy;
+  private final ExceptionInferExprVisitor exprVisitor;
+  private final ExceptionInferRefVisitor refVisitor;
+
+  public ExceptionInferStmtVisitor(TypeHierarchy hierarchy) {
+      this.hierarchy = hierarchy;
+      this.refVisitor = new ExceptionInferRefVisitor();
+      this.exprVisitor = new ExceptionInferExprVisitor(hierarchy);
+      this.result = new ExceptionInferResult(ImmutableUtils.immutableSet(ExceptionInferResult.ErrorType.VM_ERROR, ExceptionInferResult.ErrorType.THREAD_DEATH));
+  }
+
+  @Override
+  public void caseBreakpointStmt(@Nonnull JBreakpointStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseInvokeStmt(@Nonnull JInvokeStmt stmt) {
+    if(stmt.getInvokeExpr().isPresent()){
+      Expr expr = stmt.getInvokeExpr().get();
+      expr.accept(exprVisitor);
+      result.addExceptions(exprVisitor.getResult(), hierarchy);
+    }
+  }
+
+  @Override
+  public void caseAssignStmt(@Nonnull JAssignStmt stmt) {
+    Value leftOp = stmt.getLeftOp();
+    Value rightOp = stmt.getRightOp();
+    // store in array
+    if(leftOp instanceof Ref){
+      if (leftOp instanceof JArrayRef && (leftOp.getType() instanceof UnknownType || leftOp.getType() instanceof ClassType)) {
+        result.addException(ExceptionInferResult.ExceptionType.ARRAY_STORE_EXCEPTION, hierarchy);
+      }
+      ((Ref) leftOp).accept(refVisitor);
+      result.addExceptions(refVisitor.getResult(), hierarchy);
+    }
+    if(rightOp instanceof Ref){
+      ((Ref) rightOp).accept(refVisitor);
+      result.addExceptions(refVisitor.getResult(), hierarchy);
+    }else if(rightOp instanceof Expr){
+      ((Expr) rightOp).accept(exprVisitor);
+      result.addExceptions(exprVisitor.getResult(), hierarchy);
+    }
+  }
+
+  @Override
+  public void caseIdentityStmt(@Nonnull JIdentityStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseEnterMonitorStmt(@Nonnull JEnterMonitorStmt stmt) {
+    result.addException(ExceptionInferResult.ExceptionType.NUll_POINTER_EXCEPTION, hierarchy);
+  }
+
+  @Override
+  public void caseExitMonitorStmt(@Nonnull JExitMonitorStmt stmt) {
+    result.addException(ExceptionInferResult.ExceptionType.NUll_POINTER_EXCEPTION, hierarchy);
+    result.addException(ExceptionInferResult.ExceptionType.ILLEGAL_MONITOR_STATE_EXCEPTION, hierarchy);
+  }
+
+  @Override
+  public void caseGotoStmt(@Nonnull JGotoStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseIfStmt(@Nonnull JIfStmt stmt) {
+    Expr conditionExpr = stmt.getCondition();
+    conditionExpr.accept(exprVisitor);
+    result.addExceptions(exprVisitor.getResult(), hierarchy);
+  }
+
+  @Override
+  public void caseNopStmt(@Nonnull JNopStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseRetStmt(@Nonnull JRetStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseReturnStmt(@Nonnull JReturnStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseReturnVoidStmt(@Nonnull JReturnVoidStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseSwitchStmt(@Nonnull JSwitchStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void caseThrowStmt(@Nonnull JThrowStmt stmt) {
+    defaultCaseStmt(stmt);
+  }
+
+  @Override
+  public void defaultCaseStmt(@Nonnull Stmt stmt) {
+
+  }
+
+  public ExceptionInferResult getResult(){
+    return this.result;
+  }
+}
