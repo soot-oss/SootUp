ec1c2b0f1c3235c3fc32763c1d69d195bfa2a3d7
sahilagichani <sahilagichani@gmail.com>
2025-03-20 21:04:19 +0100

    fix style

--- sootup.examples/src/test/java/sootup/examples/callgraph/CallgraphExampleTest.java
+++ sootup.examples/src/test/java/sootup/examples/callgraph/CallgraphExampleTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 import sootup.callgraph.CallGraph;
 import sootup.callgraph.CallGraphAlgorithm;
