2114d6b88983295f19457ffe6e76307f7ef6bf32
Jonas Klauke <Jonas.Klauke@upb.de>
2025-05-20 14:24:27 +0200

    fmt

--- sootup.callgraph/src/main/java/sootup/callgraph/CallGraph.java
+++ sootup.callgraph/src/main/java/sootup/callgraph/CallGraph.java
@@ -27,7 +27,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.jspecify.annotations.NonNull;
 import sootup.core.jimple.common.stmt.InvokableStmt;
 import sootup.core.signatures.MethodSignature;
@@ -187,9 +186,8 @@ public interface CallGraph {
   }
 
   /**
-   * This method converts the call graph object into dot format and write it to a string file.
-   * The first entry contains the graph info and the last entry closes the graph.
-   *
+   * This method converts the call graph object into dot format and write it to a string file. The
+   * first entry contains the graph info and the last entry closes the graph.
    *
    * @return a List containing all edges as Strings in the dot format
    */
@@ -203,9 +201,8 @@ public interface CallGraph {
 
   /**
    * This method converts the call graph object into dot format and writes it to a string file. The
-   * calls are sorted by the given Comparator for Call Objects.
-   * The first entry contains the graph info and the last entry closes the graph.
-   *
+   * calls are sorted by the given Comparator for Call Objects. The first entry contains the graph
+   * info and the last entry closes the graph.
    *
    * @param callComparator the comparator responsible for sorting the calls
    * @return a List containing all edges as Strings in the dot format sorted by the given Comparator
@@ -213,7 +210,8 @@ public interface CallGraph {
   default List<String> exportAsDot(Comparator<Call> callComparator) {
     ArrayList<String> result = new ArrayList<>();
     result.add("strict digraph ObjectGraph {");
-    result.addAll(getCalls().stream()
+    result.addAll(
+        getCalls().stream()
             .sorted(callComparator)
             .map(call -> toDotEdge(call).toString())
             .toList());
--- sootup.tests/src/test/java/sootup/tests/CallGraphTest.java
+++ sootup.tests/src/test/java/sootup/tests/CallGraphTest.java
@@ -3,7 +3,6 @@ package sootup.tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -328,7 +327,7 @@ public class CallGraphTest {
     algorithmName = "RTA";
     CallGraph cg = loadCallGraph();
     // unsorted
-    List<String> actualContent =cg.exportAsDot();
+    List<String> actualContent = cg.exportAsDot();
     assertTrue(actualContent.size() > 1);
     assertEquals("strict digraph ObjectGraph {", actualContent.get(0));
     assertEquals("}", actualContent.get(actualContent.size() - 1));
@@ -346,27 +345,23 @@ public class CallGraphTest {
             "\"<Main: void main(java.lang.String[])>\"->\"<SubClassMethodImplemented: int method()>\"[label=\"5\"]"));
 
     // sorted
-    List<String> actualContentSorted =cg.exportAsDot(
-                        Comparator.comparing(
-                                (Call call) ->
-                                    call.sourceMethodSignature()
-                                        .getDeclClassType()
-                                        .getFullyQualifiedName())
-                            // src method name
-                            .thenComparing(call -> call.sourceMethodSignature().getName())
-                            // src parameter list
-                            .thenComparing(
-                                call -> call.sourceMethodSignature().getParameterTypes().toString())
-                            // target class name
-                            .thenComparing(
-                                call ->
-                                    call.targetMethodSignature().getDeclClassType().getClassName())
-                            // target method name
-                            .thenComparing(call -> call.targetMethodSignature().getName())
-                            // target parameter list
-                            .thenComparing(
-                                call ->
-                                    call.targetMethodSignature().getParameterTypes().toString()));
+    List<String> actualContentSorted =
+        cg.exportAsDot(
+            Comparator.comparing(
+                    (Call call) ->
+                        call.sourceMethodSignature().getDeclClassType().getFullyQualifiedName())
+                // src method name
+                .thenComparing(call -> call.sourceMethodSignature().getName())
+                // src parameter list
+                .thenComparing(call -> call.sourceMethodSignature().getParameterTypes().toString())
+                // target class name
+                .thenComparing(
+                    call -> call.targetMethodSignature().getDeclClassType().getClassName())
+                // target method name
+                .thenComparing(call -> call.targetMethodSignature().getName())
+                // target parameter list
+                .thenComparing(
+                    call -> call.targetMethodSignature().getParameterTypes().toString()));
     assertTrue(actualContentSorted.size() > 1);
     assertEquals("strict digraph ObjectGraph {", actualContentSorted.get(0));
     assertEquals("}", actualContentSorted.get(actualContentSorted.size() - 1));
