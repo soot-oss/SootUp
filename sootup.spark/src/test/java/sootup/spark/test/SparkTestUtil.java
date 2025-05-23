982ccfc09bb4bc69a283cbe66893bc136b5bce2f
Kadiray Karakaya <kadiray.k@hotmail.com>
2025-05-23 16:49:39 +0200

    add dependency jgrapht

--- sootup.spark/src/test/java/sootup/spark/test/SparkTestUtil.java
+++ sootup.spark/src/test/java/sootup/spark/test/SparkTestUtil.java
@@ -6,7 +6,6 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.nio.dot.DOTExporter;
 import sootup.core.signatures.PackageName;
 import sootup.core.types.ClassType;
-import sootup.spark.MethodPAG;
 import sootup.spark.node.Node;
 
 import java.io.StringWriter;
