d452af988947fb8beca3ba0b7d15c6a9c34f59a4
sahilagichani <sahilagichani@gmail.com>
2025-05-27 22:37:01 +0200

    add tc for codecov

--- sootup.java.core/src/test/java/sootup/java/core/views/JavaViewTest.java
+++ sootup.java.core/src/test/java/sootup/java/core/views/JavaViewTest.java
@@ -2,6 +2,7 @@ package sootup.java.core.views;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +10,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import sootup.core.cache.provider.FullCacheProvider;
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.inputlocation.EagerInputLocation;
 import sootup.core.types.ClassType;
@@ -89,5 +91,12 @@ public class JavaViewTest {
 
     JavaEagerView viewEagerLoad = new JavaEagerView(inputLocation);
     assertTrue(viewEagerLoad.isFullyResolved);
+
+    JavaEagerView viewEagerLoad1 = new JavaEagerView(Collections.singletonList(inputLocation));
+    assertTrue(viewEagerLoad1.isFullyResolved);
+
+    JavaEagerView viewEagerLoad2 =
+        new JavaEagerView(Collections.singletonList(inputLocation), new FullCacheProvider());
+    assertTrue(viewEagerLoad2.isFullyResolved);
   }
 }
