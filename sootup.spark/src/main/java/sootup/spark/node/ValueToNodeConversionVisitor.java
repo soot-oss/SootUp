6ffa130ed02fefb7352d6467b628c9a3954b9883
Kadiray Karakaya <kadiray.k@hotmail.com>
2025-05-28 14:57:59 +0200

    minor

--- sootup.spark/src/main/java/sootup/spark/node/ValueToNodeConversionVisitor.java
+++ sootup.spark/src/main/java/sootup/spark/node/ValueToNodeConversionVisitor.java
@@ -1,5 +1,27 @@
 package sootup.spark.node;
 
+/*-
+ * #%L
+ * SootUp
+ * %%
+ * Copyright (C) 2002-2025 Ondrej Lhotak, Kadiray Karakaya, Palaniappan Muthuraman
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -15,7 +37,7 @@ import sootup.core.jimple.visitor.AbstractValueVisitor;
 import sootup.java.core.JavaIdentifierFactory;
 
 /**
- * {@link sootup.core.jimple.basic.Value} to {@link Node} converter. Supported notes according to
+ * {@link sootup.core.jimple.basic.Value} to {@link Node} converter. Supported nodes according to
  * the Spark thesis:
  *
  * <ul>
