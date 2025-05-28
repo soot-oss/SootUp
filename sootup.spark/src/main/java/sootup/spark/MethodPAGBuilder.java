587446e9c60a37878cf352eb6cde4b21f3e3c167
Kadiray Karakaya <kadiray.k@hotmail.com>
2025-05-28 11:25:18 +0200

    license

--- sootup.spark/src/main/java/sootup/spark/MethodPAGBuilder.java
+++ sootup.spark/src/main/java/sootup/spark/MethodPAGBuilder.java
@@ -1,5 +1,27 @@
 package sootup.spark;
 
+/*-
+ * #%L
+ * SootUp
+ * %%
+ * Copyright (C) 2002-2025 Ondrej Lhotak, Kadiray Karakaya, Palaniappan Muthuraman
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
@@ -9,7 +31,5 @@ import sootup.core.model.SootMethod;
 @Builder
 public class MethodPAGBuilder {
 
-    @NonNull
-    private SootMethod sootMethod;
-
+  @NonNull private SootMethod sootMethod;
 }
--- sootup.spark/src/main/java/sootup/spark/node/Node.java
+++ sootup.spark/src/main/java/sootup/spark/node/Node.java
@@ -1,5 +1,27 @@
 package sootup.spark.node;
 
+/*-
+ * #%L
+ * SootUp
+ * %%
+ * Copyright (C) 2002-2025 Ondrej Lhotak, Kadiray Karakaya, Palaniappan Muthuraman
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
@@ -7,16 +29,14 @@ import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import sootup.core.types.Type;
 
-@FieldDefaults(makeFinal=true, level= AccessLevel.PRIVATE)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @Getter
 @SuperBuilder
 public class Node {
-    @NonNull
-    Type type;
+  @NonNull Type type;
 
-    @Override
-    public String toString() {
-        return String.format("\"%s\"", getType());
-    }
-
+  @Override
+  public String toString() {
+    return String.format("\"%s\"", getType());
+  }
 }
