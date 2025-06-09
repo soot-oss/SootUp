f9712e55608dc2b178730d3b3da8e5dead32fd4a
leoliaolei <leoliaolei@gmail.com>
2025-06-09 15:08:28 +0800

    A quick way to support Short constant

--- sootup.java.core/src/main/java/sootup/java/core/ConstantUtil.java
+++ sootup.java.core/src/main/java/sootup/java/core/ConstantUtil.java
@@ -48,6 +48,9 @@ public class ConstantUtil {
     if (obj instanceof Integer) {
       return IntConstant.getInstance((Integer) obj);
     }
+    if (obj instanceof Short) {
+      return IntConstant.getInstance(((Short) obj).intValue());
+    }
     if (obj instanceof Long) {
       return LongConstant.getInstance((Long) obj);
     }
