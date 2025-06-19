5cea9f116efd2ad4b17f8f4a4180f65a4c728a8e
sahilagichani <sahilagichani@gmail.com>
2025-06-19 17:26:47 +0200

    disable bydefault validation interceptors

--- sootup.interceptors/src/main/java/sootup/interceptors/BytecodeBodyInterceptors.java
+++ sootup.interceptors/src/main/java/sootup/interceptors/BytecodeBodyInterceptors.java
@@ -42,9 +42,9 @@ public enum BytecodeBodyInterceptors {
       new ConstantPropagatorAndFolder(),
       // new DeadAssignmentEliminator(), bug: creates unconnected exceptional flows - see
       // RuntimeJarConversionTests
-      new TypeAssigner(),
-      new BodyValidationInterceptor(),
-      new ClassValidationInterceptor());
+      // new BodyValidationInterceptor(),
+      // new ClassValidationInterceptor()
+      new TypeAssigner());
 
   @NonNull private final List<BodyInterceptor> bodyInterceptors;
 
