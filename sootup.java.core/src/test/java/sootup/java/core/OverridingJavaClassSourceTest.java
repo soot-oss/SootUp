55c18e33fee7c7886b7c7d4dc7157bc1796f304b
Jonas Klauke <Jonas.Klauke@upb.de>
2025-06-20 14:16:15 +0200

    fmt

--- sootup.java.core/src/test/java/sootup/java/core/OverridingJavaClassSourceTest.java
+++ sootup.java.core/src/test/java/sootup/java/core/OverridingJavaClassSourceTest.java
@@ -10,14 +10,13 @@ public class OverridingJavaClassSourceTest {
 
   @Test
   public void testUnsetOverridingJavaClassSource() {
-    OverridingJavaClassSource ojcs= OverridingJavaClassSourceBuilder.builder().build();
-    assertEquals(0,ojcs.resolveInterfaces().size());
-    assertEquals(0,ojcs.resolveFields().size());
-    assertEquals(0,ojcs.resolveModifiers().size());
+    OverridingJavaClassSource ojcs = OverridingJavaClassSourceBuilder.builder().build();
+    assertEquals(0, ojcs.resolveInterfaces().size());
+    assertEquals(0, ojcs.resolveFields().size());
+    assertEquals(0, ojcs.resolveModifiers().size());
     assertTrue(ojcs.resolveSuperclass().isEmpty());
     assertTrue(ojcs.resolveOuterClass().isEmpty());
-    assertEquals(0,ojcs.resolveMethods().size());
-    assertEquals(0,ojcs.resolveMethods().size());
+    assertEquals(0, ojcs.resolveMethods().size());
+    assertEquals(0, ojcs.resolveMethods().size());
   }
-
 }
