5cc4ea33d9a9a054b170e1b2a6f0975936c8ccc4
Stefan Schott <stefan.schott@gmx.net>
2025-07-21 12:27:21 +0200

    add test case that causes the removeExceptionalEdge API to create an invalid StmtGraph

---
+++ sootup.tests/src/test/java/sootup/tests/exceptions/RemoveExceptionalEdgeTest.java
@@ -0,0 +1,53 @@
+package sootup.tests.exceptions;
+
+import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.Test;
+import sootup.core.graph.*;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.model.Body;
+import sootup.core.model.SourceType;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.signatures.PackageName;
+import sootup.core.transform.BodyInterceptor;
+import sootup.core.views.View;
+import sootup.interceptors.BytecodeBodyInterceptors;
+import sootup.java.bytecode.frontend.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.types.JavaClassType;
+import sootup.java.core.views.JavaEagerView;
+import sootup.java.core.views.JavaView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RemoveExceptionalEdgeTest {
+
+    static class ExceptionalEdgeInterceptor implements BodyInterceptor {
+
+        @Override
+        public void interceptBody(Body.@NonNull BodyBuilder builder, @NonNull View view) {
+            MethodSignature methodSignature = view.getIdentifierFactory().parseMethodSignature("<org.atmosphere.util.Version: void <clinit>()>");
+            if (!builder.getMethodSignature().equals(methodSignature)) return;
+
+            MutableStmtGraph stmtGraph = builder.getStmtGraph();
+            List<? extends BasicBlock<?>> blocks = stmtGraph.getBlocks().stream().toList();
+            BasicBlock<?> basicBlock = blocks.get(11);
+            Stmt head =  basicBlock.getHead();
+
+            stmtGraph.removeExceptionalEdge(head, new JavaClassType("Throwable", new PackageName("java.lang")));
+        }
+    }
+
+    @Test
+    public void testRemoveExceptionalEdgeAndMerge() {
+        List<BodyInterceptor> interceptors = new ArrayList<>(BytecodeBodyInterceptors.Default.getBodyInterceptors());
+        interceptors.add(new ExceptionalEdgeInterceptor());
+
+        AnalysisInputLocation inputLocation = new JavaClassPathAnalysisInputLocation("src/test/resources/exception/removeExceptionalEdge/", SourceType.Application,
+                interceptors);
+        JavaView view =  new JavaEagerView(inputLocation);
+    }
+
+
+}
