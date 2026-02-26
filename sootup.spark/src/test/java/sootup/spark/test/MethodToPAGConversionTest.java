0ae1a70ebed405858ffdab264b6bccf22098dde7
Kadiray Karakaya <kadiray.k@hotmail.com>
2026-02-26 14:49:14 +0100

    test basic pag

--- sootup.spark/src/test/java/sootup/spark/test/MethodToPAGConversionTest.java
+++ sootup.spark/src/test/java/sootup/spark/test/MethodToPAGConversionTest.java
@@ -16,6 +16,9 @@ import sootup.java.core.JavaIdentifierFactory;
 import sootup.java.core.views.JavaView;
 import sootup.spark.PAG;
 import sootup.spark.Solver;
+import sootup.spark.node.AllocationNode;
+import sootup.spark.node.InstanceFieldRefNode;
+import sootup.spark.node.VariableNode;
 
 public class MethodToPAGConversionTest {
 
@@ -40,6 +43,33 @@ public class MethodToPAGConversionTest {
     solver.solve();
 
     PAG pag = solver.getPag();
-    // SparkTestUtil.vizualizeMehodPAG(pag.getDelegate());
+
+    var delegate = pag.getDelegate();
+
+    ClassType fieldType = idFactory.getClassType("Basic$Field");
+    ClassType containerType = idFactory.getClassType("Basic$Container");
+
+    var newField = AllocationNode.builder().type(fieldType).build();
+    var fieldStack5 = VariableNode.builder().type(fieldType).name("$stack5").build();
+    var newContainer = AllocationNode.builder().type(containerType).build();
+    var containerStack6 = VariableNode.builder().type(containerType).name("$stack6").build();
+    var fieldL2 = VariableNode.builder().type(fieldType).name("l2").build();
+    var containerL3 = VariableNode.builder().type(containerType).name("l3").build();
+    var containerL4 = VariableNode.builder().type(containerType).name("l4").build();
+    var fieldRef =
+        InstanceFieldRefNode.builder()
+            .base(containerStack6)
+            .field(idFactory.getFieldSignature("field", containerType, fieldType))
+            .type(fieldType)
+            .build();
+
+    assertTrue(delegate.containsEdge(newField, fieldStack5), "Edge new Field -> $stack5 not found");
+    assertTrue(
+        delegate.containsEdge(newContainer, containerStack6),
+        "Edge new Container -> $stack6 not found");
+    assertTrue(delegate.containsEdge(fieldStack5, fieldL2), "Edge $stack5 -> l2 not found");
+    assertTrue(delegate.containsEdge(containerStack6, containerL3), "Edge $stack6 -> l3 not found");
+    assertTrue(delegate.containsEdge(fieldStack5, fieldRef), "Edge $stack5 -> field ref not found");
+    assertTrue(delegate.containsEdge(containerStack6, containerL4), "Edge $stack6 -> l4 not found");
   }
 }
