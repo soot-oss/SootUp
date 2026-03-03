f6304165cc37e75b8c879086345d991774e0efc0
Hazem <hazemgamalelgendy@gmail.com>
2026-03-03 13:02:34 +0100

    tests for android callGraph generation

---
+++ sootup.apk.frontend/src/test/java/sootup/apk/frontend/CallGraphTest.java
@@ -0,0 +1,67 @@
+package sootup.apk.frontend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import sootup.callgraph.CallGraph;
+import sootup.callgraph.CallGraphAlgorithm;
+import sootup.callgraph.ClassHierarchyAnalysisAlgorithm;
+import sootup.callgraph.RapidTypeAnalysisAlgorithm;
+import sootup.core.signatures.MethodSignature;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.views.JavaView;
+
+public class CallGraphTest {
+
+  public static JavaView view;
+
+  String className = "de.ecspride.MainActivity";
+  String methodName = "onCreate";
+  List<String> methodParameters = List.of("android.os.Bundle");
+  String methodReturnType = "void";
+
+  @BeforeAll
+  public static void createView() {
+    String apk_path = "resources/FlowSensitivity1.apk";
+    ApkAnalysisInputLocation sootClassApkAnalysisInputLocation =
+        new ApkAnalysisInputLocation(
+            Paths.get(apk_path), "", DexBodyInterceptors.Default.bodyInterceptors());
+    view = new JavaView(sootClassApkAnalysisInputLocation);
+  }
+
+  @Test
+  public void testCHACallGraphAlgorithm() {
+
+    MethodSignature onCreateMethodSignature =
+        view.getIdentifierFactory()
+            .getMethodSignature(className, methodName, methodReturnType, methodParameters);
+
+    CallGraphAlgorithm cha = new ClassHierarchyAnalysisAlgorithm(view);
+    CallGraph cg = cha.initialize(List.of(onCreateMethodSignature));
+
+    assertTrue(cg.containsMethod(onCreateMethodSignature));
+    assertEquals(9, cg.callsFrom(onCreateMethodSignature).size());
+  }
+
+  @Test
+  public void testRTACallGraphAlgorithm() {
+
+    MethodSignature onCreateMethodSignature =
+        view.getIdentifierFactory()
+            .getMethodSignature(className, methodName, methodReturnType, methodParameters);
+
+    CallGraphAlgorithm rta =
+        new RapidTypeAnalysisAlgorithm(
+            view, view.getClasses().map(JavaSootClass::getType).collect(Collectors.toSet()));
+
+    CallGraph cg = rta.initialize(List.of(onCreateMethodSignature));
+
+    assertTrue(cg.containsMethod(onCreateMethodSignature));
+    assertEquals(9, cg.callsFrom(onCreateMethodSignature).size());
+  }
+}
