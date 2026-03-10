e8f88460a21dea784768b143dc120b505bd9e14e
smohan2 <smohan2@mail.uni-paderborn.de>
2026-03-10 16:42:11 +0100

    Fixing formatting errors

--- sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/interceptors/CopyPropagatorTest.java
+++ sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/interceptors/CopyPropagatorTest.java
@@ -109,7 +109,8 @@ public class CopyPropagatorTest {
   AbstractConditionExpr econdition = JavaJimple.newGtExpr(i2, IntConstant.getInstance(5));
   BranchingStmt eifstmt7 = JavaJimple.newIfStmt(econdition, noStmtPositionInfo);
   // i3 = 6
-  FallsThroughStmt estmt8 = JavaJimple.newAssignStmt(i3, IntConstant.getInstance(6), noStmtPositionInfo);
+  FallsThroughStmt estmt8 =
+      JavaJimple.newAssignStmt(i3, IntConstant.getInstance(6), noStmtPositionInfo);
 
   // r0 := @this Test; r1 = (ref) 0; r2 = (ref) 0L; r3 = (ref) 1; r4 = r1, r5 = r2
   // r1 = (ref) 0
