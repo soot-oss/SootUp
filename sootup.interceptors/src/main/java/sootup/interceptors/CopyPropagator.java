fcb3964f54cc1310cba3f1e6260251106035673a
smohan2 <smohan2@mail.uni-paderborn.de>
2026-03-11 09:57:35 +0100

    Fixing code coverage

--- sootup.interceptors/src/main/java/sootup/interceptors/CopyPropagator.java
+++ sootup.interceptors/src/main/java/sootup/interceptors/CopyPropagator.java
@@ -32,7 +32,6 @@ import sootup.core.graph.MutableControlFlowGraph;
 import sootup.core.jimple.common.Local;
 import sootup.core.jimple.common.Value;
 import sootup.core.jimple.common.constant.*;
-import sootup.core.jimple.common.expr.AbstractBinopExpr;
 import sootup.core.jimple.common.expr.JCastExpr;
 import sootup.core.jimple.common.stmt.AbstractDefinitionStmt;
 import sootup.core.jimple.common.stmt.JAssignStmt;
@@ -82,19 +81,12 @@ public class CopyPropagator implements BodyInterceptor {
 
           if (newStmt instanceof JAssignStmt) {
             Value newRhs = ((JAssignStmt) newStmt).getRightOp();
-            if (newRhs instanceof AbstractBinopExpr) {
-              Value op1 = ((AbstractBinopExpr) newRhs).getOp1();
-              Value op2 = ((AbstractBinopExpr) newRhs).getOp2();
-
-              if (op1 instanceof NumericConstant && op2 instanceof NumericConstant) {
-                Constant folded = Evaluator.getConstantValueOf(newRhs);
-                if (folded != null) {
-                  JAssignStmt modifiedStmt = ((JAssignStmt) newStmt).withRValue(folded);
-                  controlFlowGraph.replaceNode(newStmt, modifiedStmt);
-                }
+            Constant foldedRhs = Evaluator.getConstantValueOf(newRhs);
+              if (foldedRhs != null) {
+                JAssignStmt modifiedStmt = ((JAssignStmt) newStmt).withRValue(foldedRhs);
+                controlFlowGraph.replaceNode(newStmt, modifiedStmt);
               }
             }
-          }
         }
 
         // if rhs is a cast expr with a ref type and its op is 0 (IntConstant or LongConstant)
