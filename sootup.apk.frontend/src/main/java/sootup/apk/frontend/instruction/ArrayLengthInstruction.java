7f0bcc1a608a335d033d685e05d0b10a2a804e9b
Hazem <hazemgamalelgendy@gmail.com>
2026-05-26 11:39:42 +0200

    format

--- sootup.apk.frontend/src/main/java/sootup/apk/frontend/instruction/ArrayLengthInstruction.java
+++ sootup.apk.frontend/src/main/java/sootup/apk/frontend/instruction/ArrayLengthInstruction.java
@@ -26,8 +26,8 @@ import org.jf.dexlib2.iface.instruction.Instruction;
 import org.jf.dexlib2.iface.instruction.formats.Instruction12x;
 import sootup.apk.frontend.main.DexBody;
 import sootup.core.jimple.Jimple;
-import sootup.core.jimple.common.Local;
 import sootup.core.jimple.basic.SimpleStmtPositionInfo;
+import sootup.core.jimple.common.Local;
 import sootup.core.jimple.common.expr.JLengthExpr;
 import sootup.core.jimple.common.stmt.JAssignStmt;
 
