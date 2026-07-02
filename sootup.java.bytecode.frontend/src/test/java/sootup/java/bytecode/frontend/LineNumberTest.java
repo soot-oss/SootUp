b34126e772be4260327ec01d2278d3fe26cd1473
Jonas Klauke <Jonas.Klauke@upb.de>
2026-07-02 18:33:04 +0200

    added testcase to spot linenumber issues by incorrect order of jimple statements

---
+++ sootup.java.bytecode.frontend/src/test/java/sootup/java/bytecode/frontend/LineNumberTest.java
@@ -0,0 +1,44 @@
+package sootup.java.bytecode.frontend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.jimple.common.stmt.InvokableStmt;
+import sootup.core.model.SootMethod;
+import sootup.core.model.SourceType;
+import sootup.java.bytecode.frontend.inputlocation.ClassFileBasedAnalysisInputLocation;
+import sootup.java.bytecode.frontend.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.JavaSootMethod;
+import sootup.java.core.views.JavaView;
+
+public class LineNumberTest {
+  @Test
+  public void test() {
+    AnalysisInputLocation inputLocation =
+        new JavaClassPathAnalysisInputLocation(
+            "src/test/resources/linenumber/",
+            SourceType.Application);
+
+    JavaView view = new JavaView(inputLocation);
+    JavaSootClass c2= view.getClass(view.getIdentifierFactory().getClassType("b.c101")).orElse(null);
+    assertNotNull(c2);
+    JavaSootMethod m2= c2.getMethod("m1177", Collections.emptyList()).orElse(null);
+    assertNotNull(m2);
+    int [] expected = new int [] {6716,6718,6720,6722,6724,6726, 6728,6733};
+    List<Integer> invokeLineNumbers=m2.getBody().getStmts().stream()
+        .filter(stmt -> stmt instanceof InvokableStmt)
+        .filter(stmt -> ((InvokableStmt)stmt).getInvokeExpr().isPresent())
+        .map(stmt -> stmt.getPositionInfo().getStmtPosition().getFirstLine()).toList();
+    assertEquals(expected.length, invokeLineNumbers.size());
+
+    for (int i =0; i < expected.length; i++){
+      assertEquals(expected[i], invokeLineNumbers.get(i));
+    }
+  }
+}
